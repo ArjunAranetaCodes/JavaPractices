d5052058d27262735bfd54f4925f6fbb56073ff2
MoreCodes <arjunaraneta@gmail.com>
2020-06-23 22:50:39 +1000

    Java Programming Exercises

--- Basic Algorithms/Trees/Binary Search Tree - Deletion/Algorithm.java
+++ Basic Algorithms/Trees/Binary Search Tree - Deletion/Algorithm.java
@@ -45,8 +45,6 @@ public class Algorithm {
             return root;
         }
 
-
-    }
-
-
-}
+        if (key < root.key) {
+            root.left = deleteRec(root.left, key);
+        } else if (key > root.key) 
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2020-06-24" -m "Java Programming Exercises"
+git commit --date "2020-06-23" -m "Java Programming Exercises"
 git push origin
