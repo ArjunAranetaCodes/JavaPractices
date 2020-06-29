1f0b6211c95bbe03e67f9f84957867ab5658bc29
MoreCodes <arjunaraneta@gmail.com>
2020-06-29 22:49:40 +1000

    Java Programming Exercises

---
+++ Basic Algorithms/Trees/Binary Search Tree - Deletion/Algorithm.java
@@ -0,0 +1,31 @@
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+public class Algorithm {
+    private Node root;
+
+    public Algorithm() {
+        root = null;
+    }
+
+    // Insert a key into the BST
+    public void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    private Node insertRec(Node root, int key) {
+        if (root == null) {
+        }
+
+    }
+
+
+
+}
