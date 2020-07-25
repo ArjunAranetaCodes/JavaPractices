0f71d2c2bea6aa0516094dd1b9ac17cb6cfa31a3
MoreCodes <arjunaraneta@gmail.com>
2020-07-25 22:43:22 +1000

    Java Programming Exercises

--- Basic Algorithms/Trees/Binary Search Tree - Insertion/Algorithm.java
+++ Basic Algorithms/Trees/Binary Search Tree - Insertion/Algorithm.java
@@ -59,6 +59,7 @@ public class Algorithm {
         tree.insert(30);
         tree.insert(20);
         tree.insert(40);
+        tree.insert(70);
 
     }
 }
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2020-07-26" -m "Java Programming Exercises"
+git commit --date "2020-07-25" -m "Java Programming Exercises"
 git push origin
