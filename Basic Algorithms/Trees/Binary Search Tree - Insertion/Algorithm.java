46417029581b9eb4d79adab33e0b90ac552b2f15
MoreCodes <arjunaraneta@gmail.com>
2020-08-18 22:38:57 +1000

    Java Programming Exercises

--- Basic Algorithms/Trees/Binary Search Tree - Insertion/Algorithm.java
+++ Basic Algorithms/Trees/Binary Search Tree - Insertion/Algorithm.java
@@ -47,7 +47,6 @@ public class Algorithm {
         if (root != null) {
             inorderRec(root.left);
             System.out.print(root.key + " ");
-            inorderRec(root.right);
         }
     }
 }
