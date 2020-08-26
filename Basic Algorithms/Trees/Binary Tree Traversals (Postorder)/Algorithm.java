fe57f1e48d8750bfc673407f90e28050a9a1c249
MoreCodes <arjunaraneta@gmail.com>
2020-08-26 22:36:46 +1000

    Java Programming Exercises

--- Basic Algorithms/Trees/Binary Tree Traversals (Postorder)/Algorithm.java
+++ Basic Algorithms/Trees/Binary Tree Traversals (Postorder)/Algorithm.java
@@ -51,6 +51,7 @@ public class Algorithm {
         tree.root = new TreeNode(1);
         tree.root.left = new TreeNode(2);
         tree.root.right = new TreeNode(3);
+        tree.root.left.left = new TreeNode(4);
 
     }
 }
