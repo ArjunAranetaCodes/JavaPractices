708df10427a6fa50469317801bfb150b7cfa2fc9
MoreCodes <arjunaraneta@gmail.com>
2020-10-19 23:24:28 +1100

    Java Programming Exercises

--- Basic Algorithms/Trees/Binary Tree Traversals (Inorder)/Algorithm.java
+++ Basic Algorithms/Trees/Binary Tree Traversals (Inorder)/Algorithm.java
@@ -19,7 +19,6 @@ public class Algorithm {
     // Inorder traversal of the tree
     private void inorderTraversal(TreeNode node) {
         if (node != null) {
-            // Traverse the left subtree
 
 
         }
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2020-10-20" -m "Java Programming Exercises"
+git commit --date "2020-10-19" -m "Java Programming Exercises"
 git push origin
