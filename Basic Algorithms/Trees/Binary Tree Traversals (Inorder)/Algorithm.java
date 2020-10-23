26343c842a44a5870ce6f166c835138910ffa456
MoreCodes <arjunaraneta@gmail.com>
2020-10-23 23:23:42 +1100

    Java Programming Exercises

--- Basic Algorithms/Trees/Binary Tree Traversals (Inorder)/Algorithm.java
+++ Basic Algorithms/Trees/Binary Tree Traversals (Inorder)/Algorithm.java
@@ -39,14 +39,6 @@ public class Algorithm {
     public static void main(String[] args) {
         Algorithm tree = new Algorithm();
 
-        /* Constructed binary tree is:
-                   1
-                 /   \
-                2     3
-               / \
-              4   5
-        */
-
 
     }
 }
