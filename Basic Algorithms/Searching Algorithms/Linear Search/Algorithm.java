6df26464e5d7c33849bca01557d67765bb1f49d1
MoreCodes <arjunaraneta@gmail.com>
2020-11-08 23:19:04 +1100

    Java Programming Exercises

--- Basic Algorithms/Searching Algorithms/Linear Search/Algorithm.java
+++ Basic Algorithms/Searching Algorithms/Linear Search/Algorithm.java
@@ -35,6 +35,7 @@ public class Algorithm {
         if (index != -1) {
             System.out.println("Element " + target + " found at index " + index);
         } else {
+            System.out.println("Element " + target + " not found in the array");
         }
 
     }
