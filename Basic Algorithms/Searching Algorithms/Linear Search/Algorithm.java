c8430db3e40daa402d29a9439908f7f273ca8469
MoreCodes <arjunaraneta@gmail.com>
2020-11-15 23:18:07 +1100

    Java Programming Exercises

--- Basic Algorithms/Searching Algorithms/Linear Search/Algorithm.java
+++ Basic Algorithms/Searching Algorithms/Linear Search/Algorithm.java
@@ -5,6 +5,7 @@ public class Algorithm {
     public static int linearSearch(int[] array, int target) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == target) {
+                return i; // Return the index where the target is found
             }
         }
     }
