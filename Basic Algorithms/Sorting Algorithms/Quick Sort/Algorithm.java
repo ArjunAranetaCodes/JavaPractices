3ef251cac58d24dea5bd6d31479e900893c8f331
MoreCodes <arjunaraneta@gmail.com>
2020-12-12 23:12:14 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/Quick Sort/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/Quick Sort/Algorithm.java
@@ -17,7 +17,6 @@ public class Algorithm {
             int pivotIndex = partition(arr, low, high);
 
             quickSort(arr, low, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, high);
         }
     }
 
