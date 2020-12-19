fe3b0fe687b7a7de1a26eb1fa8fa59c789bc3ce0
MoreCodes <arjunaraneta@gmail.com>
2020-12-19 23:09:30 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
@@ -71,6 +71,7 @@ public class Algorithm {
     // Utility function to print an array
     public static void printArray(int[] arr) {
         for (int value : arr) {
+            System.out.print(value + " ");
         }
     }
 }
