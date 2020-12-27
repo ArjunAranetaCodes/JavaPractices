0627190c285ac2c8432ab238b152784c92fe68fe
MoreCodes <arjunaraneta@gmail.com>
2020-12-27 23:08:37 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
@@ -55,6 +55,7 @@ public class Algorithm {
 
         // Copy the remaining elements of leftArray[], if there are any
         while (i < n1) {
+            arr[k] = leftArray[i];
         }
 
     }
