f2bda3fb32d817015ecde9832f234aa90e8f43cd
MoreCodes <arjunaraneta@gmail.com>
2020-12-30 23:08:11 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
@@ -44,6 +44,7 @@ public class Algorithm {
         int i = 0, j = 0, k = left;
         while (i < n1 && j < n2) {
             if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
             } else {
             }
         }
