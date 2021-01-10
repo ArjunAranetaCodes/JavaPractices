0dc78e0247a9e42221a98a9b40237bbba133fc79
MoreCodes <arjunaraneta@gmail.com>
2021-01-10 23:05:53 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
@@ -15,7 +15,6 @@ public class Algorithm {
     // Merge Sort function
     public static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
-            // Find the middle point
 
 
         }
