dfd50f6be533e97be22ab583161ac92e80e7eb30
MoreCodes <arjunaraneta@gmail.com>
2021-01-18 23:04:23 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/Merge Sort/Algorithm.java
@@ -43,9 +43,6 @@ public class Algorithm {
         // Merge the temporary arrays
         int i = 0, j = 0, k = left;
         while (i < n1 && j < n2) {
-            if (leftArray[i] <= rightArray[j]) {
-            } else {
-            }
         }
 
 
