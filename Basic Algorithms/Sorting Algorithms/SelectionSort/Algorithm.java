664ffc057aee2317d9327147f0473abe3682e91d
MoreCodes <arjunaraneta@gmail.com>
2021-03-04 22:54:16 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/SelectionSort/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/SelectionSort/Algorithm.java
@@ -27,7 +27,6 @@ public class Algorithm {
             // Swap the found minimum element with the element at i
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
-            arr[i] = temp;
         }
     }
 }
