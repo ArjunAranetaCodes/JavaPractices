ffd05f733dd53e8dbfec48861a27d0b55f082b8e
MoreCodes <arjunaraneta@gmail.com>
2021-03-06 22:52:41 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/BubbleSort/1/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/BubbleSort/1/Algorithm.java
@@ -20,6 +20,7 @@ public class Algorithm {
                     // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2021-03-07" -m "Java Programming Exercises"
+git commit --date "2021-03-06" -m "Java Programming Exercises"
 git push origin
