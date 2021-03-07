05f32d1b61d68639ef66ebfa05bacc930784609b
MoreCodes <arjunaraneta@gmail.com>
2021-03-07 22:52:35 +1100

    Java Programming Exercises

--- Basic Algorithms/Sorting Algorithms/BubbleSort/1/Algorithm.java
+++ Basic Algorithms/Sorting Algorithms/BubbleSort/1/Algorithm.java
@@ -19,6 +19,7 @@ public class Algorithm {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
+                    arr[j] = arr[j + 1];
                 }
             }
         }
