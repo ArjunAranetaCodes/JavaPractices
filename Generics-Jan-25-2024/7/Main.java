3f957bc1a9b1a09ae0ed1bc085ae5840c92781af
MoreCodes <arjunaraneta@gmail.com>
2021-03-20 22:45:08 +1100

    Java Programming Exercises

--- Generics-Jan-25-2024/7/Main.java
+++ Generics-Jan-25-2024/7/Main.java
@@ -1,31 +1,15 @@
-import java.util.concurrent.*;
-
 public class Main {
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Future<Long> future = executorService.submit(new FactorialTask(5));
-
-        // Waiting for the result
-        long result = future.get();
-        System.out.println("Factorial result: " + result);
-
-        executorService.shutdown();
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
+        printArray(intArray);    // Output: 1 2 3 4
+        printArray(doubleArray); // Output: 1.1 2.2 3.3 4.4
     }
 
-    static class FactorialTask implements Callable<Long> {
-        private int number;
-
-        FactorialTask(int number) {
-            this.number = number;
+    static <T extends Number> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
         }
-
-        @Override
-        public Long call() {
-            long factorial = 1;
-            for (int i = 1; i <= number; i++) {
-                factorial *= i;
-            }
-            return factorial;
-        }
+        System.out.println();
     }
 }
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2021-03-21" -m "Java Programming Exercises"
+git commit --date "2021-03-20" -m "Java Programming Exercises"
 git push origin
