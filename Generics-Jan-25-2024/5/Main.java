8d0e6279fc82637f7eb9f9c2ea38b0a1c190415c
MoreCodes <arjunaraneta@gmail.com>
2021-03-22 22:44:31 +1100

    Java Programming Exercises

--- Generics-Jan-25-2024/5/Main.java
+++ Generics-Jan-25-2024/5/Main.java
@@ -1,20 +1,35 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Main {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        GenericStack<Integer> stack = new GenericStack<>();
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.pop());   // Output: 2
+        System.out.println(stack.peek());  // Output: 1
+    }
+}
 
-        for (int i = 0; i < 5; i++) {
-            int taskNumber = i + 1;
-            executorService.execute(() -> processTask(taskNumber));
-        }
+class GenericStack<T> {
+    private List<T> stack = new ArrayList<>();
 
-        executorService.shutdown();
+    public void push(T item) {
+        stack.add(item);
     }
 
-    private static void processTask(int taskNumber) {
-        // Task processing logic
-        System.out.println(Thread.currentThread().getName() + ": Processing Task " + taskNumber);
+    public T pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+        return stack.get(stack.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
     }
 }
