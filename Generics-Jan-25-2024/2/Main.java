017caad98319ca8796fb890ce4eb416b9d62ffbc
MoreCodes <arjunaraneta@gmail.com>
2021-03-24 22:43:32 +1100

    Java Programming Exercises

--- Generics-Jan-25-2024/2/Main.java
+++ Generics-Jan-25-2024/2/Main.java
@@ -1,21 +1,38 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
+
+class Stack<T> {
+    private List<T> elements = new ArrayList<>();
+
+    public void push(T element) {
+        elements.add(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return elements.remove(elements.size() - 1);
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return elements.get(elements.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> buffer = new LinkedList<>();
-        Thread producer = new Thread(() -> produce(buffer));
-        Thread consumer = new Thread(() -> consume(buffer));
-
-        producer.start();
-        consumer.start();
-    }
-
-    private static void produce(Queue<Integer> buffer) {
-        // Producer logic to add elements to the buffer
-    }
-
-    private static void consume(Queue<Integer> buffer) {
-        // Consumer logic to remove elements from the buffer
+        Stack<String> stack = new Stack<>();
+        stack.push("Java");
+        stack.push("Generics");
+        System.out.println("Peek: " + stack.peek());
+        System.out.println("Pop: " + stack.pop());
     }
 }
