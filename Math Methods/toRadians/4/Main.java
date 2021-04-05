190d558fdc85649a2ffedcb5cfc38f74730738ef
MoreCodes <arjunaraneta@gmail.com>
2021-04-05 21:29:01 +1000

    Java Programming Exercises

--- Math Methods/toRadians/4/Main.java
+++ Math Methods/toRadians/4/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Radians\t\tDegrees");
-        for (double radians = 0; radians <= 2 * Math.PI; radians += Math.PI / 4) {
-            double degrees = Math.toDegrees(radians);
-            System.out.printf("%.2f\t\t%.2f\n", radians, degrees);
-        }
+        double radius = 5.0;
+        double centralAngleDegrees = 60.0;
+
+        double centralAngleRadians = Math.toRadians(centralAngleDegrees);
+        double area = 0.5 * Math.pow(radius, 2) * centralAngleRadians;
+
+        System.out.println("Area of the sector: " + area);
     }
 }
