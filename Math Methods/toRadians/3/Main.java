8de66479ae177a9e7045525ed5a4ced2154ebe15
MoreCodes <arjunaraneta@gmail.com>
2021-04-06 21:28:45 +1000

    Java Programming Exercises

--- Math Methods/toRadians/3/Main.java
+++ Math Methods/toRadians/3/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double[] radiansArray = { Math.PI / 6, Math.PI / 3, Math.PI / 2 }; // 30, 60, 90 degrees in radians
-
-        for (double radians : radiansArray) {
-            double degrees = Math.toDegrees(radians);
-            System.out.println("Angle in radians: " + radians);
-            System.out.println("Angle in degrees: " + degrees);
-            System.out.println("---");
+        for (int degrees = 0; degrees <= 180; degrees += 30) {
+            double radians = Math.toRadians(degrees);
+            System.out.println(degrees + " degrees is equal to " + radians + " radians.");
         }
     }
 }
-  
