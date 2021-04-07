a42bca31c73356dcc89f72259d4310bf35b2a890
MoreCodes <arjunaraneta@gmail.com>
2021-04-07 21:28:36 +1000

    Java Programming Exercises

--- Math Methods/toRadians/2/Main.java
+++ Math Methods/toRadians/2/Main.java
@@ -1,17 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        double angleInDegrees = 30.0;
+        double angleInRadians = Math.toRadians(angleInDegrees);
 
-        System.out.print("Enter angle in radians: ");
-        double radians = scanner.nextDouble();
+        double sineValue = Math.sin(angleInRadians);
+        double cosineValue = Math.cos(angleInRadians);
 
-        double degrees = Math.toDegrees(radians);
-        System.out.println("Angle in radians: " + radians);
-        System.out.println("Angle in degrees: " + degrees);
-
-        scanner.close();
+        System.out.println("Sine(" + angleInDegrees + " degrees) = " + sineValue);
+        System.out.println("Cosine(" + angleInDegrees + " degrees) = " + cosineValue);
     }
 }
- 
