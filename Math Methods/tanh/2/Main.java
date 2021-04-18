3b5850dc5c21fa8db41153893ef0691ec73b6b8b
MoreCodes <arjunaraneta@gmail.com>
2021-04-18 21:25:57 +1000

    Java Programming Exercises

---
+++ Math Methods/tanh/2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double angle = 45; // Specify the angle in degrees
+        double tangentValue = Math.tan(Math.toRadians(angle)); // Convert degrees to radians
+        System.out.println("Tangent of " + angle + " degrees: " + tangentValue);
+    }
+}
+    
