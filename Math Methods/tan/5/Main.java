6a9675465795b7dfcd6ff811cfa965b3ea07db06
MoreCodes <arjunaraneta@gmail.com>
2021-04-19 21:25:26 +1000

    Java Programming Exercises

--- Math Methods/tan/5/Main.java
+++ Math Methods/tan/5/Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double a = 1.0, b = -3.0, c = 2.0;
-        
-        double discriminant = b * b - 4 * a * c;
-        
-        if (discriminant >= 0) {
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            System.out.println("Roots of the quadratic equation are: " + root1 + " and " + root2);
-        } else {
-            System.out.println("The quadratic equation has complex roots.");
-        }
+        double angle = 120; // Specify the angle in degrees
+        double tangentValue = Math.tan(Math.toRadians(angle)); // Convert degrees to radians
+        System.out.println("Tangent of " + angle + " degrees: " + tangentValue);
     }
 }
-    
