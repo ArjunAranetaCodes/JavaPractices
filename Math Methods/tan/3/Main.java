0ca701b8496dc61e0aff0066ad44182053422e25
MoreCodes <arjunaraneta@gmail.com>
2021-04-21 21:24:53 +1000

    Java Programming Exercises

--- Math Methods/tan/3/Main.java
+++ Math Methods/tan/3/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double x1 = 1.0, y1 = 2.0;
-        double x2 = 4.0, y2 = 6.0;
-        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("Distance between points (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is: " + distance);
+        double angle = 60; // Specify the angle in degrees
+        double tangentValue = Math.tan(Math.toRadians(angle)); // Convert degrees to radians
+        System.out.println("Tangent of " + angle + " degrees: " + tangentValue);
     }
 }
