4880443693b8ca36da72af4bda68ce010296bb89
MoreCodes <arjunaraneta@gmail.com>
2021-04-22 21:24:37 +1000

    Java Programming Exercises

--- Math Methods/tan/2/Main.java
+++ Math Methods/tan/2/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double side1 = 3.0;
-        double side2 = 4.0;
-        double hypotenuse = Math.sqrt(side1 * side1 + side2 * side2);
-        System.out.println("Hypotenuse of a right-angled triangle with sides " + side1 + " and " + side2 + " is: " + hypotenuse);
+        double angle = 45; // Specify the angle in degrees
+        double tangentValue = Math.tan(Math.toRadians(angle)); // Convert degrees to radians
+        System.out.println("Tangent of " + angle + " degrees: " + tangentValue);
     }
 }
-  
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2021-04-23" -m "Java Programming Exercises"
+git commit --date "2021-04-22" -m "Java Programming Exercises"
 git push origin
