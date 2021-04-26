04dd3a30fb6bf6b450c5c910abd374771b4f2f6b
MoreCodes <arjunaraneta@gmail.com>
2021-04-26 21:22:49 +1000

    Java Programming Exercises

--- Math Methods/sqrt/3/Main.java
+++ Math Methods/sqrt/3/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double x = 0.0;
-        double result = Math.sinh(x);
-        System.out.println("sinh(" + x + ") = " + result);
+        double x1 = 1.0, y1 = 2.0;
+        double x2 = 4.0, y2 = 6.0;
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("Distance between points (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is: " + distance);
     }
 }
-  
