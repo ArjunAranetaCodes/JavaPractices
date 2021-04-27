cca72bb8c0025db95fc3eab76005465524375474
MoreCodes <arjunaraneta@gmail.com>
2021-04-27 21:22:35 +1000

    Java Programming Exercises

--- Math Methods/sqrt/2/Main.java
+++ Math Methods/sqrt/2/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double x = -3.0;
-        double result = Math.sinh(x);
-        System.out.println("sinh(" + x + ") = " + result);
+        double side1 = 3.0;
+        double side2 = 4.0;
+        double hypotenuse = Math.sqrt(side1 * side1 + side2 * side2);
+        System.out.println("Hypotenuse of a right-angled triangle with sides " + side1 + " and " + side2 + " is: " + hypotenuse);
     }
 }
-    
