a855abd885bb1e03d535a9d3728107822106bec9
MoreCodes <arjunaraneta@gmail.com>
2021-04-30 15:59:14 +1000

    Java Programming Exercises

--- Math Methods/sinh/4/Main.java
+++ Math Methods/sinh/4/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double[] anglesInDegrees = {0, 45, 90, 135, 180};
-        for (double angleInDegrees : anglesInDegrees) {
-            double angleInRadians = Math.toRadians(angleInDegrees);
-            double sinValue = Math.sin(angleInRadians);
-            System.out.println("Sine of " + angleInDegrees + " degrees: " + sinValue);
-        }
+        double x = Math.PI;
+        double result = Math.sinh(x);
+        System.out.println("sinh(" + x + ") = " + result);
     }
 }
-  
