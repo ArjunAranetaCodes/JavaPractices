e19b0950e0f63d2a2d91541c91eb89f6d9b85600
MoreCodes <arjunaraneta@gmail.com>
2021-05-01 15:58:46 +1000

    Java Programming Exercises

--- Math Methods/sinh/3/Main.java
+++ Math Methods/sinh/3/Main.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an angle in degrees: ");
-        double angleInDegrees = scanner.nextDouble();
-        double angleInRadians = Math.toRadians(angleInDegrees);
-        double sinValue = Math.sin(angleInRadians);
-        System.out.println("Sine of " + angleInDegrees + " degrees: " + sinValue);
+        double x = 0.0;
+        double result = Math.sinh(x);
+        System.out.println("sinh(" + x + ") = " + result);
     }
 }
-    
