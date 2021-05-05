d36d402d191743c54cf4a20057b1d3c783549078
MoreCodes <arjunaraneta@gmail.com>
2021-05-05 15:56:59 +1000

    Java Programming Exercises

--- Math Methods/sin/2/Main.java
+++ Math Methods/sin/2/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double x = 8.5;
-        double y = -3.0;
-
-        System.out.println("Comparison result using signum:");
-        System.out.println("Compare " + x + " and " + y + ": " + Math.signum(x - y));
+        double angleInRadians = 1.5;
+        double sinValue = Math.sin(angleInRadians);
+        System.out.println("Sine of " + angleInRadians + " radians: " + sinValue);
     }
 }
-  
