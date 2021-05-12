ebaae3ec9435669b808c14b154dad0135dc99d5e
MoreCodes <arjunaraneta@gmail.com>
2021-05-12 15:54:53 +1000

    Java Programming Exercises

--- Math Methods/rint/5/Main.java
+++ Math Methods/rint/5/Main.java
@@ -1,16 +1,8 @@
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
-
 public class Main {
     public static void main(String[] args) {
-        double number = 9.8765;
-        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.getDefault());
-        symbols.setDecimalSeparator('.');
-        DecimalFormat decimalFormat = new DecimalFormat("#.##", symbols);
-        String roundedNumber = decimalFormat.format(number);
-        System.out.println("Original number: " + number);
-        System.out.println("Rounded number: " + roundedNumber);
+        double input = -3.7;
+        double roundedInput = Math.rint(input);
+        System.out.println("Original Input: " + input);
+        System.out.println("Rounded Input: " + roundedInput);
     }
 }
- 
