e97737498ddb647cb0664733fa197c750415ecf5
MoreCodes <arjunaraneta@gmail.com>
2021-05-18 15:52:51 +1000

    Java Programming Exercises

--- Math Methods/round/3/Main.java
+++ Math Methods/round/3/Main.java
@@ -1,10 +1,11 @@
-import java.util.Random;
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        boolean randomBoolean = random.nextBoolean(); // Generates a random boolean value
-        System.out.println("Random Boolean: " + randomBoolean);
+        double number = 123.456789;
+        BigDecimal originalValue = new BigDecimal(number);
+        BigDecimal roundedValue = originalValue.setScale(2, BigDecimal.ROUND_HALF_UP);
+        System.out.println("Original number: " + originalValue);
+        System.out.println("Rounded number: " + roundedValue);
     }
 }
-   
