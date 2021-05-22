7e34e7ba0282e2cbf4946ac8890a25372f68102f
MoreCodes <arjunaraneta@gmail.com>
2021-05-22 15:51:44 +1000

    Java Programming Exercises

--- Math Methods/random/4/Main.java
+++ Math Methods/random/4/Main.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        double base = 4.0;
-        double exponent = 0.5;
-        double result = Math.pow(base, exponent);
-        System.out.println("Result: " + result);
+        Random random = new Random();
+        int arraySize = 5;
+        int[] randomArray = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            randomArray[i] = random.nextInt(50); // Generates random integers between 0 (inclusive) and 50 (exclusive)
+        }
+
+        System.out.print("Random Array: ");
+        for (int num : randomArray) {
+            System.out.print(num + " ");
+        }
     }
 }
-    
