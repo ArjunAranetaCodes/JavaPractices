e9cccd5443cf61613a3cba54abfde66f77b5db43
MoreCodes <arjunaraneta@gmail.com>
2021-05-23 15:51:31 +1000

    Java Programming Exercises

--- Math Methods/random/3/Main.java
+++ Math Methods/random/3/Main.java
@@ -1,9 +1,9 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        double base = 10.5;
-        double exponent = 2.0;
-        double result = Math.pow(base, exponent);
-        System.out.println("Result: " + result);
+        Random random = new Random();
+        boolean randomBoolean = random.nextBoolean(); // Generates a random boolean value
+        System.out.println("Random Boolean: " + randomBoolean);
     }
 }
-   
