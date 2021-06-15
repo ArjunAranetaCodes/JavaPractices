5ca3a8e32dba97f212a047fd7e4cb8db1120743d
MoreCodes <arjunaraneta@gmail.com>
2021-06-15 15:45:03 +1000

    Java Programming Exercises

--- Math Methods/max/5/Main.java
+++ Math Methods/max/5/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double x = -0.5;
-        double result = Math.log1p(x);
-        System.out.println("The result of log1p(" + x + ") is: " + result);
+        double num1 = 14.5, num2 = 11.2;
+        double max = Math.max(num1, num2);
+        System.out.println("Maximum value between " + num1 + " and " + num2 + ": " + max);
     }
 }
-  
