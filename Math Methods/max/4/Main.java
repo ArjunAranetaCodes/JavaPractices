e826e252348145dea6115b1e031f953ae60aefb0
MoreCodes <arjunaraneta@gmail.com>
2021-06-16 15:44:51 +1000

    Java Programming Exercises

--- Math Methods/max/4/Main.java
+++ Math Methods/max/4/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double x = 0.0;
-        double result = Math.log1p(x);
-        System.out.println("The result of log1p(" + x + ") is: " + result);
+        int num1 = 15, num2 = 9;
+        int max = (num1 > num2) ? num1 : num2;
+        System.out.println("Maximum value between " + num1 + " and " + num2 + ": " + max);
     }
 }
-     
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2021-06-17" -m "Java Programming Exercises"
+git commit --date "2021-06-16" -m "Java Programming Exercises"
 git push origin
