28e852f7c2d073dcb2e356f05864e8802c49e0be
MoreCodes <arjunaraneta@gmail.com>
2021-06-22 15:43:09 +1000

    Java Programming Exercises

--- Math Methods/log1p/3/Main.java
+++ Math Methods/log1p/3/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double x = 1000;
-        double logResult = Math.log10(x);
-        System.out.println("Log base 10 of " + x + " is: " + logResult);
+        double x = 1.0;
+        double result = Math.log1p(x);
+        System.out.println("The result of log1p(" + x + ") is: " + result);
     }
 }
