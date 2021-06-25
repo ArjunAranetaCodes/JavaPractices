b658579098556c18d744459d73df472d7df48886
MoreCodes <arjunaraneta@gmail.com>
2021-06-25 15:34:55 +1000

    Java Programming Exercises

--- Math Methods/log10/5/Main.java
+++ Math Methods/log10/5/Main.java
@@ -1,17 +1,7 @@
-import java.util.logging.Logger;
-
 public class Main {
-    private static final Logger log = Logger.getLogger(Main.class.getName());
-
     public static void main(String[] args) {
-        log.info("Starting application...");
-        performSomeTask();
-        log.info("Application completed successfully.");
-    }
-
-    private static void performSomeTask() {
-        log.info("Performing some task...");
-        // Simulating some task execution
-        log.warning("Task execution encountered a warning.");
+        double input = 1024;
+        double logBase10 = Math.log10(input);
+        System.out.println("Logarithm base 10 of " + input + " is: " + logBase10);
     }
 }
