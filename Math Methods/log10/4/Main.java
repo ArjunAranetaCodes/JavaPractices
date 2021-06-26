3fed7b3c18ef1cc3e6042a85d85d6deec2f5fe34
MoreCodes <arjunaraneta@gmail.com>
2021-06-26 15:34:44 +1000

    Java Programming Exercises

--- Math Methods/log10/4/Main.java
+++ Math Methods/log10/4/Main.java
@@ -1,11 +1,7 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
-        log.info("SLF4J and Logback logging example.");
+        double num = 500;
+        double logValue = Math.log10(num);
+        System.out.println("Logarithm base 10 of " + num + " is: " + logValue);
     }
 }
-  
