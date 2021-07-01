495c8a298f17f7e87e031f7f4a5c131f4dbe706b
MoreCodes <arjunaraneta@gmail.com>
2021-07-01 15:33:00 +1000

    Java Programming Exercises

--- Math Methods/log/4/Main.java
+++ Math Methods/log/4/Main.java
@@ -1,11 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        double dividend = 17.3;
-        double divisor = 5.6;
-        
-        double result = Math.IEEEremainder(dividend, divisor);
-        
-        System.out.println("The IEEE remainder of " + dividend + " divided by " + divisor + " is: " + result);
+        log.info("SLF4J and Logback logging example.");
     }
 }
-    
