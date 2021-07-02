a406f60f61f32f2209b495d3db78a1f891e01747
MoreCodes <arjunaraneta@gmail.com>
2021-07-02 15:32:44 +1000

    Java Programming Exercises

--- Math Methods/log/3/Main.java
+++ Math Methods/log/3/Main.java
@@ -1,10 +1,9 @@
+import org.apache.log4j.Logger;
+
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
-        double dividend = 8.9;
-        double divisor = 2.5;
-        
-        double result = Math.IEEEremainder(dividend, divisor);
-        
-        System.out.println("The IEEE remainder of " + dividend + " divided by " + divisor + " is: " + result);
+        log.info("Log4j logging example.");
     }
 }
