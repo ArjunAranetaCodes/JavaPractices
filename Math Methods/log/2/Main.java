f6506b0631fc781e135749a213de942ebf5d1c7d
MoreCodes <arjunaraneta@gmail.com>
2021-07-03 15:32:28 +1000

    Java Programming Exercises

--- Math Methods/log/2/Main.java
+++ Math Methods/log/2/Main.java
@@ -1,11 +1,12 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        double dividend = 15.8;
-        double divisor = 4.2;
-        
-        double result = Math.IEEEremainder(dividend, divisor);
-        
-        System.out.println("The IEEE remainder of " + dividend + " divided by " + divisor + " is: " + result);
+        log.info("This is an informational message.");
+        log.warning("This is a warning message.");
+        log.log(Level.SEVERE, "This is a severe message.");
     }
 }
-    
