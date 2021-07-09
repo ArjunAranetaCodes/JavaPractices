cdb5a5f6929331418176384354e547dbb547cd2f
MoreCodes <arjunaraneta@gmail.com>
2021-07-09 15:30:53 +1000

    Java Programming Exercises

---
+++ Math Methods/IEEEremainder/1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double dividend = 10.5;
+        double divisor = 3.2;
+        
+        double result = Math.IEEEremainder(dividend, divisor);
+        
+        System.out.println("The IEEE remainder of " + dividend + " divided by " + divisor + " is: " + result);
+    }
+}
---
+++ Math Methods/IEEEremainder/3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double side1 = 8.0;
+        double side2 = 15.0;
+        double hypotenuse = Math.hypot(side1, side2);
+
+        System.out.println("Hypotenuse of the right-angled triangle: " + hypotenuse);
+    }
+}
+   
