de87509415503b28e4ec53a6275ac52da5bf8fdd
MoreCodes <arjunaraneta@gmail.com>
2021-07-11 15:30:03 +1000

    Java Programming Exercises

--- Math Methods/hypot/4/Main.java
+++ Math Methods/hypot/4/Main.java
@@ -1,17 +1,9 @@
 public class Main {
-    static double calculateExponent(double base, double exponent) {
-        double result = 1;
-        for (int i = 0; i < exponent; i++) {
-            result *= base;
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        double base = 2;
-        double exponent = 3;
-        double result = calculateExponent(base, exponent);
-        System.out.println("Result: " + result);
+        double side1 = 7.0;
+        double side2 = 24.0;
+        double hypotenuse = Math.hypot(side1, side2);
+
+        System.out.println("Hypotenuse of the right-angled triangle: " + hypotenuse);
     }
 }
-  
