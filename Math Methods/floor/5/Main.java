9e5685eb8622da039095fdfa739ad902368f095f
MoreCodes <arjunaraneta@gmail.com>
2021-07-20 15:27:11 +1000

    Java Programming Exercises

--- Math Methods/floor/5/Main.java
+++ Math Methods/floor/5/Main.java
@@ -1,13 +1,15 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> expList = new ArrayList<>();
-        expList.add("Java");
-        expList.add("is");
-        expList.add("awesome");
-        
-        System.out.println("Elements in the ArrayList: " + expList);
+        int[] array = { 15, 28, 41, 53, 67 };
+
+        System.out.println("Original Array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\nFloor Values:");
+        for (int num : array) {
+            System.out.print(Math.floor(num) + " ");
+        }
     }
 }
- 
