de132475fa7c80ec61492b135af479037a5df48a
MoreCodes <arjunaraneta@gmail.com>
2021-07-21 15:27:02 +1000

    Java Programming Exercises

--- Math Methods/floor/4/Main.java
+++ Math Methods/floor/4/Main.java
@@ -1,11 +1,13 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            int result = 10 / 0; // This will cause an ArithmeticException
-            System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
-            System.out.println("Exception caught: " + e.getMessage());
-        }
+        Random random = new Random();
+
+        double randomNumber = random.nextDouble() * 100; // Generate a random double between 0 and 100
+        double floorValue = Math.floor(randomNumber);
+
+        System.out.println("Random Number: " + randomNumber);
+        System.out.println("Floor Value: " + floorValue);
     }
 }
-   
