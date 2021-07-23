9fe67ba0bad0be33ab61dfea87271c0717c1038a
MoreCodes <arjunaraneta@gmail.com>
2021-07-23 15:26:40 +1000

    Java Programming Exercises

--- Math Methods/floor/2/Main.java
+++ Math Methods/floor/2/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double x = 2.5;
-        double result = Math.exp(x);
-        System.out.println("Exponential of " + x + " is: " + result);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        double userInput = scanner.nextDouble();
+
+        double floorValue = Math.floor(userInput);
+
+        System.out.println("Entered Number: " + userInput);
+        System.out.println("Floor Value: " + floorValue);
     }
 }
-  
