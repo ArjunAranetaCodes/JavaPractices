c2eca17cefa4d14fd528b47030b8c29fc054fdbb
MoreCodes <arjunaraneta@gmail.com>
2021-07-28 15:25:04 +1000

    Java Programming Exercises

--- Math Methods/exp/2/Main.java
+++ Math Methods/exp/2/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        for (double angle = 0; angle <= 180; angle += 30) {
-            double angleInRadians = Math.toRadians(angle);
-            double cosineValue = Math.cos(angleInRadians);
-            System.out.println("Cosine value of " + angle + " degrees: " + cosineValue);
-        }
+        double x = 2.5;
+        double result = Math.exp(x);
+        System.out.println("Exponential of " + x + " is: " + result);
     }
 }
- 
