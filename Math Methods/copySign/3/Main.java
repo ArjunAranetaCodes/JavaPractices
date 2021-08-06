2cfde95e0275a499b0f418926563dbd652ec7731
MoreCodes <arjunaraneta@gmail.com>
2021-08-06 15:18:58 +1000

    Java Programming Exercises

--- Math Methods/copySign/3/Main.java
+++ Math Methods/copySign/3/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double x = 9.8;
-        double ceilValue = Math.ceil(x);
-        System.out.println("Ceil value of " + x + " is: " + ceilValue);
+        double x = -15.3;
+        double y = 8.7;
+        double result = Math.copySign(x, y);
+        System.out.println("Result: " + result);
     }
 }
-   
