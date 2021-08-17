48ccb89e515b804760200d8f01d667e636b3c4d3
MoreCodes <arjunaraneta@gmail.com>
2021-08-17 15:15:51 +1000

    Java Programming Exercises

--- Math Methods/cbrt/2/Main.java
+++ Math Methods/cbrt/2/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        double x = -2.5;
-        double y = 1.5;
-        double result = Math.atan2(y, x);
-        System.out.println("The arctangent of " + y + "/" + x + " is: " + result);
+        double num = 27.0;
+        
+        double cubeRoot = Math.cbrt(num);
+        
+        System.out.println("Cube root of " + num + " is: " + cubeRoot);
     }
 }
-   
