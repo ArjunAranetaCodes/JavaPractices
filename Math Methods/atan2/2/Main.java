a7a386ae68140ba3fd3a889a1eee89ae32bc3342
MoreCodes <arjunaraneta@gmail.com>
2021-08-22 15:14:38 +1000

    Java Programming Exercises

--- Math Methods/atan2/2/Main.java
+++ Math Methods/atan2/2/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double x = 0.8;
-        double y = 1.2;
+        double x = -2.5;
+        double y = 1.5;
         double result = Math.atan2(y, x);
         System.out.println("The arctangent of " + y + "/" + x + " is: " + result);
     }
 }
- 
