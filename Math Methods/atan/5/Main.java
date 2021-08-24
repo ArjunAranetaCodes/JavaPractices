2e131adcc7f8c6122c5f5f9b9fe1787ccc15997e
MoreCodes <arjunaraneta@gmail.com>
2021-08-24 15:13:59 +1000

    Java Programming Exercises

--- Math Methods/atan/5/Main.java
+++ Math Methods/atan/5/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        double sinValue = 0.9; // Replace with your desired sine value
-        double angle = Math.asin(sinValue);
-        System.out.println("The arcsine of sine " + sinValue + " is: " + angle);
+        double[] angles = {0.3, 0.6, 0.9};
+        for (double angle : angles) {
+            double result = Math.atan(angle);
+            System.out.println("The arctangent of " + angle + " is: " + result);
+        }
     }
 }
