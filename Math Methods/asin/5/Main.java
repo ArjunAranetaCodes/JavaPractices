1c93779fb80f03fc28d02d3fc8181db047f9e0d7
MoreCodes <arjunaraneta@gmail.com>
2021-08-29 15:12:40 +1000

    Java Programming Exercises

--- Math Methods/asin/5/Main.java
+++ Math Methods/asin/5/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double x = 0.6; // Some value for x
-        double y = Math.sqrt(1 - x*x); // Calculating y using trigonometric identity
-        double angle = Math.acos(x);
-        System.out.println("For x=" + x + ", y=" + y + ", the angle is: " + angle);
+        double sinValue = 0.9; // Replace with your desired sine value
+        double angle = Math.asin(sinValue);
+        System.out.println("The arcsine of sine " + sinValue + " is: " + angle);
     }
 }
-    
