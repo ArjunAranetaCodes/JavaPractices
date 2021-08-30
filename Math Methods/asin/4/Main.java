e1653bf67c09ca184114dd7079e185d49f93a8d7
MoreCodes <arjunaraneta@gmail.com>
2021-08-30 15:12:31 +1000

    Java Programming Exercises

--- Math Methods/asin/4/Main.java
+++ Math Methods/asin/4/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double angle = 0.3; // Input angle in radians
-        double cosineValue = Math.cos(angle);
-        double result = Math.acos(cosineValue);
-        System.out.println("Cosine of " + angle + " is: " + cosineValue);
-        System.out.println("Arccosine of cosine(" + angle + ") is: " + result);
+        double angleInRadians = 1.2; // Replace with your desired angle value
+        double arcsine = Math.asin(angleInRadians);
+        System.out.println("The arcsine of " + angleInRadians + " radians is: " + arcsine);
     }
 }
- 
