3df0c1b55c7904c2ca1fec044bff6cdad12c6dcf
MoreCodes <arjunaraneta@gmail.com>
2021-09-01 15:11:44 +1000

    Java Programming Exercises

--- Math Methods/asin/2/Main.java
+++ Math Methods/asin/2/Main.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an angle in radians: ");
-        double angle = scanner.nextDouble();
-        double result = Math.acos(angle);
-        System.out.println("The arccosine of " + angle + " is: " + result);
+        double value = 0.707; // Replace with your desired value
+        double angle = Math.asin(value);
+        System.out.println("The arcsine of " + value + " is: " + angle);
     }
 }
-   
