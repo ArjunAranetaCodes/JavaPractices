74b4e01dd0ca79b6998f5a4575cc5a4adf138ca6
MoreCodes <arjunaraneta@gmail.com>
2021-09-08 15:09:26 +1000

    Java Programming Exercises

--- Math Methods/abs/5/Main.java
+++ Math Methods/abs/5/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int intValue = 42;
-        String strValue = String.valueOf(intValue);
-        System.out.println("Converted String: " + strValue);
+        float value = -2.71828f;
+        float absoluteValue = Math.abs(value);
+        System.out.println("Absolute value of " + value + " is: " + absoluteValue);
     }
 }
-  
