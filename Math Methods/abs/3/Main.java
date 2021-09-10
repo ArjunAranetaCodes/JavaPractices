64c1eead007fc67d99ce4677b9f7d1a221e6740f
MoreCodes <arjunaraneta@gmail.com>
2021-09-10 15:08:57 +1000

    Java Programming Exercises

--- Math Methods/abs/3/Main.java
+++ Math Methods/abs/3/Main.java
@@ -1,8 +1,9 @@
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-        boolean boolValue = true;
-        String strValue = String.valueOf(boolValue);
-        System.out.println("Converted String: " + strValue);
+        BigInteger number = new BigInteger("-987654321");
+        BigInteger absoluteValue = number.abs();
+        System.out.println("Absolute value of " + number + " is: " + absoluteValue);
     }
 }
- 
