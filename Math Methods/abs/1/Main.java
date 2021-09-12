d9df791eb96a4c0e38bc9a6ac6b9318c233b4081
MoreCodes <arjunaraneta@gmail.com>
2021-09-12 15:08:25 +1000

    Java Programming Exercises

---
+++ Math Methods/abs/1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = -42;
+        int absoluteValue = Math.abs(num);
+        System.out.println("Absolute value of " + num + " is: " + absoluteValue);
+    }
+}
--- Math Methods/valueOf/1/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String strNumber = "123";
-        int intValue = Integer.valueOf(strNumber);
-        System.out.println("Converted Integer: " + intValue);
-    }
-}
-    
