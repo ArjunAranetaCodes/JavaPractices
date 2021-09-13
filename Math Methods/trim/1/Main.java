faa701180796786e34ccc27c4e16e4c0c6dbb2fc
MoreCodes <arjunaraneta@gmail.com>
2021-09-13 15:07:33 +1000

    Java Programming Exercises

--- Math Methods/trim/1/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = "   Hello, World!   ";
-        String trimmedStr = str.trim();
-        System.out.println("Original String: '" + str + "'");
-        System.out.println("Trimmed String: '" + trimmedStr + "'");
-    }
-}
