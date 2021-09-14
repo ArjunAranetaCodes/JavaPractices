cd54f6324e185244c24ce387b5fabda65afb8f84
MoreCodes <arjunaraneta@gmail.com>
2021-09-14 15:07:20 +1000

    Java Programming Exercises

--- Math Methods/toString/2/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(java.util.Arrays.toString(numbers));
-    }
-}
- 
--- Math Methods/toString/4/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("StringBuilder");
-        System.out.println(sb.toString());
-    }
-}
-  
