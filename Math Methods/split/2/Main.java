555ed7875df402934ee18835200587c86b282f03
MoreCodes <arjunaraneta@gmail.com>
2021-09-16 15:06:45 +1000

    Java Programming Exercises

--- Math Methods/split/2/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String sentence = "Java programming is fun";
-        String[] words = sentence.split(" ");
-        
-        System.out.println("Number of words: " + words.length);
-    }
-}
- 
