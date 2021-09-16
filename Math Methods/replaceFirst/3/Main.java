5e4ceb896d489f49e83f4610e9c87bc1c10079ff
MoreCodes <arjunaraneta@gmail.com>
2021-09-16 15:06:42 +1000

    Java Programming Exercises

--- Math Methods/replaceFirst/3/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String originalString = "Java is cool";
-        String replacedString = originalString.replaceFirst("(?i)java", "Python");
-        System.out.println("Original: " + originalString);
-        System.out.println("Replaced: " + replacedString);
-    }
-}
-    
--- Math Methods/replaceFirst/5/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String originalString = "apple orange apple orange";
-        String replacedString = originalString.replaceFirst("apple", "banana").replaceFirst("orange", "grape");
-        System.out.println("Original: " + originalString);
-        System.out.println("Replaced: " + replacedString);
-    }
-}
-  
