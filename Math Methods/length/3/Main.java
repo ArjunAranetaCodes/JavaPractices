a88be0d9141f2045f41416898aeae5e841b82f52
MoreCodes <arjunaraneta@gmail.com>
2021-09-17 15:06:10 +1000

    Java Programming Exercises

--- Math Methods/length/3/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int numberOfArguments = args.length;
-        System.out.println("Number of command line arguments: " + numberOfArguments);
-    }
-}
-    
--- Math Methods/length/5/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("Java is ");
-        stringBuilder.append("awesome!");
-        int builderLength = stringBuilder.length();
-        System.out.println("StringBuilder length: " + builderLength);
-    }
-}
-   
