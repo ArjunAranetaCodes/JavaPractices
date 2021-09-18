ec24d166a30f25e00012184c9fe483bf7e40ef72
MoreCodes <arjunaraneta@gmail.com>
2021-09-18 15:05:49 +1000

    Java Programming Exercises

--- Math Methods/indexOf/2/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = "Java Programming";
-        int index = str.toLowerCase().indexOf("PROGRAMMING".toLowerCase());
-        System.out.println("Case-insensitive index of 'PROGRAMMING': " + index);
-    }
-}
-  
--- Math Methods/indexOf/3/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = "Hello, Java!";
-        char targetChar = 'J';
-        int index = str.indexOf(targetChar);
-        System.out.println("Index of '" + targetChar + "': " + index);
-    }
-}
-   
