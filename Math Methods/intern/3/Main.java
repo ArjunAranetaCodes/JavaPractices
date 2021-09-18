7d93aa9d6889fba1ad0fe05a5876bfc506f3a657
MoreCodes <arjunaraneta@gmail.com>
2021-09-18 15:05:52 +1000

    Java Programming Exercises

--- Math Methods/intern/3/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str1 = "Java";
-        String str2 = new String("Java").intern();
-        
-        System.out.println(str1 == str2); // Output: true
-    }
-}
-  
--- Math Methods/intern/4/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Hello");
-        String str = sb.toString().intern();
-        
-        System.out.println(str); // Output: Hello
-    }
-}
- 
