9bf1f7dfa9e6675b2d13bc5c163090da9391e035
MoreCodes <arjunaraneta@gmail.com>
2021-09-22 15:05:01 +1000

    Java Programming Exercises

--- Math Methods/contentEquals/1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str1 = "Hello";
-        String str2 = "Hello";
-        boolean result = str1.contentEquals(str2);
-        System.out.println("Are the strings equal? " + result);
-    }
-}
-   
--- Math Methods/contentEquals/4/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        CharSequence charSeq1 = "Java";
-        CharSequence charSeq2 = new StringBuilder("Java");
-        boolean result = charSeq1.toString().contentEquals(charSeq2);
-        System.out.println("Are the CharSequences equal? " + result);
-    }
-}
