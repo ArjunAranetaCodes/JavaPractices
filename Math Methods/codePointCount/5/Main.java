f7c84bc71dfc1a97569300189e9565e206e8a5a9
MoreCodes <arjunaraneta@gmail.com>
2021-09-23 15:04:41 +1000

    Java Programming Exercises

--- Math Methods/codePointCount/5/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String unicodeText = "😃 Unicode Characters 😍";
-        int codePointsCount = unicodeText.codePointCount(0, unicodeText.length());
-
-        System.out.println("Code points count in Unicode text: " + codePointsCount);
-    }
-}
-  
