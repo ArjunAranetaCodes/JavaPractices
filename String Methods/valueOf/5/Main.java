782dc4a33e7c79cbb6f9321a1dfc6f9a84688750
MoreCodes <arjunaraneta@gmail.com>
2021-09-25 15:03:18 +1000

    Java Programming Exercises

--- String Methods/valueOf/5/Main.java
+++ String Methods/valueOf/5/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String str1 = "   Hello   ";
-        String str2 = "   World   ";
-        
-        String trimmedConcatenation = str1.trim() + " " + str2.trim();
-        
-        System.out.println("Original String 1: '" + str1 + "'");
-        System.out.println("Original String 2: '" + str2 + "'");
-        System.out.println("Trimmed Concatenation: '" + trimmedConcatenation + "'");
+        int intValue = 42;
+        String strValue = String.valueOf(intValue);
+        System.out.println("Converted String: " + strValue);
     }
 }
-  
