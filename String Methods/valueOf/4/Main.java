844d01460505e5bd2925395d2c793c344ceb10c0
MoreCodes <arjunaraneta@gmail.com>
2021-09-26 15:03:04 +1000

    Java Programming Exercises

--- String Methods/valueOf/4/Main.java
+++ String Methods/valueOf/4/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String str1 = "   Hello   ";
-        String str2 = "Hello";
-        
-        boolean isEqual = str1.trim().equals(str2.trim());
-        
-        System.out.println("Original String 1: '" + str1 + "'");
-        System.out.println("Original String 2: '" + str2 + "'");
-        System.out.println("Trimmed Strings are equal: " + isEqual);
+        float floatValue = 2.5f;
+        String strValue = String.valueOf(floatValue);
+        System.out.println("Converted String: " + strValue);
     }
 }
-  
