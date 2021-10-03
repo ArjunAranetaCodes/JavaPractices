8162bd56734a02cae0c018dae2333be4c7e8584c
MoreCodes <arjunaraneta@gmail.com>
2021-10-03 11:03:48 +1000

    Java Programming Exercises

--- String Methods/trim/2/Main.java
+++ String Methods/trim/2/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        String input = "java programming";
-        String upperCaseResult = input.toUpperCase();
-        System.out.println("Original String: " + input);
-        System.out.println("UpperCase Result: " + upperCaseResult);
+        String[] words = {"   Apple  ", "Banana  ", "   Orange"};
+        
+        System.out.println("Original Array:");
+        for (String word : words) {
+            System.out.println("'" + word + "'");
+        }
+        
+        System.out.println("\nTrimmed Array:");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].trim();
+            System.out.println("'" + words[i] + "'");
+        }
     }
 }
