acb186107ec089afce6dc39978764159275dfe77
MoreCodes <arjunaraneta@gmail.com>
2021-10-09 12:02:28 +1100

    Java Programming Exercises

---
+++ String Methods/toUpperCase/1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "hello world";
+        String upperCaseStr = str.toUpperCase();
+        System.out.println("Original String: " + str);
+        System.out.println("UpperCase String: " + upperCaseStr);
+    }
+}
---
+++ String Methods/toUpperCase/5/Main.java
@@ -0,0 +1,20 @@
+class MyClass {
+    private String name;
+
+    public MyClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClass [name=" + name + "]";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass("Object Example");
+        System.out.println(obj.toString());
+    }
+}
+    
