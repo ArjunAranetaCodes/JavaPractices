fcd0653ced1a4f21e529bf9b767bbedb046e79b0
MoreCodes <arjunaraneta@gmail.com>
2021-10-12 12:01:24 +1100

    Java Programming Exercises

--- String Methods/toString/3/Main.java
+++ String Methods/toString/3/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String str1 = "Hello";
-        String str2 = "HELLO";
-
-        if (str1.toLowerCase().equals(str2.toLowerCase())) {
-            System.out.println("The strings are equal (case-insensitive).");
-        } else {
-            System.out.println("The strings are not equal.");
-        }
+        String text = "Hello, toString!";
+        System.out.println(text.toString());
     }
 }
