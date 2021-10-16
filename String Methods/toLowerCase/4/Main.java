00f5eb5b10992abcadf21b20824d5b74226a30ef
MoreCodes <arjunaraneta@gmail.com>
2021-10-16 12:00:20 +1100

    Java Programming Exercises

--- String Methods/toLowerCase/4/Main.java
+++ String Methods/toLowerCase/4/Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        char[] charArray = {'H', 'e', 'l', 'l', 'o'};
-        String str = new String(charArray);
+        String[] words = {"Java", "Programming", "Language"};
 
-        System.out.println("Char Array: " + new String(charArray));
-        System.out.println("Converted String: " + str);
+        System.out.println("Original Array:");
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+        System.out.println("\nArray in Lowercase:");
+
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+            System.out.println(words[i]);
+        }
     }
 }
-  
