e1ad96a2eaca01134522b27d46e8f5b8ff3a1b48
MoreCodes <arjunaraneta@gmail.com>
2021-10-19 11:59:15 +1100

    Java Programming Exercises

--- String Methods/toCharArray/5/Main.java
+++ String Methods/toCharArray/5/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        String firstString = "Hello";
-        String secondString = "Hello, World";
+        String str = "Programming is Fun";
+        char[] charArray = str.toCharArray();
 
-        if (secondString.startsWith(firstString)) {
-            System.out.println("The second string starts with the first string.");
-        } else {
-            System.out.println("The second string does not start with the first string.");
+        int vowelCount = 0;
+        for (char c : charArray) {
+            if ("aeiouAEIOU".indexOf(c) != -1) {
+                vowelCount++;
+            }
         }
+
+        System.out.println("Number of vowels in \"" + str + "\": " + vowelCount);
     }
 }
-  
