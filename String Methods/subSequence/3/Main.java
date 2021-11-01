d2804af688824637bed40e544d2795a64fdfbf09
MoreCodes <arjunaraneta@gmail.com>
2021-11-01 11:02:26 +1100

    Java Programming Exercises

--- String Methods/subSequence/3/Main.java
+++ String Methods/subSequence/3/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("OpenAI is amazing!");
-        
-        // Check if the StringBuilder content starts with "Open"
-        if (stringBuilder.toString().startsWith("Open")) {
-            System.out.println("The StringBuilder content starts with 'Open'");
-        } else {
-            System.out.println("The StringBuilder content does not start with 'Open'");
-        }
+        String text = "Java is powerful";
+        CharSequence subSeq = text.subSequence(2, 8);
+        System.out.println("Subsequence: " + subSeq);
     }
 }
-    
