29c1a387056597fa6082beac9435af0ba76fee36
MoreCodes <arjunaraneta@gmail.com>
2021-11-08 11:00:21 +1100

    Java Programming Exercises

---
+++ String Methods/startsWith/1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        
+        // Check if the string starts with "Hello"
+        if (str.startsWith("Hello")) {
+            System.out.println("The string starts with 'Hello'");
+        } else {
+            System.out.println("The string does not start with 'Hello'");
+        }
+    }
+}
---
+++ String Methods/startsWith/2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String sentence = "Java programming is fun";
+        String[] words = sentence.split(" ");
+        
+        System.out.println("Number of words: " + words.length);
+    }
+}
+ 
