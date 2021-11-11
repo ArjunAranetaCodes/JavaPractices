2fc9beca441577db1ff1de8b428545d7a945c895
MoreCodes <arjunaraneta@gmail.com>
2021-11-12 10:58:57 +1100

    Java Programming Exercises

---
+++ String Methods/split/5/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String sentence = "This is a sample sentence.";
+        String replacedString = sentence.replaceAll("\\s", "_");
+        System.out.println(replacedString);
+    }
+}
+  
