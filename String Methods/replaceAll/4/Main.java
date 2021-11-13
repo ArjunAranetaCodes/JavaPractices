5238152e83dac2886e6ce85f5bf55f7e74c0deff
MoreCodes <arjunaraneta@gmail.com>
2021-11-14 10:58:11 +1100

    Java Programming Exercises

--- String Methods/replaceAll/4/Main.java
+++ String Methods/replaceAll/4/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String originalString = "Price: $20.50";
-        String replacedString = originalString.replaceFirst("\\$\\d+", "$50.00");
-        System.out.println("Original: " + originalString);
-        System.out.println("Replaced: " + replacedString);
+        String input = "12345 is a number";
+        String replacedString = input.replaceAll("\\d", "*");
+        System.out.println(replacedString);
     }
 }
-   
