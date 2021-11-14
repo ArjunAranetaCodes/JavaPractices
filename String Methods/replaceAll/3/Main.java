d9861141e624ee969b289480388f4a59d3e9350d
MoreCodes <arjunaraneta@gmail.com>
2021-11-15 10:58:01 +1100

    Java Programming Exercises

--- String Methods/replaceAll/3/Main.java
+++ String Methods/replaceAll/3/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String originalString = "Java is cool";
-        String replacedString = originalString.replaceFirst("(?i)java", "Python");
-        System.out.println("Original: " + originalString);
-        System.out.println("Replaced: " + replacedString);
+        String originalString = "Java is amazing!";
+        String replacedString = originalString.replaceAll("(?i)java", "Python");
+        System.out.println(replacedString);
     }
 }
-    
