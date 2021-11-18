7291c0cbdaf149abc1d3445c51062417a6412be9
MoreCodes <arjunaraneta@gmail.com>
2021-11-19 10:56:11 +1100

    Java Programming Exercises

--- String Methods/replaceFirst/3/Main.java
+++ String Methods/replaceFirst/3/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("This is a sample sentence.");
-        stringBuilder.replace(11, 17, "example");
-        System.out.println(stringBuilder.toString());
+        String originalString = "Java is cool";
+        String replacedString = originalString.replaceFirst("(?i)java", "Python");
+        System.out.println("Original: " + originalString);
+        System.out.println("Replaced: " + replacedString);
     }
 }
-   
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2021-11-20" -m "Java Programming Exercises"
+git commit --date "2021-11-19" -m "Java Programming Exercises"
 git push origin
