f04748df9f4361b8e9ee555b1149ec2d1e9e62f4
MoreCodes <arjunaraneta@gmail.com>
2021-11-20 10:55:55 +1100

    Java Programming Exercises

--- String Methods/replaceFirst/2/Main.java
+++ String Methods/replaceFirst/2/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String inputString = "Java is fun!";
-        char oldChar = 'a';
-        char newChar = '*';
-        String replacedString = inputString.replace(oldChar, newChar);
-        System.out.println(replacedString);
+        String originalString = "apple orange apple orange";
+        String replacedString = originalString.replaceFirst("apple", "banana");
+        System.out.println("Original: " + originalString);
+        System.out.println("Replaced: " + replacedString);
     }
 }
-   
