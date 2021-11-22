ccbb1d995fd12753b90ba5d8681e7a96892eca89
MoreCodes <arjunaraneta@gmail.com>
2021-11-23 10:54:56 +1100

    Java Programming Exercises

--- String Methods/replace/4/Main.java
+++ String Methods/replace/4/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String str1 = "Programming is fun";
-        String str2 = "Fun";
-
-        boolean result = str1.regionMatches(14, str2, 0, 3);
-
-        System.out.println("Do the regions match? " + result);
+        String text = "Replace all digits 123 with X";
+        String replacedText = text.replaceAll("\\d", "X");
+        System.out.println(replacedText);
     }
 }
-  
