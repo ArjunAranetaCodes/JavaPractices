a19b986499519385848bcc3adb8ae29a4610a884
MoreCodes <arjunaraneta@gmail.com>
2021-11-25 10:54:20 +1100

    Java Programming Exercises

--- String Methods/replace/2/Main.java
+++ String Methods/replace/2/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String str1 = "Java Programming";
-        String str2 = "Java";
-
-        boolean result = str1.regionMatches(true, 0, str2, 0, 4);
-
-        System.out.println("Do the regions (case-insensitive) match? " + result);
+        String inputString = "Java is fun!";
+        char oldChar = 'a';
+        char newChar = '*';
+        String replacedString = inputString.replace(oldChar, newChar);
+        System.out.println(replacedString);
     }
 }
