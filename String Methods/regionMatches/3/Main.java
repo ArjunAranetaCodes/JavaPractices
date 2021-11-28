13a6323e2adf4a3bd1574d4c0e8ade088ebb3f87
MoreCodes <arjunaraneta@gmail.com>
2021-11-29 00:11:49 +1100

    Java Programming Exercises

--- String Methods/regionMatches/3/Main.java
+++ String Methods/regionMatches/3/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "Coding in Java is awesome!";
-        int index = str.offsetByCodePoints(10, 3);
-        System.out.println("Offset of the fourteenth code point from the eleventh code point: " + index);
+        String str1 = "OpenAI is amazing!";
+        String str2 = "OpenAI";
+
+        boolean result = str1.regionMatches(0, str2, 0, 6);
+
+        System.out.println("Do the regions match? " + result);
     }
 }
