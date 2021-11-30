9b630b2c35e669db1ccb03047267de50c3b81194
MoreCodes <arjunaraneta@gmail.com>
2021-12-01 00:10:19 +1100

    Java Programming Exercises

--- String Methods/offsetByCodePoints/5/Main.java
+++ String Methods/offsetByCodePoints/5/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String date = "2024-01-24";
-        if (date.matches("\\d{4}-\\d{2}-\\d{2}")) {
-            System.out.println("Date format is valid.");
-        } else {
-            System.out.println("Invalid date format.");
-        }
+        String str = "Programming is fascinating!";
+        int index = str.offsetByCodePoints(0, 5);
+        System.out.println("Offset of the sixth code point from the first code point: " + index);
     }
 }
-    
+ 
