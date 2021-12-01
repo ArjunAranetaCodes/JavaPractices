388c1012895c1a5ce67ecf3b3cec36b787c051a0
MoreCodes <arjunaraneta@gmail.com>
2021-12-02 00:10:10 +1100

    Java Programming Exercises

--- String Methods/offsetByCodePoints/4/Main.java
+++ String Methods/offsetByCodePoints/4/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String numericInput = "12345";
-        if (numericInput.matches("\\d+")) {
-            System.out.println("Input is a valid numeric value.");
-        } else {
-            System.out.println("Input is not a valid numeric value.");
-        }
+        String str = "OpenAI GPT-3";
+        int index = str.offsetByCodePoints(6, -2);
+        System.out.println("Offset of the fourth code point from the fourth code point (backward): " + index);
     }
 }
- 
