f6c74b6032ca9f37cd20c0e36727db3e71110bab
MoreCodes <arjunaraneta@gmail.com>
2021-12-04 00:09:48 +1100

    Java Programming Exercises

--- String Methods/offsetByCodePoints/2/Main.java
+++ String Methods/offsetByCodePoints/2/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String email = "user@example.com";
-        if (email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
-            System.out.println("Email address is valid.");
-        } else {
-            System.out.println("Invalid email address.");
-        }
+        String str = "Java is fun!";
+        int index = str.offsetByCodePoints(5, 2);
+        System.out.println("Offset of the eighth code point from the sixth code point: " + index);
     }
 }
  
