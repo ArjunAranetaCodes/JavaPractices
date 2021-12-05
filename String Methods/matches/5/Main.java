1393af1d9ef7d92b373daaa85a3c5b6bdc6ff05d
MoreCodes <arjunaraneta@gmail.com>
2021-12-06 00:08:46 +1100

    Java Programming Exercises

--- String Methods/matches/5/Main.java
+++ String Methods/matches/5/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("Java is ");
-        stringBuilder.append("awesome!");
-        int builderLength = stringBuilder.length();
-        System.out.println("StringBuilder length: " + builderLength);
+        String date = "2024-01-24";
+        if (date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+            System.out.println("Date format is valid.");
+        } else {
+            System.out.println("Invalid date format.");
+        }
     }
 }
-   
+ 
