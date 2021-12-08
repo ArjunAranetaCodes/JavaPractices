622d72de48e99efcd3095cfb1501ebe86e3ff6a3
MoreCodes <arjunaraneta@gmail.com>
2021-12-09 00:08:12 +1100

    Java Programming Exercises

--- String Methods/matches/2/Main.java
+++ String Methods/matches/2/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello, Java!";
-        int stringLength = message.length();
-        System.out.println("String length: " + stringLength);
+        String email = "user@example.com";
+        if (email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            System.out.println("Email address is valid.");
+        } else {
+            System.out.println("Invalid email address.");
+        }
     }
 }
-   
