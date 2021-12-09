08407657e0fbce4c5241a566264364de6c815904
MoreCodes <arjunaraneta@gmail.com>
2021-12-10 00:08:00 +1100

    Java Programming Exercises

---
+++ String Methods/matches/3/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOfArguments = args.length;
+        System.out.println("Number of command line arguments: " + numberOfArguments);
+    }
+}
+    
