bd80a01a4e85de1cc4cb96121717261909694bf4
MoreCodes <arjunaraneta@gmail.com>
2021-12-15 00:06:32 +1100

    Java Programming Exercises

---
+++ String Methods/length/5/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String line = "The quick brown fox jumps over the lazy brown dog.";
+        int lastIndex = line.lastIndexOf('b', 30);
+        System.out.println("Last index of 'b' before index 30: " + lastIndex);
+    }
+}
+   
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2021-12-16" -m "Java Programming Exercises"
+git commit --date "2021-12-15" -m "Java Programming Exercises"
 git push origin
