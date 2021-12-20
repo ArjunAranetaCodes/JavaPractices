02200c54415b7c9464330ed1443660abbf87a902
MoreCodes <arjunaraneta@gmail.com>
2021-12-21 00:04:55 +1100

    Java Programming Exercises

---
+++ String Methods/lastIndexOf/2/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        
+        if (list.isEmpty()) {
+            System.out.println("List is empty");
+        } else {
+            System.out.println("List is not empty");
+        }
+    }
+}
+ 
