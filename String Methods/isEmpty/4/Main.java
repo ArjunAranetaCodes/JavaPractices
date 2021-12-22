96037d0986bd96592e23daf7b72417fdd5931812
MoreCodes <arjunaraneta@gmail.com>
2021-12-23 00:04:04 +1100

    Java Programming Exercises

--- String Methods/isEmpty/4/Main.java
+++ String Methods/isEmpty/4/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Hello");
-        String str = sb.toString().intern();
+        int[] array = new int[0];
         
-        System.out.println(str); // Output: Hello
+        if (array.length == 0) {
+            System.out.println("Array is empty");
+        } else {
+            System.out.println("Array is not empty");
+        }
     }
 }
- 
