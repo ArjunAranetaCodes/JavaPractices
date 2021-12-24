18e4123fba3094f237563dbcdfef3c841d2e6970
MoreCodes <arjunaraneta@gmail.com>
2021-12-25 00:03:45 +1100

    Java Programming Exercises

--- String Methods/isEmpty/2/Main.java
+++ String Methods/isEmpty/2/Main.java
@@ -1,17 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        String day = "Monday";
+        List<String> list = new ArrayList<>();
         
-        switch (day.intern()) {
-            case "Monday":
-                System.out.println("It's Monday!");
-                break;
-            case "Tuesday":
-                System.out.println("It's Tuesday!");
-                break;
-            default:
-                System.out.println("It's another day.");
+        if (list.isEmpty()) {
+            System.out.println("List is empty");
+        } else {
+            System.out.println("List is not empty");
         }
     }
 }
- 
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2021-12-26" -m "Java Programming Exercises"
+git commit --date "2021-12-25" -m "Java Programming Exercises"
 git push origin
