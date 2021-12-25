333e8a07504e864422a1d72dfe5e466e6ac1f5a7
MoreCodes <arjunaraneta@gmail.com>
2021-12-26 00:03:31 +1100

    Java Programming Exercises

---
+++ String Methods/isEmpty/4/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        String str = sb.toString().intern();
+        
+        System.out.println(str); // Output: Hello
+    }
+}
+ 
