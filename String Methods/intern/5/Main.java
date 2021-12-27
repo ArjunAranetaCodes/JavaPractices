412c61603ab16e544d4de0feb2816112ae507654
MoreCodes <arjunaraneta@gmail.com>
2021-12-27 23:55:33 +1100

    Java Programming Exercises

--- String Methods/intern/5/Main.java
+++ String Methods/intern/5/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "Java programming is interesting";
-        String substring = "ing";
-        int fromIndex = 7;
-        int index = str.indexOf(substring, fromIndex);
-        System.out.println("Index of '" + substring + "' after position " + fromIndex + ": " + index);
+        CustomClass obj = new CustomClass();
+        obj.printInternedString();
     }
 }
-   
+
+class CustomClass {
+    String str = new String("CustomString").intern();
+    
+    public void printInternedString() {
+        System.out.println(str); // Output: CustomString
+    }
+}
