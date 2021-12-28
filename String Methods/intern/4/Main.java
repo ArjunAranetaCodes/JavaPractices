dc3368a2c62d04ed11dca6e467d61f6a7d6dbe53
MoreCodes <arjunaraneta@gmail.com>
2021-12-28 23:55:22 +1100

    Java Programming Exercises

--- String Methods/intern/4/Main.java
+++ String Methods/intern/4/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "Java is fun and Java is powerful";
-        String target = "Java";
-        int index = str.indexOf(target, 10); // Start searching from index 10
-        System.out.println("Index of '" + target + "' (starting from index 10): " + index);
+        StringBuilder sb = new StringBuilder("Hello");
+        String str = sb.toString().intern();
+        
+        System.out.println(str); // Output: Hello
     }
 }
-   
