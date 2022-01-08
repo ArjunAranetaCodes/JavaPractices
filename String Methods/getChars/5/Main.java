3cc275604359d344c2baa9380169a7ac1356b814
MoreCodes <arjunaraneta@gmail.com>
2022-01-08 23:50:11 +1100

    Java Programming Exercises

--- String Methods/getChars/5/Main.java
+++ String Methods/getChars/5/Main.java
@@ -1,16 +1,12 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        String inputString = "Writing to File";
-        byte[] byteArray = inputString.getBytes();
-        
-        try (FileOutputStream fos = new FileOutputStream("output.txt")) {
-            fos.write(byteArray);
-            System.out.println("Byte Array written to file successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String sourceString = "Java Examples";
+        char[] targetArray = new char[7];
+
+        // Using getChars to copy characters from sourceString to targetArray
+        sourceString.getChars(5, 12, targetArray, 0);
+
+        // Displaying the content of targetArray
+        System.out.println(targetArray);
     }
 }
