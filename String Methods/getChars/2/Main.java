97ac670b75d3ff3c1024bd62e607b9e792061d21
MoreCodes <arjunaraneta@gmail.com>
2022-01-11 23:49:41 +1100

    Java Programming Exercises

--- String Methods/getChars/2/Main.java
+++ String Methods/getChars/2/Main.java
@@ -1,19 +1,12 @@
-import java.io.UnsupportedEncodingException;
-
 public class Main {
     public static void main(String[] args) {
-        String inputString = "Java Programming";
-        try {
-            byte[] byteArray = inputString.getBytes("UTF-8");
-            
-            System.out.println("Original String: " + inputString);
-            System.out.print("Byte Array (UTF-8): ");
-            for (byte b : byteArray) {
-                System.out.print(b + " ");
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        String sourceString = "Java Programming";
+        char[] targetArray = new char[10];
+
+        // Using getChars to copy characters from sourceString to targetArray
+        sourceString.getChars(5, 15, targetArray, 0);
+
+        // Displaying the content of targetArray
+        System.out.println(targetArray);
     }
 }
-   
