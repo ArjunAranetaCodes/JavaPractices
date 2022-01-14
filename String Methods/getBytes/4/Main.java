27d20fd84457ef4a98fe1a12c653e0ad10c6540b
MoreCodes <arjunaraneta@gmail.com>
2022-01-14 23:48:37 +1100

    Java Programming Exercises

--- String Methods/getBytes/4/Main.java
+++ String Methods/getBytes/4/Main.java
@@ -1,11 +1,20 @@
-import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double pi = 3.141592653589793;
-        DecimalFormat decimalFormat = new DecimalFormat("#.##");
-
-        String formattedPi = decimalFormat.format(pi);
-        System.out.println("Formatted Pi: " + formattedPi);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        
+        byte[] byteArray = inputString.getBytes();
+        
+        System.out.println("Original String: " + inputString);
+        System.out.print("Byte Array: ");
+        for (byte b : byteArray) {
+            System.out.print(b + " ");
+        }
+        
+        scanner.close();
     }
 }
