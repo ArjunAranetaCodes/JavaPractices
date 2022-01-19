340f6565c6f8e35c84d2bd4b1fb4c60ec345be6f
MoreCodes <arjunaraneta@gmail.com>
2022-01-19 23:47:21 +1100

    Java Programming Exercises

--- String Methods/format/4/Main.java
+++ String Methods/format/4/Main.java
@@ -1,21 +1,11 @@
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
-        String[] array1 = {"apple", "banana", "orange"};
-        String[] array2 = {"Apple", "Banana", "Orange"};
+        double pi = 3.141592653589793;
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
-        boolean arraysEqual = true;
-
-        for (int i = 0; i < array1.length; i++) {
-            if (!array1[i].equalsIgnoreCase(array2[i])) {
-                arraysEqual = false;
-                break;
-            }
-        }
-
-        if (arraysEqual) {
-            System.out.println("Arrays are equal (case-insensitive).");
-        } else {
-            System.out.println("Arrays are not equal.");
-        }
-    } 
+        String formattedPi = decimalFormat.format(pi);
+        System.out.println("Formatted Pi: " + formattedPi);
+    }
 }
