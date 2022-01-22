83a2efd1acfae44fc23e471672f87b7239015f4d
MoreCodes <arjunaraneta@gmail.com>
2022-01-22 23:46:42 +1100

    Java Programming Exercises

---
+++ String Methods/format/1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 25;
+
+        String formattedString = String.format("Name: %s, Age: %d", name, age);
+        System.out.println(formattedString);
+    }
+}
---
+++ String Methods/format/3/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String correctUsername = "admin";
+
+        System.out.print("Enter your username: ");
+        String enteredUsername = scanner.next();
+
+        if (correctUsername.equalsIgnoreCase(enteredUsername)) {
+            System.out.println("Login successful (case-insensitive).");
+        } else {
+            System.out.println("Incorrect username. Login failed.");
+        }
+    }
+}
+  
