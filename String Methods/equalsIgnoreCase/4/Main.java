b129da4336eb4dcfe51c06fb5433648211e201ec
MoreCodes <arjunaraneta@gmail.com>
2022-01-24 23:45:55 +1100

    Java Programming Exercises

--- String Methods/equalsIgnoreCase/4/Main.java
+++ String Methods/equalsIgnoreCase/4/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3};
-        int[] arr2 = {1, 2, 3};
-        
-        boolean result = java.util.Arrays.equals(arr1, arr2);
-        System.out.println("Are arr1 and arr2 equal? " + result);
+        String[] array1 = {"apple", "banana", "orange"};
+        String[] array2 = {"Apple", "Banana", "Orange"};
+
+        boolean arraysEqual = true;
+
+        for (int i = 0; i < array1.length; i++) {
+            if (!array1[i].equalsIgnoreCase(array2[i])) {
+                arraysEqual = false;
+                break;
+            }
+        }
+
+        if (arraysEqual) {
+            System.out.println("Arrays are equal (case-insensitive).");
+        } else {
+            System.out.println("Arrays are not equal.");
+        }
     }
 }
