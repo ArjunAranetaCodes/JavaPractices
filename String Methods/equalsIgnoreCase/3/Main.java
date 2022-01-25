7f587fdc7bfb89ccf8245f18aa33f3f7186b7b62
MoreCodes <arjunaraneta@gmail.com>
2022-01-25 23:45:42 +1100

    Java Programming Exercises

--- String Methods/equalsIgnoreCase/3/Main.java
+++ String Methods/equalsIgnoreCase/3/Main.java
@@ -1,25 +1,18 @@
-class Person {
-    String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Person person = (Person) obj;
-        return name.equals(person.name);
-    }
-}
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person("John");  
-        Person person2 = new Person("John");
-        
-        boolean result = person1.equals(person2);
-        System.out.println("Are person1 and person2 equal? " + result);
+        Scanner scanner = new Scanner(System.in);
+
+        String correctUsername = "admin";
+
+        System.out.print("Enter your username: ");
+        String enteredUsername = scanner.next();
+
+        if (correctUsername.equalsIgnoreCase(enteredUsername)) {
+            System.out.println("Login successful (case-insensitive).");
+        } else {
+            System.out.println("Incorrect username. Login failed.");
+        }
     }
 }
