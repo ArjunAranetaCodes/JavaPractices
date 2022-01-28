2f603ec1abd60bd136d82df62dfd6f129240c7e8
MoreCodes <arjunaraneta@gmail.com>
2022-01-28 23:44:30 +1100

    Java Programming Exercises

--- String Methods/equals/5/Main.java
+++ String Methods/equals/5/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String url = "https://www.example.com";
-        boolean endsWithDotCom = url.endsWith(".com");
-        System.out.println("Is the URL ending with '.com': " + endsWithDotCom);
+        String str1 = "Java";
+        String str2 = "java";
+        
+        boolean result = str1.equalsIgnoreCase(str2);
+        System.out.println("Are str1 and str2 equal (case-insensitive)? " + result);
     }
 }
-   
