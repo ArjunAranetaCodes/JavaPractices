d2d51067065f7be80f837ced90589faafc8aab3e
MoreCodes <arjunaraneta@gmail.com>
2022-01-30 23:44:09 +1100

    Java Programming Exercises

--- String Methods/equals/3/Main.java
+++ String Methods/equals/3/Main.java
@@ -1,8 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        String fileName = "document.txt";
-        boolean endsWithTxt = fileName.endsWith(".txt");
-        System.out.println("Is the file a text file: " + endsWithTxt);
+class Person {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person person = (Person) obj;
+        return name.equals(person.name);
     }
 }
-  
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        
+        boolean result = person1.equals(person2);
+        System.out.println("Are person1 and person2 equal? " + result);
+    }
+}
