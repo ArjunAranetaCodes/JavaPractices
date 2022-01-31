de90510a7147e05034f447ec154b788b7ea3b919
MoreCodes <arjunaraneta@gmail.com>
2022-01-31 23:43:59 +1100

    Java Programming Exercises

--- String Methods/equals/2/Main.java
+++ String Methods/equals/2/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String sentence = "Java programming is fun!";
-        boolean endsWithFun = sentence.endsWith("fun!");
-        System.out.println("Does the sentence end with 'fun!': " + endsWithFun);
+        Integer num1 = 5;
+        Integer num2 = 5;
+        
+        boolean result = num1.equals(num2);
+        System.out.println("Are num1 and num2 equal? " + result);
     }
 }
-  
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2022-02-01" -m "Java Programming Exercises"
+git commit --date "2022-01-31" -m "Java Programming Exercises"
 git push origin
