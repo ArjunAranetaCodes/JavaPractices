25496c4a70e03760e43b12abe961c1fd293b9111
MoreCodes <arjunaraneta@gmail.com>
2022-02-06 23:42:23 +1100

    Java Programming Exercises

---
+++ String Methods/endsWith/4/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char[] charArray = {'C', 'o', 'p', 'y', 'V', 'a', 'l', 'u', 'e', 'O', 'f'};
+        String result = String.copyValueOf(charArray, 4, 6);
+        System.out.println(result);
+    }
+}
+ 
---
+++ String Methods/endsWith/5/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char[] charArray = {'J', 'a', 'v', 'a', ' ', 'P', 'r', 'o', 'g', 'r', 'a', 'm'};
+        String result = String.copyValueOf(charArray, 0, 4);
+        System.out.println(result);
+    }
+}
+   
