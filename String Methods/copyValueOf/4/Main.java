8e1563570d937f4ae03057eb74e36ddfc3c0810b
MoreCodes <arjunaraneta@gmail.com>
2022-02-08 23:41:34 +1100

    Java Programming Exercises

--- String Methods/copyValueOf/4/Main.java
+++ String Methods/copyValueOf/4/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        CharSequence charSeq1 = "Java";
-        CharSequence charSeq2 = new StringBuilder("Java");
-        boolean result = charSeq1.toString().contentEquals(charSeq2);
-        System.out.println("Are the CharSequences equal? " + result);
+        char[] charArray = {'C', 'o', 'p', 'y', 'V', 'a', 'l', 'u', 'e', 'O', 'f'};
+        String result = String.copyValueOf(charArray, 4, 6);
+        System.out.println(result);
     }
 }
