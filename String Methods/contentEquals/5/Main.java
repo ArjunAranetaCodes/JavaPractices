d26953362a7eefb77e9f600c55c1293c2f5139d7
MoreCodes <arjunaraneta@gmail.com>
2022-02-12 23:40:20 +1100

    Java Programming Exercises

--- String Methods/contentEquals/5/Main.java
+++ String Methods/contentEquals/5/Main.java
@@ -1,27 +1,8 @@
-import java.util.HashSet;
-import java.util.Set;
-
-class Person {
-    String name;
-
-    Person(String name) {
-        this.name = name;
-    }
-} 
-  
 public class Main {
     public static void main(String[] args) {
-        Set<Person> personSet = new HashSet<>();
-        personSet.add(new Person("Alice"));
-        personSet.add(new Person("Bob"));
-        personSet.add(new Person("Charlie"));
-
-        Person targetPerson = new Person("Bob");
-
-        if (personSet.contains(targetPerson)) {
-            System.out.println("The Set contains the target person.");
-        } else {
-            System.out.println("The Set does not contain the target person.");
-        }
+        String str = "Hello, World!";
+        CharSequence subSeq = str.subSequence(0, 5);
+        boolean result = "Hello".contentEquals(subSeq);
+        System.out.println("Is the substring equal to 'Hello'? " + result);
     }
 }
