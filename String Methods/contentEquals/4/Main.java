39fcf55acab26a7f794996642c539fff062ddcf5
MoreCodes <arjunaraneta@gmail.com>
2022-02-13 23:40:12 +1100

    Java Programming Exercises

--- String Methods/contentEquals/4/Main.java
+++ String Methods/contentEquals/4/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Java Programming");
-
-        String keyword = "Programming";
-
-        if (sb.toString().contains(keyword)) {
-            System.out.println("The StringBuilder contains the keyword.");
-        } else {
-            System.out.println("The StringBuilder does not contain the keyword.");
-        }
+        CharSequence charSeq1 = "Java";
+        CharSequence charSeq2 = new StringBuilder("Java");
+        boolean result = charSeq1.toString().contentEquals(charSeq2);
+        System.out.println("Are the CharSequences equal? " + result);
     }
 }
