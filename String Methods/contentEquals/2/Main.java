ef8742a0fd733d5594982e59f8a326e7276e6feb
MoreCodes <arjunaraneta@gmail.com>
2022-02-15 23:39:43 +1100

    Java Programming Exercises

--- String Methods/contentEquals/2/Main.java
+++ String Methods/contentEquals/2/Main.java
@@ -1,18 +1,8 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Apple");
-        list.add("Banana");
-        list.add("Orange");
-
-        String keyword = "Banana";
-
-        if (list.contains(keyword)) {
-            System.out.println("The ArrayList contains the keyword.");
-        } else {
-            System.out.println("The ArrayList does not contain the keyword.");
-        }
+        StringBuilder builder1 = new StringBuilder("Java");
+        StringBuilder builder2 = new StringBuilder("Java");
+        boolean result = builder1.toString().contentEquals(builder2);
+        System.out.println("Are the StringBuilders equal? " + result);
     }
 }
