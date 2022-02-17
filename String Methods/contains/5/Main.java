86aac2145e5526da1c0429ce4a871bf9b1f5a3db
MoreCodes <arjunaraneta@gmail.com>
2022-02-17 23:38:48 +1100

    Java Programming Exercises

--- String Methods/contains/5/Main.java
+++ String Methods/contains/5/Main.java
@@ -1,13 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
-        
-        stringBuilder.append("Java ");
-        stringBuilder.append("is ");
-        stringBuilder.append("fun!");
-        
-        String result = stringBuilder.toString();
-        System.out.println(result);
+import java.util.HashSet;
+import java.util.Set;
+
+class Person {
+    String name;
+
+    Person(String name) {
+        this.name = name;
     }
 }
-  
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Person> personSet = new HashSet<>();
+        personSet.add(new Person("Alice"));
+        personSet.add(new Person("Bob"));
+        personSet.add(new Person("Charlie"));
+
+        Person targetPerson = new Person("Bob");
+
+        if (personSet.contains(targetPerson)) {
+            System.out.println("The Set contains the target person.");
+        } else {
+            System.out.println("The Set does not contain the target person.");
+        }
+    }
+}
