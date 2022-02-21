3bf361de00159af19c97bd051ad8aeb7b61549b9
MoreCodes <arjunaraneta@gmail.com>
2022-02-21 23:38:16 +1100

    Java Programming Exercises

---
+++ String Methods/contains/1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String keyword = "World";
+
+        if (str.contains(keyword)) {
+            System.out.println("The string contains the keyword.");
+        } else {
+            System.out.println("The string does not contain the keyword.");
+        }
+    }
+}
