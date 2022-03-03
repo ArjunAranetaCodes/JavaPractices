b3d108ac4ca453d1d64aad6ca103263a36137aac
MoreCodes <arjunaraneta@gmail.com>
2022-03-03 23:07:28 +1100

    Java Programming Exercises

--- String Methods/compareTo/5/Main.java
+++ String Methods/compareTo/5/Main.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String unicodeText = "😃 Unicode Characters 😍";
-        int codePointsCount = unicodeText.codePointCount(0, unicodeText.length());
-
-        System.out.println("Code points count in Unicode text: " + codePointsCount);
+        String[] names = {"Alice", "Bob", "Charlie", "David"};
+        
+        Arrays.sort(names);
+        
+        System.out.println("Sorted names: " + Arrays.toString(names));
     }
 }
-  
