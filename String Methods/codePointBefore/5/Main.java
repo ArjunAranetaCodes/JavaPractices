c868a835c9aa60f9792b413051b344bd80829db1
MoreCodes <arjunaraneta@gmail.com>
2022-03-13 23:04:16 +1100

    Java Programming Exercises

--- String Methods/codePointBefore/5/Main.java
+++ String Methods/codePointBefore/5/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "A1B";
-        for (int i = 0; i < str.length(); i++) {
-            int codePoint = str.codePointAt(i);
-            boolean isLetter = Character.isLetter(codePoint);
-            System.out.println("Code point at index " + i + " is a letter: " + isLetter);
-        }
+        String input = "Programming";
+        int charIndex = 5;
+        
+        int codePoint = input.codePointBefore(charIndex);
+        
+        System.out.println("Code point before index " + charIndex + ": " + codePoint);
     }
 }
-    
