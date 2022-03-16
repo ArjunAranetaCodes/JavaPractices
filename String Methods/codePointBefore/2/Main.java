1f0b23ab8fb7cfbf16c24330f701555ab316939d
MoreCodes <arjunaraneta@gmail.com>
2022-03-16 23:03:39 +1100

    Java Programming Exercises

--- String Methods/codePointBefore/2/Main.java
+++ String Methods/codePointBefore/2/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "Java";
-        for (int i = 0; i < str.length(); i++) {
-            int codePoint = str.codePointAt(i);
-            System.out.println("Code point at index " + i + ": " + codePoint);
-        }
+        String text = "Java Programming";
+        int position = 9;
+        
+        int codePoint = text.codePointBefore(position);
+        
+        System.out.println("Code point before position " + position + ": " + codePoint);
     }
 }
-  
