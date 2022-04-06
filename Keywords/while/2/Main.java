08bbafd706bb0fcc65b75f1f8c9f1d4415455b33
MoreCodes <arjunaraneta@gmail.com>
2022-04-06 21:30:48 +1000

    Java Programming Exercises

---
+++ Keywords/while/2/Main.java
@@ -0,0 +1,23 @@
+class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton(); 
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+class Main {  
+    public static void main(String[] args) { 
+        Singleton singleton = Singleton.getInstance();
+        System.out.println("Singleton instance created.");
+    }
+}
