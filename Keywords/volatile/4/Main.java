7eb4085484654d794a0064f84d2124f02badc2f1
MoreCodes <arjunaraneta@gmail.com>
2022-04-08 21:29:59 +1000

    Java Programming Exercises

--- Keywords/volatile/4/Main.java
+++ Keywords/volatile/4/Main.java
@@ -1,12 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            printIteration(i);
-        }
-    }
+class SharedData {
+    volatile int[] dataArray = new int[10];
+}
 
-    static void printIteration(int iteration) {
-        System.out.println("Iteration: " + iteration);
+class Main {
+    public static void main(String[] args) {
+        SharedData sharedData = new SharedData();
+
+        // Thread 1: updates array elements
+        new Thread(() -> {
+            for (int i = 0; i < sharedData.dataArray.length; i++) {
+                sharedData.dataArray[i] = i;
+            }
+        }).start();
+
+        // Thread 2: reads array elements
+        new Thread(() -> {
+            while (sharedData.dataArray[sharedData.dataArray.length - 1] == 0) {
+                // busy-wait until the array is initialized
+            }
+            for (int value : sharedData.dataArray) {
+                System.out.println(value);
+            }
+        }).start();
     }
 }
- 
