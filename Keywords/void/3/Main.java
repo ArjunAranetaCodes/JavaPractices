3d6b8b3ca7fd6cb75477c4d5e0f7b8958803839f
MoreCodes <arjunaraneta@gmail.com>
2022-04-12 21:28:15 +1000

    Java Programming Exercises

--- Keywords/void/3/Main.java
+++ Keywords/void/3/Main.java
@@ -1,11 +1,10 @@
-interface Calculator {
-    int calculate(int x, int y);
-}
-
 public class Main {
     public static void main(String[] args) {
-        Calculator addition = (var x, var y) -> x + y;
-        System.out.println("Sum: " + addition.calculate(5, 3));
+        int result = addNumbers(5, 7);
+        System.out.println("The sum is: " + result);
+    }
+
+    static int addNumbers(int a, int b) {
+        return a + b;
     }
 }
- 
