2130038ceb9787974c6a1f8e115178214c50a17e
MoreCodes <arjunaraneta@gmail.com>
2022-04-13 21:28:05 +1000

    Java Programming Exercises

--- Keywords/void/2/Main.java
+++ Keywords/void/2/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        var numbers = new int[]{1, 2, 3, 4, 5};
-        for (var number : numbers) {
-            System.out.print(number + " ");
-        }
+        greetUser("John");
+    }
+
+    static void greetUser(String name) {
+        System.out.println("Hello, " + name + "! Welcome to the void method with parameters.");
     }
 }
-   
