0dbf1e67fcd4b6e669cb7516d0c184608db902d4
MoreCodes <arjunaraneta@gmail.com>
2022-04-17 21:26:47 +1000

    Java Programming Exercises

--- Keywords/var/3/Main.java
+++ Keywords/var/3/Main.java
@@ -1,15 +1,10 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+interface Calculator {
+    int calculate(int x, int y);
+}
 
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("example.txt"))) {
-            String line = br.readLine();
-            System.out.println(line);
-        } catch (IOException e) {
-            System.out.println("Error reading the file: " + e.getMessage());
-        }
+        Calculator addition = (var x, var y) -> x + y;
+        System.out.println("Sum: " + addition.calculate(5, 3));
     }
 }
- 
