7c9bbe939943e9ce6dc8c2ad155206460213c2ea
MoreCodes <arjunaraneta@gmail.com>
2022-04-25 21:23:26 +1000

    Java Programming Exercises

--- Keywords/transient/4/Main.java
+++ Keywords/transient/4/Main.java
@@ -1,29 +1,13 @@
+import java.io.*;
+
+class MyClass implements Serializable {
+    transient static int transientStaticVariable = 10;
+    static int nonTransientStaticVariable = 20;
+}
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            throwMultipleExceptions(1);
-        } catch (CustomException | AnotherException e) {
-            System.out.println("Caught Exception: " + e.getMessage());
-        }
-    }
-
-    static void throwMultipleExceptions(int option) throws CustomException, AnotherException {
-        if (option == 1) {
-            throw new CustomException("This is a custom exception.");
-        } else {
-            throw new AnotherException("This is another exception.");
-        }
-    }
-}
-
-class CustomException extends Exception {
-    public CustomException(String message) {
-        super(message);
-    }
-}
-  
-class AnotherException extends Exception { 
-    public AnotherException(String message) { 
-        super(message);
+        System.out.println("Transient Static Variable: " + MyClass.transientStaticVariable);
+        System.out.println("Non-Transient Static Variable: " + MyClass.nonTransientStaticVariable);
     }
 }
