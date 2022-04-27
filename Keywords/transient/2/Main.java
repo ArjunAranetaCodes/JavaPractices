e2ad7c2581084160498031b77db9eb8a8e94b3bb
MoreCodes <arjunaraneta@gmail.com>
2022-04-27 21:22:54 +1000

    Java Programming Exercises

--- Keywords/transient/2/Main.java
+++ Keywords/transient/2/Main.java
@@ -1,18 +1,25 @@
+import java.io.*;
+
+class MyClass implements Serializable {
+    transient int transientVariable = 10;
+    int nonTransientVariable = 20;
+}
+
 public class Main {
-    public static void main(String[] args) {
-        try {
-            method1();
-        } catch (Exception e) {
-            System.out.println("Caught Exception: " + e.getMessage());
-        }
-    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        MyClass obj = new MyClass();
 
-    static void method1() throws Exception {
-        method2();
-    }
+        // Serialization
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data.ser"));
+        out.writeObject(obj);
+        out.close();
 
-    static void method2() throws Exception {
-        throw new Exception("Exception thrown in method2");
+        // Deserialization
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("data.ser"));
+        MyClass newObj = (MyClass) in.readObject();
+        in.close();
+
+        System.out.println("Transient Variable after Deserialization: " + newObj.transientVariable);
+        System.out.println("Non-Transient Variable after Deserialization: " + newObj.nonTransientVariable);
     }
 }
-   
