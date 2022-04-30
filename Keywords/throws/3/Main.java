b55eb6d171a691cc080b572a549a786f46854e29
MoreCodes <arjunaraneta@gmail.com>
2022-04-30 21:21:33 +1000

    Java Programming Exercises

--- Keywords/throws/3/Main.java
+++ Keywords/throws/3/Main.java
@@ -1,13 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            methodThatThrowsException();
-        } catch (Exception e) {
-            System.out.println("Caught exception: " + e.getMessage());
+            throwCheckedException();
+        } catch (CheckedException e) {
+            System.out.println("Caught CheckedException: " + e.getMessage());
         }
+
+        throwUncheckedException();
     }
 
-    private static void methodThatThrowsException() throws Exception {
-        throw new Exception("Exception thrown from method");
+    static void throwCheckedException() throws CheckedException {
+        throw new CheckedException("This is a checked exception.");
+    }
+
+    static void throwUncheckedException() {
+        throw new UncheckedException("This is an unchecked exception.");
+    }
+}
+
+class CheckedException extends Exception {
+    public CheckedException(String message) {
+        super(message);
+    }
+}
+
+class UncheckedException extends RuntimeException {
+    public UncheckedException(String message) {
+        super(message);
     }
 }
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2022-05-15" -m "Java Programming Exercises"
+git commit --date "2022-04-30" -m "Java Programming Exercises"
 git push origin
