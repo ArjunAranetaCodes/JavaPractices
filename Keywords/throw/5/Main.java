a51347893ad737b2e5001c624bd47be942d00989
MoreCodes <arjunaraneta@gmail.com>
2022-05-03 21:20:03 +1000

    Java Programming Exercises

--- Keywords/throw/5/Main.java
+++ Keywords/throw/5/Main.java
@@ -1,25 +1,9 @@
-class Main {
-    private String name;
-
-    public Main(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        Main other = (Main) obj;
-        return this.name.equals(other.name); 
-    }
-
+public class Main {
     public static void main(String[] args) {
-        Main obj1 = new Main("John");
-        Main obj2 = new Main("John");
-        System.out.println("Objects are equal: " + obj1.equals(obj2));
+        throwUncheckedException();
+    }
+
+    private static void throwUncheckedException() {
+        throw new RuntimeException("Unchecked exception thrown");
     }
 }
