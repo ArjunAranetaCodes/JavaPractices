93a76a2dbebf98a10bdf23c8fdce77fecd5d9da8
MoreCodes <arjunaraneta@gmail.com>
2022-05-10 21:18:22 +1000

    Java Programming Exercises

--- Keywords/this/2/Main.java
+++ Keywords/this/2/Main.java
@@ -1,19 +1,22 @@
-class SharedResource {
-    private Object lock = new Object();
+class Main {
+    private int x;
+    private int y;
 
-    void someMethod() {
-
-        synchronized (lock) {
-        }
-
+    public Main() {
+        this(0, 0); // Calls another constructor using "this"
     }
-}
 
-public class Main {
+    public Main(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void displayCoordinates() {
+        System.out.println("X: " + this.x + ", Y: " + this.y);
+    }
+
     public static void main(String[] args) {
-        SharedResource sharedResource = new SharedResource();
-
-        // Use synchronized block
-        sharedResource.someMethod();
+        Main point = new Main();
+        point.displayCoordinates();
     }
 }
