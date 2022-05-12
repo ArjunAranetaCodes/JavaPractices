299b8ba2b10681710895456efeb6a6d2c00fcc4c
MoreCodes <arjunaraneta@gmail.com>
2022-05-12 21:17:13 +1000

    Java Programming Exercises

--- Keywords/synchronized/5/Main.java
+++ Keywords/synchronized/5/Main.java
@@ -27,7 +27,6 @@ public class Main {
     public static void main(String[] args) {
         SharedResource sharedResource = new SharedResource();
 
-        // Use synchronized blocks with different objects
         synchronized (sharedResource) {
             sharedResource.method1();
             sharedResource.method2();
