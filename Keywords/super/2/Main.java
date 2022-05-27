4021265afcdccd961a540cd2c0ebd16d2d6c0696
MoreCodes <arjunaraneta@gmail.com>
2022-05-27 21:13:09 +1000

    Java Programming Exercises

--- Keywords/super/2/Main.java
+++ Keywords/super/2/Main.java
@@ -7,7 +7,6 @@ class Shape {
 class Circle extends Shape {
     @Override
     void draw() {
-        super.draw(); // Using super to call the overridden method of the superclass
         System.out.println("Drawing a circle");
     }
 }
