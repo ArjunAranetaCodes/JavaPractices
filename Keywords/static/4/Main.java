a7899b3e1f96739792cfd629b03acc1c0a505db2
MoreCodes <arjunaraneta@gmail.com>
2022-06-04 21:09:52 +1000

    Java Programming Exercises

--- Keywords/static/4/Main.java
+++ Keywords/static/4/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Creating an instance of a static nested class
         StaticNestedClass nestedObj = new StaticNestedClass();
         nestedObj.display();
     }
