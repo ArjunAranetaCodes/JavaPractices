a126a75bad74b818ee4e315f972bef9193b649f9
MoreCodes <arjunaraneta@gmail.com>
2022-06-08 21:08:28 +1000

    Java Programming Exercises

--- Keywords/short/5/Main.java
+++ Keywords/short/5/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 7;
-        String result = checkEvenOdd(number);
-        System.out.println(result);
-    }
-
-    static String checkEvenOdd(int num) {
-        return (num % 2 == 0) ? "Even" : "Odd";
+        short[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Array elements:");
+        for (short num : numbers) {
+            System.out.println(num);
+        }
     }
 }
-   
