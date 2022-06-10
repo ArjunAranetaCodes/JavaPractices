011b528866a3a1c242064adfc16b22cdd21efb78
MoreCodes <arjunaraneta@gmail.com>
2022-06-10 21:08:01 +1000

    Java Programming Exercises

--- Keywords/short/3/Main.java
+++ Keywords/short/3/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int factorial = calculateFactorial(5);
-        System.out.println("Factorial: " + factorial);
-    }
-
-    static int calculateFactorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        } else {
-            return n * calculateFactorial(n - 1); 
-        }
+        int intValue = 200;
+        short shortValue = (short) intValue;
+        System.out.println("Short value: " + shortValue);
     }
 }
