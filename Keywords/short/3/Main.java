837e0799cdb7c974b5d3939498b64704ec0a32e8
MoreCodes <arjunaraneta@gmail.com>
2022-06-10 21:08:04 +1000

    Java Programming Exercises

--- Keywords/short/3/Main.java
+++ Keywords/short/3/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int intValue = 200;
         short shortValue = (short) intValue;
-        System.out.println("Short value: " + shortValue);
+        System.out.println("Short value: " + shortValue); 
     }
 }
