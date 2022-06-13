0c39b90c11d411f8640d10011b37dfe8d30e9bfb
MoreCodes <arjunaraneta@gmail.com>
2022-06-13 21:06:47 +1000

    Java Programming Exercises

--- Keywords/return/5/Main.java
+++ Keywords/return/5/Main.java
@@ -1,9 +1,11 @@
-import com.example.modulec.ModuleC;
-
-public class MainApp {
+public class Main {
     public static void main(String[] args) {
-        ModuleC moduleC = new ModuleC();
-        System.out.println("MainApp running with ModuleC: " + moduleC);
+        int number = 7;
+        String result = checkEvenOdd(number);
+        System.out.println(result);
+    }
+
+    static String checkEvenOdd(int num) {
+        return (num % 2 == 0) ? "Even" : "Odd";
     }
 }
-   
