1f3836c7244154136c6c9ab39324eb70d8f57ef9
MoreCodes <arjunaraneta@gmail.com>
2022-06-14 21:06:37 +1000

    Java Programming Exercises

--- Keywords/return/4/Main.java
+++ Keywords/return/4/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main module running!");
+        String input = "Hello, World!";
+        int length = calculateStringLength(input);
+        System.out.println("String Length: " + length);
+    }
+
+    static int calculateStringLength(String str) {
+        return str.length();
     }
 }
-   
