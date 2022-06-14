5c7e0bf611981d1c79c4589f0ab5f78c2565b541
MoreCodes <arjunaraneta@gmail.com>
2022-06-14 21:06:26 +1000

    Java Programming Exercises

--- Keywords/return/3/Main.java
+++ Keywords/return/3/Main.java
@@ -1,5 +1,14 @@
-module ModuleC {
-    requires ModuleB;
-    exports com.example.modulec;
+public class Main {
+    public static void main(String[] args) {
+        int factorial = calculateFactorial(5);
+        System.out.println("Factorial: " + factorial);
+    }
+
+    static int calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
 }
-   
