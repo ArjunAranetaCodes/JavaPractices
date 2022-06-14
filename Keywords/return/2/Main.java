26792a4e3d953fdbb130e7a31aea48733f9aed0a
MoreCodes <arjunaraneta@gmail.com>
2022-06-14 21:06:14 +1000

    Java Programming Exercises

--- Keywords/return/2/Main.java
+++ Keywords/return/2/Main.java
@@ -1,4 +1,10 @@
-module ModuleB {
-    requires ModuleA;
-    exports com.example.moduleb;
+public class Main {
+    public static void main(String[] args) {
+        int maxNumber = findMax(8, 12);
+        System.out.println("Maximum Number: " + maxNumber);
+    }
+
+    static int findMax(int num1, int num2) {
+        return (num1 > num2) ? num1 : num2;
+    }
 }
