8b2cfbf515797c337d65113cf1392e18e2507ef7
MoreCodes <arjunaraneta@gmail.com>
2022-06-16 21:05:22 +1000

    Java Programming Exercises

---
+++ Keywords/requires/5/Main.java
@@ -0,0 +1,8 @@
+import com.example.modulec.ModuleC;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ModuleC moduleC = new ModuleC();
+        System.out.println("MainApp running with ModuleC: " + moduleC);
+    }
+}
