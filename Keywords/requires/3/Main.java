27e2992098604706501f770d969f0a8501c9b1d7
MoreCodes <arjunaraneta@gmail.com>
2022-06-20 21:04:14 +1000

    Java Programming Exercises

---
+++ Keywords/requires/3/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "Welcome to Java Programming!";
+        System.out.println(message);
+    }
+}
+   
---
+++ Keywords/requires/4/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("Area of the circle: " + area);
+    }
+}
+  
