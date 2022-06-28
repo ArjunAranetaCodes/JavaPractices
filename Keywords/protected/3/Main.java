69ddd228063f941fe72d732f69b7e0df75d957f1
MoreCodes <arjunaraneta@gmail.com>
2022-06-28 21:01:16 +1000

    Java Programming Exercises

--- Keywords/protected/3/Main.java
+++ Keywords/protected/3/Main.java
@@ -1,10 +1,7 @@
-class Main {
-    private Main() {
-        System.out.println("This is a private constructor.");
-    }
+public class Main {
+    protected static String protectedString = "Hello, protected world!";
 
     public static void main(String[] args) {
-        // Cannot create an instance of Main using private constructor
-        // Main obj = new Main(); // Uncommenting this line will result in a compilation error
+        System.out.println("Protected String: " + protectedString);
     }
 }
