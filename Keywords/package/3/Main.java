b70fe613b88454041cbc9456e04f7b3fd19b43ba
MoreCodes <arjunaraneta@gmail.com>
2022-07-09 19:34:55 +1000

    Java Programming Exercises

--- Keywords/package/3/Main.java
+++ Keywords/package/3/Main.java
@@ -2,6 +2,6 @@ package com.example.program3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Program 3!");
+        System.out.println("Hello from Program 3!"); 
     }
 }
