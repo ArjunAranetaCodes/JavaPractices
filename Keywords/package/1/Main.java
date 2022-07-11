7e9f3eece2a21e0ea8d5e893006504cac18b8100
MoreCodes <arjunaraneta@gmail.com>
2022-07-11 19:34:35 +1000

    Java Programming Exercises

---
+++ Keywords/package/1/Main.java
@@ -0,0 +1,7 @@
+package com.example.program1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Program 1!");
+    }
+}
---
+++ Keywords/package/3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating a new String object using the new keyword
+        String greeting = new String("Hello, World!");
+        
+        // Printing the String
+        System.out.println(greeting);
+    }
+}
