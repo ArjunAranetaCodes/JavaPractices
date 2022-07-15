834709474e383b9860d8d0e3e6fb7b408651ff3e
MoreCodes <arjunaraneta@gmail.com>
2022-07-15 19:33:03 +1000

    Java Programming Exercises

--- Keywords/new/2/Main.java
+++ Keywords/new/2/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Creating a new array and initializing it using the new keyword
         int[] numbers = new int[]{1, 2, 3, 4, 5};
         
         for (int num : numbers) {
