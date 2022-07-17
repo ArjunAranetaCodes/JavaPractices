fb52f26246382b4e21eb307015999835ec8ea8b6
MoreCodes <arjunaraneta@gmail.com>
2022-07-17 19:32:17 +1000

    Java Programming Exercises

--- Keywords/native/5/Main.java
+++
@@ -1,9 +0,0 @@
-module MyModule {
-    uses com.example.MyService;
-
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello from MyModule with services!");
-        }
-    }
-}
