d473e54261842317a5e80acfbe9c36e14767a6a6
MoreCodes <arjunaraneta@gmail.com>
2022-07-18 19:32:08 +1000

    Java Programming Exercises

--- Keywords/native/4/Main.java
+++ Keywords/native/4/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.myNativeMethod();
+        main.myNativeMethod(); 
     }
 
     static {
