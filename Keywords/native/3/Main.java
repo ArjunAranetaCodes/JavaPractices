e0bb6c274ac8822c1a87d9e9edea94e90d796e82
MoreCodes <arjunaraneta@gmail.com>
2022-07-19 19:31:45 +1000

    Java Programming Exercises

--- Keywords/native/3/Main.java
+++ Keywords/native/3/Main.java
@@ -1,9 +1,14 @@
-module MyModule {
-    exports com.example;
- 
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello from MyModule with exported com.example package!");
-        }
+public class Main {
+    // Loading a native library
+    static {
+        System.loadLibrary("MyLibrary");
+    }
+
+    // Native method declaration
+    public native void myNativeMethod();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.myNativeMethod();
     }
 }
