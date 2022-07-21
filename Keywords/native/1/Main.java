9be8a7fa4e04706fca7a61fa4d4e8d0683f87ed4
MoreCodes <arjunaraneta@gmail.com>
2022-07-21 19:31:23 +1000

    Java Programming Exercises

--- Keywords/native/1/Main.java
+++ Keywords/native/1/Main.java
@@ -2,7 +2,7 @@ public class Main {
     // Native method declaration
     public native void myNativeMethod();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Main main = new Main();
         main.myNativeMethod();
     }
