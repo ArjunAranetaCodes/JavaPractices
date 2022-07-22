b535dde9e6f65ff6f772007ec5d72983196528f3
MoreCodes <arjunaraneta@gmail.com>
2022-07-22 19:30:34 +1000

    Java Programming Exercises

--- Keywords/module/5/Main.java
+++ Keywords/module/5/Main.java
@@ -2,7 +2,6 @@
 module MyModule {
     uses com.example.MyService;
 
-    // Main class
     public class Main {
         public static void main(String[] args) {
             System.out.println("Hello from MyModule with services!");
