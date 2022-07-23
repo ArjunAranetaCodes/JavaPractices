14be91525a470975972542b8b067274bf75c8117
MoreCodes <arjunaraneta@gmail.com>
2022-07-23 19:30:22 +1000

    Java Programming Exercises

--- Keywords/module/4/Main.java
+++ Keywords/module/4/Main.java
@@ -1,6 +1,6 @@
 module MyModule {
     requires someOtherModule;
-
+ 
     public class Main {
         public static void main(String[] args) {
             System.out.println("Hello from MyModule with dependencies!");
