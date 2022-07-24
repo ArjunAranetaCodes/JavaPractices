b4f222ba4441a8e054fe3925e16ecaf6c7d84e64
MoreCodes <arjunaraneta@gmail.com>
2022-07-24 19:29:55 +1000

    Java Programming Exercises

--- Keywords/module/3/Main.java
+++ Keywords/module/3/Main.java
@@ -1,7 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        for (long i = 1L; i <= 5L; i++) {
-            System.out.println("Iteration: " + i);
+// Module declaration
+module MyModule {
+    // Package declaration with export
+    exports com.example;
+
+    // Main class
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello from MyModule with exported com.example package!");
         }
     }
 }
