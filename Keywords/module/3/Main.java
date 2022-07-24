fa4250b77cda04fb47bc2f40c7299c1feb266909
MoreCodes <arjunaraneta@gmail.com>
2022-07-24 19:29:57 +1000

    Java Programming Exercises

--- Keywords/module/3/Main.java
+++ Keywords/module/3/Main.java
@@ -3,7 +3,6 @@ module MyModule {
     // Package declaration with export
     exports com.example;
 
-    // Main class
     public class Main {
         public static void main(String[] args) {
             System.out.println("Hello from MyModule with exported com.example package!");
