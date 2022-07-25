a99977e344a908a17db092efb7971b1774d239ef
MoreCodes <arjunaraneta@gmail.com>
2022-07-25 19:29:42 +1000

    Java Programming Exercises

--- Keywords/module/2/Main.java
+++ Keywords/module/2/Main.java
@@ -2,7 +2,6 @@
 module MyModule {
     package com.example;
 
-    // Main class
     public class Main {
         public static void main(String[] args) {
             System.out.println("Hello from MyModule in com.example package!");
