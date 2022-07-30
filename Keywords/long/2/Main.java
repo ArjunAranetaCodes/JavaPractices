e4735a087c6a40e035fe3e2eaa0891244e61b6bc
MoreCodes <arjunaraneta@gmail.com>
2022-07-30 19:28:08 +1000

    Java Programming Exercises

--- Keywords/long/2/Main.java
+++ Keywords/long/2/Main.java
@@ -1,26 +1,8 @@
-// Program 2
-interface Interface1 {
-    void method1();
-}
-
-interface Interface2 {
-    void method2();
-}
-
-class MyClass implements Interface1, Interface2 {
-    public void method1() {
-        System.out.println("Implementation of method1");
-    }
-
-    public void method2() {
-        System.out.println("Implementation of method2");
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        MyClass obj = new MyClass();
-        obj.method1();
-        obj.method2();
+        long num1 = 1000000L;
+        long num2 = 500000L;
+        long result = num1 + num2;
+        System.out.println("Result of addition: " + result);
     }
 }
