3f20878ec21d3b4b0fcff706c82e3709ed0f5283
MoreCodes <arjunaraneta@gmail.com>
2022-07-31 19:28:00 +1000

    Java Programming Exercises

---
+++ Keywords/long/1/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        long myLongVariable = 123456789L;
+        System.out.println("Value of myLongVariable: " + myLongVariable);
+    }
+}
