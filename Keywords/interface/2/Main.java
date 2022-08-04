c15c0d7d7f7b54f3ea391a286581ba1c76c9ce72
MoreCodes <arjunaraneta@gmail.com>
2022-08-04 19:25:32 +1000

    Java Programming Exercises

--- Keywords/interface/2/Main.java
+++ Keywords/interface/2/Main.java
@@ -9,7 +9,6 @@ interface Interface2 {
 
 class MyClass implements Interface1, Interface2 {
     public void method1() {
-        System.out.println("Implementation of method1");
     }
 
     public void method2() {
