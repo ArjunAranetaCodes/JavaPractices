60c2ba4a31b60c3a0f60f292dda6a0bc1b1951d3
MoreCodes <arjunaraneta@gmail.com>
2022-08-07 19:24:22 +1000

    Java Programming Exercises

--- Keywords/int/4/Main.java
+++ Keywords/int/4/Main.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String text = null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
 
-        if (text instanceof String) {
-            System.out.println("The text is a String");
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
         } else {
-            System.out.println("The text is null");
+            System.out.println(num + " is an odd number.");
         }
     }
 }
