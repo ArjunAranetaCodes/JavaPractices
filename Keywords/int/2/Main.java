eb7218bd229da89cd2ad1db87fbf3e5536b8347c
MoreCodes <arjunaraneta@gmail.com>
2022-08-08 19:23:50 +1000

    Java Programming Exercises

--- Keywords/int/2/Main.java
+++ Keywords/int/2/Main.java
@@ -1,21 +1,8 @@
-interface Shape {
-    void draw();
-}
-
-class Circle implements Shape {
-    public void draw() {
-        System.out.println("Drawing a circle");
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Shape myShape = new Circle();
-
-        if (myShape instanceof Circle) {
-            System.out.println("My shape is a circle!");
-        } else {
-            System.out.println("My shape is not a circle.");
-        }
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
 }
