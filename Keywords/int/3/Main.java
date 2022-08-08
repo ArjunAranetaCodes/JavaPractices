067b5be90ac3c83fae24e4a95a932a42c6a0df9a
MoreCodes <arjunaraneta@gmail.com>
2022-08-08 19:24:00 +1000

    Java Programming Exercises

--- Keywords/int/3/Main.java
+++ Keywords/int/3/Main.java
@@ -1,15 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Object[] objects = {"Hello", 42, 3.14, new Main()};
-
-        for (Object obj : objects) {
-            if (obj instanceof String) {
-                System.out.println(obj + " is a String");
-            } else if (obj instanceof Integer) {
-                System.out.println(obj + " is an Integer");
-            } else {
-                System.out.println(obj + " is of unknown type");
-            }
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: " + userInput);
     }
 }
