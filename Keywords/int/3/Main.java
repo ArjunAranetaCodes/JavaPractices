725d071d4e524f9ada6d9be162406f3a6259ad37
MoreCodes <arjunaraneta@gmail.com>
2022-08-08 19:24:10 +1000

    Java Programming Exercises

--- Keywords/int/3/Main.java
+++ Keywords/int/3/Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: " + userInput);
     }
 }
