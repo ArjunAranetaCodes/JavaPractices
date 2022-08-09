9aaa5bf822c615790b62fb14cc1b2cca3b1cd412
MoreCodes <arjunaraneta@gmail.com>
2022-08-09 19:22:44 +1000

    Java Programming Exercises

--- Keywords/int/1/Main.java
+++ Keywords/int/1/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        int number = 10; 
         System.out.println("The value of the integer variable is: " + number);
     }
 }
