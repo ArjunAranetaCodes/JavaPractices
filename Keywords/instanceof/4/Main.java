d16e6508c647e19d11ba6bf19cf8a98030940dbf
MoreCodes <arjunaraneta@gmail.com>
2022-08-11 19:21:34 +1000

    Java Programming Exercises

--- Keywords/instanceof/4/Main.java
+++ Keywords/instanceof/4/Main.java
@@ -1,13 +1,11 @@
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Simple GUI");
-        JButton button = new JButton("Click Me");
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(button); 
-        frame.setSize(300, 200);
-        frame.setVisible(true);
+        String text = null;
+
+        if (text instanceof String) {
+            System.out.println("The text is a String");
+        } else {
+            System.out.println("The text is null");
+        }
     }
 }
