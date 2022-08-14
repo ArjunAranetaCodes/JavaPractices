b68562b935580314c1d54f863838cb664b46ed03
MoreCodes <arjunaraneta@gmail.com>
2022-08-14 19:20:44 +1000

    Java Programming Exercises

--- Keywords/instanceof/1/Main.java
+++ Keywords/instanceof/1/Main.java
@@ -6,9 +6,12 @@ class Dog extends Animal {
 
 public class Main {
     public static void main(String[] args) {
+        Animal myPet = new Dog();
 
         if (myPet instanceof Dog) {
+            System.out.println("My pet is a dog!");
         } else {
+            System.out.println("My pet is not a dog.");
         }
     }
 }
