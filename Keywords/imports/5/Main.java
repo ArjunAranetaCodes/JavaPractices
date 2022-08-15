6ff33d163dda613b7eb15c31ddef15f1e0c3f081
MoreCodes <arjunaraneta@gmail.com>
2022-08-15 19:19:28 +1000

    Java Programming Exercises

--- Keywords/imports/5/Main.java
+++ Keywords/imports/5/Main.java
@@ -1,24 +1,38 @@
-interface MyInterface2 {
-    void method2();
-}
-
-abstract class AbstractClass implements MyInterface2 {
-    void commonMethod() {
-        System.out.println("Common method in abstract class");
-    }
-}
-
-class ConcreteClass extends AbstractClass {
-    @Override
-    public void method2() {
-        System.out.println("Implementing MyInterface2"); 
-    }
-}
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        ConcreteClass obj = new ConcreteClass(); 
-        obj.commonMethod(); 
-        obj.method2();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 5; i++) {
+            Runnable worker = new WorkerThread("Task " + i);
+            executorService.execute(worker);
+        }
+
+        executorService.shutdown();
+        while (!executorService.isTerminated()) {
+        }
+
+        System.out.println("All tasks completed.");
+    }
+}
+
+class WorkerThread implements Runnable {
+    private String taskName;
+
+    public WorkerThread(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " is executing " + taskName);
+        // Simulate some task execution
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
