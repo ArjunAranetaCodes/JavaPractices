2145bee8cb304e6f1539eb563df376eefb5f4387
MoreCodes <arjunaraneta@gmail.com>
2022-08-22 19:17:20 +1000

    Java Programming Exercises

--- Keywords/implements/3/Main.java
+++ Keywords/implements/3/Main.java
@@ -1,15 +1,24 @@
+class MyBaseClass {
+    void baseMethod() {
+        System.out.println("Base class method");
+    }
+}
+
+interface MyExtendedInterface {
+    void extendedMethod();
+}
+
+class CombinedClass extends MyBaseClass implements MyExtendedInterface {
+    @Override
+    public void extendedMethod() {
+        System.out.println("Implementing MyExtendedInterface");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        int num1 = 5, num2 = 10;
-
-        if (num1 > 0) {
-            if (num2 > 0) {
-                System.out.println("Both numbers are positive.");
-            } else {
-                System.out.println("Num1 is positive, but num2 is non-positive.");
-            }
-        } else {
-            System.out.println("Num1 is non-positive."); 
-        }
+        CombinedClass obj = new CombinedClass(); 
+        obj.baseMethod();
+        obj.extendedMethod();
     }
 }
