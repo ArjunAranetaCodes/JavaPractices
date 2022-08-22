0837133f1b0b5e877cb4a5dc422b7306bb6f946f
MoreCodes <arjunaraneta@gmail.com>
2022-08-22 19:17:36 +1000

    Java Programming Exercises

--- Keywords/implements/5/Main.java
+++ Keywords/implements/5/Main.java
@@ -1,8 +1,24 @@
+interface MyInterface2 {
+    void method2();
+}
+
+abstract class AbstractClass implements MyInterface2 {
+    void commonMethod() {
+        System.out.println("Common method in abstract class");
+    }
+}
+
+class ConcreteClass extends AbstractClass {
+    @Override
+    public void method2() {
+        System.out.println("Implementing MyInterface2");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        int number = 8;
-        String result = (number % 2 == 0) ? "Even" : "Odd";
-
-        System.out.println("The number is " + result + "."); 
+        ConcreteClass obj = new ConcreteClass(); 
+        obj.commonMethod();
+        obj.method2();
     }
 }
