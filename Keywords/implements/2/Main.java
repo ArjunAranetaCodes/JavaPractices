861ee5c1992d2ecc111ed303d69bf71bf751a4c8
MoreCodes <arjunaraneta@gmail.com>
2022-08-23 19:17:09 +1000

    Java Programming Exercises

--- Keywords/implements/2/Main.java
+++ Keywords/implements/2/Main.java
@@ -1,11 +1,27 @@
+interface Interface1 {
+    void method1();
+}
+
+interface Interface2 {
+    void method2();
+}
+
+class MultiInterfaceClass implements Interface1, Interface2 {
+    @Override
+    public void method1() {
+        System.out.println("Implementing Interface1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Implementing Interface2");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        int number = -5;
-
-        if (number > 0) {
-            System.out.println("The number is positive.");
-        } else {
-            System.out.println("The number is non-positive.");
-        }
+        MultiInterfaceClass obj = new MultiInterfaceClass();
+        obj.method1();
+        obj.method2();
     }
 }
