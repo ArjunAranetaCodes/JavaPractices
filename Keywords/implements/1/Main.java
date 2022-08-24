365359afbe9773ed38522220a483ced7ef8b1869
MoreCodes <arjunaraneta@gmail.com>
2022-08-24 19:16:59 +1000

    Java Programming Exercises

---
+++ Keywords/implements/1/Main.java
@@ -0,0 +1,17 @@
+interface MyInterface {
+    void myMethod();
+}
+
+class MyClass implements MyInterface {
+    @Override
+    public void myMethod() {
+        System.out.println("Implementing MyInterface");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.myMethod();
+    }
+}
