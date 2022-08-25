f914e2b1e689f262e5a3767e0382891ba0a0e1d0
MoreCodes <arjunaraneta@gmail.com>
2022-08-25 19:16:23 +1000

    Java Programming Exercises

--- Keywords/if/5/Main.java
+++ Keywords/if/5/Main.java
@@ -1,24 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int number = 8;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
 
-        System.out.print("Enter the number of terms in Fibonacci series: ");
-        int n = scanner.nextInt();
-
-        int firstTerm = 0, secondTerm = 1;
-
-        System.out.print("Fibonacci Series: " + firstTerm + ", " + secondTerm);
-
-        for (int i = 2; i < n; ++i) {
-            int nextTerm = firstTerm + secondTerm;
-            System.out.print(", " + nextTerm);  
-
-            firstTerm = secondTerm;  
-            secondTerm = nextTerm;
-        }
-
-        System.out.println();
+        System.out.println("The number is " + result + ".");
     }
 }
