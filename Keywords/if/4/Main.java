ed013bcb080819c450770ed19586bfcd246047ec
MoreCodes <arjunaraneta@gmail.com>
2022-08-26 19:16:13 +1000

    Java Programming Exercises

--- Keywords/if/4/Main.java
+++ Keywords/if/4/Main.java
@@ -6,7 +6,7 @@ public class Main {
             System.out.println("Grade: A");
         } else if (score >= 80) {
             System.out.println("Grade: B"); 
-        } else if (score >= 70) {
+        } else if (score >= 70) { 
             System.out.println("Grade: C");
         } else { 
             System.out.println("Grade: D"); 
