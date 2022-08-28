7800cc2fb66ba834f78db9c5d1c8caeb82c6ac88
MoreCodes <arjunaraneta@gmail.com>
2022-08-28 19:15:44 +1000

    Java Programming Exercises

--- Keywords/if/2/Main.java
+++ Keywords/if/2/Main.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int number = -5;
 
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
-
-        int sum = num1 + num2;
-
-        System.out.println("Sum: " + sum);
-    } 
-} 
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is non-positive.");
+        }
+    }
+}
