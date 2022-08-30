c398ac4925ea0172be407cf212f3fed727ab7b24
MoreCodes <arjunaraneta@gmail.com>
2022-08-30 19:14:55 +1000

    Java Programming Exercises

--- Keywords/goto/5/Main.java
+++ Keywords/goto/5/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String original = "Hello";
-        System.out.print("Reversed string: ");
-        for (int i = original.length() - 1; i >= 0; i--) {
-            System.out.print(original.charAt(i));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms in Fibonacci series: ");
+        int n = scanner.nextInt();
+
+        int firstTerm = 0, secondTerm = 1;
+
+        System.out.print("Fibonacci Series: " + firstTerm + ", " + secondTerm);
+
+        for (int i = 2; i < n; ++i) {
+            int nextTerm = firstTerm + secondTerm;
+            System.out.print(", " + nextTerm);
+
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
         }
+
+        System.out.println();
     }
 }
