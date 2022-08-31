e4b9e425dd18816f709817dc1f25edb0a07efc1a
MoreCodes <arjunaraneta@gmail.com>
2022-08-31 19:14:40 +1000

    Java Programming Exercises

--- Keywords/goto/4/Main.java
+++ Keywords/goto/4/Main.java
@@ -1,8 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Even numbers from 1 to 20:");
-        for (int i = 2; i <= 20; i += 2) {
-            System.out.println(i);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
         }
+
+        System.out.println("Result: " + result);
     }
 }
