51581c7a24adc41de988a0e0dba5fcf446577679
MoreCodes <arjunaraneta@gmail.com>
2022-09-04 19:12:49 +1000

    Java Programming Exercises

--- Keywords/for/5/Main.java
+++ Keywords/for/5/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        String original = "Hello";
         for (int i = original.length() - 1; i >= 0; i--) {
             System.out.print(original.charAt(i));
         }
