fd42a0c18ae25625b5d0ccb1d30086e803d2fbee
MoreCodes <arjunaraneta@gmail.com>
2022-09-05 19:12:33 +1000

    Java Programming Exercises

--- Keywords/for/4/Main.java
+++ Keywords/for/4/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        float[] floatArray = { 1.2f, 3.5f, 4.8f, 2.7f, 6.0f };
-
-        System.out.println("Float Array Elements:");
-        for (float element : floatArray) {
-            System.out.println(element);
+        System.out.println("Even numbers from 1 to 20:");
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
         }
     }
 }
