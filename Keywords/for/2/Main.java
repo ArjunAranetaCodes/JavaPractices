8f334269054571971cc705e869b460bc4d3867d1
MoreCodes <arjunaraneta@gmail.com>
2022-09-07 19:12:02 +1000

    Java Programming Exercises

--- Keywords/for/2/Main.java
+++ Keywords/for/2/Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        float num1 = 5.5f;
-        float num2 = 2.0f;
-
-        float sum = num1 + num2;
-        float difference = num1 - num2;
-        float product = num1 * num2;
-        float quotient = num1 / num2;
-
-        System.out.println("Sum: " + sum);
-        System.out.println("Difference: " + difference);
-        System.out.println("Product: " + product);
-        System.out.println("Quotient: " + quotient);
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of numbers from 1 to 10: " + sum);
     }
 }
