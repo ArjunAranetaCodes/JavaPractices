db3565491ddee039bb9c7d332f581ba00cee1200
MoreCodes <arjunaraneta@gmail.com>
2022-09-08 19:11:53 +1000

    Java Programming Exercises

--- Keywords/for/1/Main.java
+++ Keywords/for/1/Main.java
@@ -1,1 +1,7 @@
-Keywords/float
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration " + i);
+        }
+    }
+}
