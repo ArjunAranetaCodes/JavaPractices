2a52de01d6484dc9de7a933fb385e89a15832f8e
MoreCodes <arjunaraneta@gmail.com>
2022-09-08 19:11:50 +1000

    Java Programming Exercises

---
+++ Keywords/for/2/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        float num1 = 5.5f;
+        float num2 = 2.0f;
+
+        float sum = num1 + num2;
+        float difference = num1 - num2;
+        float product = num1 * num2;
+        float quotient = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
---
+++ Keywords/for/3/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        float temperature = 27.5f;
+
+        if (temperature > 30.0f) {
+            System.out.println("It's a hot day!");
+        } else {
+            System.out.println("The weather is pleasant.");
+        }
+    }
+}
