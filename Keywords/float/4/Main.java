483755a3e8d2e47b5e0f77fcc38f95875ce846e1
MoreCodes <arjunaraneta@gmail.com>
2022-09-10 19:10:56 +1000

    Java Programming Exercises

--- Keywords/float/4/Main.java
+++ Keywords/float/4/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        float[] floatArray = { 1.2f, 3.5f, 4.8f, 2.7f, 6.0f };
 
         System.out.println("Float Array Elements:");
         for (float element : floatArray) {
