e42760bc364639cd8b423d1cdb5535d164d3c98a
MoreCodes <arjunaraneta@gmail.com>
2022-09-11 19:10:33 +1000

    Java Programming Exercises

--- Keywords/float/3/Main.java
+++ Keywords/float/3/Main.java
@@ -1,17 +1,11 @@
-import java.io.FileReader;
-import java.io.IOException;
-
-class Main {
+public class Main {
     public static void main(String[] args) {
-        try (FileReader reader = new FileReader("sample.txt")) {
-            // Code that may throw an IOException
-            char[] buffer = new char[10];
-            reader.read(buffer);
-            System.out.println("Data read: " + new String(buffer));
-        } catch (IOException e) {
-            System.err.println("IOException caught: " + e.getMessage());
-        } finally {
-            System.out.println("Finally block executed.");
+        float temperature = 27.5f;
+
+        if (temperature > 30.0f) {
+            System.out.println("It's a hot day!");
+        } else {
+            System.out.println("The weather is pleasant.");
         }
     }
 }
