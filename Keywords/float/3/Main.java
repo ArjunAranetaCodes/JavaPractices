1d8cfc39b670a7daf8bbda39448d351f0ccc2a3f
MoreCodes <arjunaraneta@gmail.com>
2022-09-11 19:10:40 +1000

    Java Programming Exercises

--- Keywords/float/3/Main.java
+++ Keywords/float/3/Main.java
@@ -3,7 +3,9 @@ public class Main {
         float temperature = 27.5f;
 
         if (temperature > 30.0f) {
+            System.out.println("It's a hot day!");
         } else {
+            System.out.println("The weather is pleasant.");
         }
     }
 }
