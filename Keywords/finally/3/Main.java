2c4e1460de98f57f5d7602ecd4edd15d5e236462
MoreCodes <arjunaraneta@gmail.com>
2022-09-16 19:08:54 +1000

    Java Programming Exercises

--- Keywords/finally/3/Main.java
+++ Keywords/finally/3/Main.java
@@ -1,6 +1,17 @@
-final class FinalClass {
-    // Some code here
-}
+import java.io.FileReader;
+import java.io.IOException;
 
-// This will result in a compilation error as a final class cannot be extended
-// class SubClass extends FinalClass {}
+class Main {
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("sample.txt")) {
+            // Code that may throw an IOException
+            char[] buffer = new char[10];
+            reader.read(buffer);
+            System.out.println("Data read: " + new String(buffer));
+        } catch (IOException e) {
+            System.err.println("IOException caught: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block executed.");
+        }
+    }
+}
