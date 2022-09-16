f93a1dbdae07f06786198ee0ade44b4483e094be
MoreCodes <arjunaraneta@gmail.com>
2022-09-16 19:08:57 +1000

    Java Programming Exercises

--- Keywords/finally/3/Main.java
+++ Keywords/finally/3/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 class Main {
     public static void main(String[] args) {
         try (FileReader reader = new FileReader("sample.txt")) {
-            // Code that may throw an IOException
             char[] buffer = new char[10];
             reader.read(buffer);
             System.out.println("Data read: " + new String(buffer));
