12a8a3d417706ce05b8ee96a31f7a2ed491768de
MoreCodes <arjunaraneta@gmail.com>
2022-09-17 19:08:43 +1000

    Java Programming Exercises

--- Keywords/finally/2/Main.java
+++ Keywords/finally/2/Main.java
@@ -5,7 +5,6 @@ class Main {
 
     static int divideNumbers() {
         try {
-            // Code that may throw an exception
             return 10 / 0;
         } catch (ArithmeticException e) {
             System.err.println("Exception caught: " + e.getMessage());
