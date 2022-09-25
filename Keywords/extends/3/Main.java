60f75d37ae2fa9e55f518907509b2a04e1a2d589
MoreCodes <arjunaraneta@gmail.com>
2022-09-25 19:05:40 +1000

    Java Programming Exercises

--- Keywords/extends/3/Main.java
+++ Keywords/extends/3/Main.java
@@ -1,8 +1,24 @@
-// Main.java
-package com.example.mymodule;
+class Box<T> {
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
+
+class StringBox extends Box<String> {
+    public StringBox(String value) {
+        super(value);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Main class!");
+        StringBox myStringBox = new StringBox("Hello, Generics!");
+        System.out.println("Value in the box: " + myStringBox.getValue());
     }
 }
