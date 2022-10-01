4d3eff75d1b948d23f4786060fed62dc07461f79
MoreCodes <arjunaraneta@gmail.com>
2022-10-01 18:52:41 +1000

    Java Programming Exercises

--- Keywords/exports/4/Main.java
+++ Keywords/exports/4/Main.java
@@ -1,27 +1,8 @@
-// Program 4
+// Main.java
+package com.example;
+
 public class Main {
-    enum Direction {
-        NORTH, SOUTH, EAST, WEST
-    }
-
     public static void main(String[] args) {
-        Direction myDirection = Direction.EAST;
-
-        switch (myDirection) {
-            case NORTH:
-                System.out.println("Heading North");
-                break;
-            case SOUTH:
-                System.out.println("Heading South");
-                break;
-            case EAST:
-                System.out.println("Heading East");
-                break;
-            case WEST:
-                System.out.println("Heading West");
-                break;
-            default:
-                System.out.println("Invalid direction");
-        }
+        System.out.println("Hello from Main class!");
     }
 }
