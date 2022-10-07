9d8fb0ee8cd8c19c59796d89d5c4d820d217da2d
MoreCodes <arjunaraneta@gmail.com>
2022-10-07 19:49:59 +1100

    Java Programming Exercises

--- Keywords/enum/4/Main.java
+++ Keywords/enum/4/Main.java
@@ -1,9 +1,27 @@
+// Program 4
 public class Main {
-    public static void main(String[] args) {
-        int x = 10;
-        int y = 20;
+    enum Direction {
+        NORTH, SOUTH, EAST, WEST
+    }
 
-        String result = (x > y) ? "x is greater" : "y is greater or equal";
-        System.out.println(result);
+    public static void main(String[] args) {
+        Direction myDirection = Direction.EAST;
+
+        switch (myDirection) {
+            case NORTH:
+                System.out.println("Heading North");
+                break;
+            case SOUTH:
+                System.out.println("Heading South");
+                break;
+            case EAST:
+                System.out.println("Heading East");
+                break;
+            case WEST:
+                System.out.println("Heading West");
+                break;
+            default:
+                System.out.println("Invalid direction");
+        }
     }
 }
