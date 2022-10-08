a7772fa1e35d1cf8c125a7031b16d484a06dca96
MoreCodes <arjunaraneta@gmail.com>
2022-10-08 19:49:45 +1100

    Java Programming Exercises

--- Keywords/enum/3/Main.java
+++ Keywords/enum/3/Main.java
@@ -1,15 +1,35 @@
+// Program 3
 public class Main {
-    public static void main(String[] args) {
-        int marks = 75;
+    enum Planet {
+        MERCURY(3.303e+23, 2.4397e6),
+        VENUS(4.869e+24, 6.0518e6),
+        EARTH(5.976e+24, 6.3781e6);
 
-        if (marks >= 90) {
-            System.out.println("Grade A");
-        } else if (marks >= 80) {
-            System.out.println("Grade B");
-        } else if (marks >= 70) {
-            System.out.println("Grade C");
-        } else {
-            System.out.println("Grade F");
+        private final double mass;   // in kilograms
+        private final double radius; // in meters
+
+        Planet(double mass, double radius) {
+            this.mass = mass;
+            this.radius = radius;
         }
+
+        public double getMass() {
+            return mass;
+        }
+
+        public double getRadius() {
+            return radius;
+        }
+
+        public double surfaceGravity() {
+            return 6.67300e-11 * mass / (radius * radius);
+        }
+    }
+
+    public static void main(String[] args) {
+        Planet earth = Planet.EARTH;
+        System.out.println("Mass: " + earth.getMass() + " kg");
+        System.out.println("Radius: " + earth.getRadius() + " m");
+        System.out.println("Surface Gravity: " + earth.surfaceGravity() + " N/kg");
     }
 }
