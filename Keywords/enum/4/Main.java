3d5f1dcf077ebebc0338e8584c6263de1fa1aed5
MoreCodes <arjunaraneta@gmail.com>
2022-10-09 19:49:12 +1100

    Java Programming Exercises

---
+++ Keywords/enum/4/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        String result = (x > y) ? "x is greater" : "y is greater or equal";
+        System.out.println(result);
+    }
+}
