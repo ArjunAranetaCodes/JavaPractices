4343caf09cc75da02eb773db4b35e53a36cf93b3
MoreCodes <arjunaraneta@gmail.com>
2022-10-10 19:48:22 +1100

    Java Programming Exercises

--- Keywords/else/4/Main.java
+++ Keywords/else/4/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
 
         String result = (x > y) ? "x is greater" : "y is greater or equal";
         System.out.println(result);
