2586313acc2e2b6405ff38c3cc940aa47d9f1f29
MoreCodes <arjunaraneta@gmail.com>
2022-10-13 19:47:26 +1100

    Java Programming Exercises

--- Keywords/else/1/Main.java
+++ Keywords/else/1/Main.java
@@ -5,7 +5,6 @@ public class Main {
         if (number % 2 == 0) {
             System.out.println("Even number");
         } else {
-            System.out.println("Odd number");
         }
     }
 }
