7b9f8fb328ac9aeee13a16580a88d5152c39c0f9
MoreCodes <arjunaraneta@gmail.com>
2022-10-14 19:46:48 +1100

    Java Programming Exercises

--- Keywords/do/5/Main.java
+++ Keywords/do/5/Main.java
@@ -1,30 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-        do {
-            System.out.println("1. Print Hello");
-            System.out.println("2. Print Goodbye");
-            System.out.println("3. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
+        int integerValue = 10;
 
-            switch (choice) {
-                case 1:
-                    System.out.println("Hello!");
-                    break;
-                case 2:
-                    System.out.println("Goodbye!");
-                    break;
-                case 3:
-                    System.out.println("Exiting program.");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Try again.");
-            }
-        } while (choice != 3);
-        scanner.close();
+        double doubleValue = (double) integerValue;
+
+        System.out.println("Integer value: " + integerValue);
+        System.out.println("Converted to double: " + doubleValue);
     }
 }
