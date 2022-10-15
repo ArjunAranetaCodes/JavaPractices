622ab8c8bc209a694ae3e86d5dcc6154dfaca8a4
MoreCodes <arjunaraneta@gmail.com>
2022-10-15 19:46:34 +1100

    Java Programming Exercises

--- Keywords/do/4/Main.java
+++ Keywords/do/4/Main.java
@@ -5,7 +5,6 @@ public class Main {
         double area = Math.PI * Math.pow(radius, 2);
         double sqrtValue = Math.sqrt(16.0);
 
-        System.out.println("Area of the circle: " + area);
         System.out.println("Square root of 16: " + sqrtValue);
     }
 }
