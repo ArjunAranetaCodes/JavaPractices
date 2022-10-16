d1a5ef7957653814f00c15c8272e1fc2b6adb9b4
MoreCodes <arjunaraneta@gmail.com>
2022-10-16 19:46:22 +1100

    Java Programming Exercises

--- Keywords/do/3/Main.java
+++ Keywords/do/3/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         double[] numbers = { 2.5, 4.8, 7.2, 1.6, 9.3 };
 
+        System.out.println("Printing double array elements:");
         for (double num : numbers) {
+            System.out.println(num);
         }
     }
 }
