2a48f7cedc176de4e12d1d5583b8e1696323d565
MoreCodes <arjunaraneta@gmail.com>
2022-10-16 19:46:14 +1100

    Java Programming Exercises

--- Keywords/do/3/Main.java
+++ Keywords/do/3/Main.java
@@ -1,16 +1,10 @@
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        int sum = 0;
-        int i = 0;
-        do {
-            int randomNumber = random.nextInt(10);
-            System.out.println("Random Number: " + randomNumber);
-            sum += randomNumber;
-            i++;
-        } while (i < 5);
-        System.out.println("Sum of random numbers: " + sum); 
+        double[] numbers = { 2.5, 4.8, 7.2, 1.6, 9.3 };
+
+        System.out.println("Printing double array elements:");
+        for (double num : numbers) {
+            System.out.println(num);
+        }
     }
 }
