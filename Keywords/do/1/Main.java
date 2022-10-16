e2ad5ae7cdc26acd4cd068c4840a4ffbc35d9835
MoreCodes <arjunaraneta@gmail.com>
2022-10-16 19:45:53 +1100

    Java Programming Exercises

--- Keywords/do/1/Main.java
+++ Keywords/do/1/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        do { 
-            System.out.println("Iteration: " + i);
-            i++;
-        } while (i <= 5);
+        double myDouble = 3.14;
+        System.out.println("Value of myDouble: " + myDouble);
     }
 }
