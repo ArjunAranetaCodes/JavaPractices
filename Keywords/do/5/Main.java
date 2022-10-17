fcc02a7e9b330b97de59aace6c6a4d6c4624f766
MoreCodes <arjunaraneta@gmail.com>
2022-10-17 19:45:18 +1100

    Java Programming Exercises

--- Keywords/do/5/Main.java
+++ Keywords/do/5/Main.java
@@ -1,5 +1,30 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from the default package"); 
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("1. Print Hello");
+            System.out.println("2. Print Goodbye");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Hello!");
+                    break;
+                case 2:
+                    System.out.println("Goodbye!");
+                    break;
+                case 3:
+                    System.out.println("Exiting program.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        } while (choice != 3);
+        scanner.close();
     }
 }
