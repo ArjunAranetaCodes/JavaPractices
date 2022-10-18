35dc05e4f618ee9511bd7d6b4aae05d2990962f5
MoreCodes <arjunaraneta@gmail.com>
2022-10-18 19:44:36 +1100

    Java Programming Exercises

---
+++ Keywords/do/4/Main.java
@@ -0,0 +1,10 @@
+class Sample {
+    int num = 20;
+}
+
+class Main {
+    public static void main(String[] args) {
+        Sample obj = new Sample();
+        System.out.println("Value of num: " + obj.num);
+    }
+}
