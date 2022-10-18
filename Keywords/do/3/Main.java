990329b6b0231ced62af4dfcc251fae812969107
MoreCodes <arjunaraneta@gmail.com>
2022-10-18 19:44:56 +1100

    Java Programming Exercises

--- Keywords/do/3/Main.java
+++ Keywords/do/3/Main.java
@@ -1,23 +1,16 @@
-class Main {
+import java.util.Random;
+
+public class Main {
     public static void main(String[] args) {
-        int day = 3;
-        String dayName;
-
-        switch (day) {
-            default:
-                dayName = "Invalid day";
-                break;
-            case 1:
-                dayName = "Monday";
-                break;
-            case 2:
-                dayName = "Tuesday";
-                break;
-            case 3:
-                dayName = "Wednesday";
-                break;
-        }
-
-        System.out.println("Day: " + dayName);
+        Random random = new Random();
+        int sum = 0;
+        int i = 0;
+        do {
+            int randomNumber = random.nextInt(10);
+            System.out.println("Random Number: " + randomNumber);
+            sum += randomNumber;
+            i++;
+        } while (i < 5);
+        System.out.println("Sum of random numbers: " + sum);
     }
 }
