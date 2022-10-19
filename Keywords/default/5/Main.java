9fb7afaa624c84b168c5086646139c04a78ec9c0
MoreCodes <arjunaraneta@gmail.com>
2022-10-19 19:43:55 +1100

    Java Programming Exercises

--- Keywords/default/5/Main.java
+++ Keywords/default/5/Main.java
@@ -1,5 +1,5 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from the default package");
+        System.out.println("Hello from the default package"); 
     }
 }
