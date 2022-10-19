cd3c428a372ff1c6da956ae0188a768ec7156e6a
MoreCodes <arjunaraneta@gmail.com>
2022-10-19 19:43:53 +1100

    Java Programming Exercises

--- Keywords/default/5/Main.java
+++ Keywords/default/5/Main.java
@@ -1,18 +1,5 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
-        MyClass myObject = new MyClass();
-        System.out.println("Constant value from constructor: " + myObject.getConstantValue());
-    }
-}
-
-class MyClass {
-    private final int CONSTANT_VALUE;
-
-    public MyClass() {
-        CONSTANT_VALUE = 7;
-    }
-
-    public int getConstantValue() {
-        return CONSTANT_VALUE;
+        System.out.println("Hello from the default package");
     }
 }
