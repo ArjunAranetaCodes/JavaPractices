88398eac9c89530e651bc57e32bfb079153a4e37
MoreCodes <arjunaraneta@gmail.com>
2022-10-23 19:42:44 +1100

    Java Programming Exercises

---
+++ Keywords/default/3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Constant value from class: " + Constants.MY_CONSTANT);
+    }
+}
+ 
+class Constants {
+    static final int MY_CONSTANT = 99;
+}
---
+++ Keywords/default/5/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        MyClass myObject = new MyClass();
+        System.out.println("Constant value from constructor: " + myObject.getConstantValue());
+    }
+}
+
+class MyClass {
+    private final int CONSTANT_VALUE;
+
+    public MyClass() {
+        CONSTANT_VALUE = 7;
+    }
+
+    public int getConstantValue() {
+        return CONSTANT_VALUE;
+    }
+}
