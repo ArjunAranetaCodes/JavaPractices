f39a599ee609a3de9809c1dbb1ffb74bf71eb702
MoreCodes <arjunaraneta@gmail.com>
2022-10-26 19:41:38 +1100

    Java Programming Exercises

--- Keywords/const/3/Main.java
+++ Keywords/const/3/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        do {
-            i++;
-            if (i == 3) {
-                System.out.println("Skipping iteration " + i);
-                continue;
-            }
-            System.out.println("Executing iteration " + i);
-        } while (i < 5);
+        System.out.println("Constant value from class: " + Constants.MY_CONSTANT);
     }
+}
+
+class Constants {
+    static final int MY_CONSTANT = 99;
 }
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2022-10-27" -m "Java Programming Exercises"
+git commit --date "2022-10-26" -m "Java Programming Exercises"
 git push origin
