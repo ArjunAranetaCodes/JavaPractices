a0c16cde9b6e97d5df778992c704c16cbdff55d7
MoreCodes <arjunaraneta@gmail.com>
2022-10-27 19:41:24 +1100

    Java Programming Exercises

--- Keywords/const/2/Main.java
+++ Keywords/const/2/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 5) {
-            i++;
-            if (i == 2 || i == 4) {
-                System.out.println("Skipping iteration " + i);
-                continue;
-            }
-            System.out.println("Executing iteration " + i);
-        }
+        System.out.println("Result: " + calculateConstant());
+    }
+
+    private static final int calculateConstant() {
+        return 5 * 8;
     }
 }
