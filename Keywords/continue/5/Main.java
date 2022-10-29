6a6d6ba1765a9de6d5a2751576bab3b54eabf1f4
MoreCodes <arjunaraneta@gmail.com>
2022-10-29 19:40:22 +1100

    Java Programming Exercises

--- Keywords/continue/5/Main.java
+++ Keywords/continue/5/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // Simple if-else statement
-        int number = 8;
-        if (number % 2 == 0) {
-            System.out.println(number + " is even.");
-        } else {
-            System.out.println(number + " is odd.");
+        for (int i = 1; i <= 3; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("Processing case 1");
+                    break;
+                case 2:
+                    System.out.println("Skipping case 2");
+                    continue;
+                case 3:
+                    System.out.println("Processing case 3");
+                    break;
+            }
         }
     }
 }
