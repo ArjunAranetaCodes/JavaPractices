99512e4567c95bcb8534f3f8136eeb124d29dd37
MoreCodes <arjunaraneta@gmail.com>
2022-11-03 19:38:31 +1100

    Java Programming Exercises

--- Keywords/continue/1/Main.java
+++ Keywords/continue/1/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             if (i == 3) {
+                System.out.println("Skipping iteration " + i);
             }
         }
     }
