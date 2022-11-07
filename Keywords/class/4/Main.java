5f8df3eb37f0da28482d3c6c1f1e6d2d95cc7f11
MoreCodes <arjunaraneta@gmail.com>
2022-11-07 19:36:48 +1100

    Java Programming Exercises

--- Keywords/class/4/Main.java
+++ Keywords/class/4/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        char char1 = 'X';
-        char char2 = 'Y';
-        boolean isEqual = (char1 == char2);
-        System.out.println("Are char1 and char2 equal? " + isEqual);
+        double radius = 5.0;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("Circle Area: " + area);
     }
 }
