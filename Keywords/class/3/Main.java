f961ce263354f5abc125cdf65bb66b3d95e919aa
MoreCodes <arjunaraneta@gmail.com>
2022-11-08 19:36:37 +1100

    Java Programming Exercises

--- Keywords/class/3/Main.java
+++ Keywords/class/3/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        char[] charArray = {'H', 'e', 'l', 'l', 'o'};
-        System.out.print("Char array: ");
-        for (char c : charArray) {
-            System.out.print(c);
-        }
-        System.out.println();
+        String message = "Java Programming";
+        System.out.println("Message: " + message);
     }
 }
