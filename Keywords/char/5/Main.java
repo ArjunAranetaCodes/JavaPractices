ec02d2744b8aaa003fee52cd2f733d6e267b540b
MoreCodes <arjunaraneta@gmail.com>
2022-11-09 19:35:45 +1100

    Java Programming Exercises

--- Keywords/char/5/Main.java
+++ Keywords/char/5/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            int[] arr = {1, 2, 3};
-            int element = arr[5]; // ArrayIndexOutOfBoundsException will be thrown
-            String str = null;
-            int length = str.length(); // NullPointerException will be thrown
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
-        } catch (NullPointerException e) {
-            System.out.println("Caught NullPointerException: " + e.getMessage());
-        }
+        int asciiValue = 65;
+        char convertedChar = (char) asciiValue;
+        System.out.println("Converted char from ASCII value: " + convertedChar);
     }
 }
