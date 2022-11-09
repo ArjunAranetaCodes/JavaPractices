0a0d769e62c03c988a26cd70fa2668d9f9a26b3d
MoreCodes <arjunaraneta@gmail.com>
2022-11-09 19:35:35 +1100

    Java Programming Exercises

--- Keywords/char/3/Main.java
+++ Keywords/char/3/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            int[] arr = {1, 2, 3};
-            int element = arr[5]; // ArrayIndexOutOfBoundsException will be thrown
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
+        char[] charArray = {'H', 'e', 'l', 'l', 'o'};
+        System.out.print("Char array: ");
+        for (char c : charArray) {
+            System.out.print(c);
         }
+        System.out.println();
     }
 }
