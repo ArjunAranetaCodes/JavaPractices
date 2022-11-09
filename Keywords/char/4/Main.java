fe4dfbf39efc80389201b8f39e6a99281fd1caf1
MoreCodes <arjunaraneta@gmail.com>
2022-11-09 19:35:40 +1100

    Java Programming Exercises

--- Keywords/char/4/Main.java
+++ Keywords/char/4/Main.java
@@ -1,14 +1,8 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            File file = new File("nonexistent.txt");
-            FileReader fr = new FileReader(file); // FileNotFoundException will be thrown
-        } catch (FileNotFoundException e) {
-            System.out.println("Caught FileNotFoundException: " + e.getMessage());
-        }
+        char char1 = 'X';
+        char char2 = 'Y';
+        boolean isEqual = (char1 == char2);
+        System.out.println("Are char1 and char2 equal? " + isEqual);
     }
 }
