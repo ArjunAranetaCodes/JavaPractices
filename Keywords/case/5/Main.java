af4aace537ed478394ff510b5ea1bfc8d6cb9511
MoreCodes <arjunaraneta@gmail.com>
2022-11-11 19:33:08 +1100

    Java Programming Exercises

--- Keywords/case/5/Main.java
+++ Keywords/case/5/Main.java
@@ -1,5 +1,28 @@
-package Keywords.2;
-
 public class Main {
-    
+    enum Season {
+        SPRING, SUMMER, AUTUMN, WINTER
+    }
+
+    public static void main(String[] args) {
+        Season currentSeason = Season.SUMMER;
+
+        switchSeason(currentSeason);
+    }
+
+    static void switchSeason(Season season) {
+        switch (season) {
+            case SPRING:
+                System.out.println("It's Spring!");
+                break;
+            case SUMMER:
+                System.out.println("It's Summer!");
+                break;
+            case AUTUMN:
+                System.out.println("It's Autumn!");
+                break;
+            case WINTER:
+                System.out.println("It's Winter!");
+                break;
+        }
+    }
 }
