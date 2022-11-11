fc5fc7436ee6ca03ac293296a7daafb54cdcc9cf
MoreCodes <arjunaraneta@gmail.com>
2022-11-11 19:33:03 +1100

    Java Programming Exercises

--- Keywords/case/4/Main.java
+++ Keywords/case/4/Main.java
@@ -1,5 +1,17 @@
-package Keywords.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        int number = 2;
+
+        switch (number) {
+            case 1:
+                System.out.println("One");
+            case 2:
+                System.out.println("Two");
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Invalid number");
+        }
+    }
 }
