d7d84652aed6f9bb6a0f7b8ba2b870b2ee836f9f
MoreCodes <arjunaraneta@gmail.com>
2022-11-11 19:32:57 +1100

    Java Programming Exercises

--- Keywords/case/3/Main.java
+++ Keywords/case/3/Main.java
@@ -1,5 +1,16 @@
-package Keywords.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        String color = "red";
+
+        switch (color) {
+            case "red":
+                System.out.println("The color is red");
+                break;
+            case "blue":
+                System.out.println("The color is blue");
+                break;
+            default:
+                System.out.println("Unknown color");
+        }
+    }
 }
