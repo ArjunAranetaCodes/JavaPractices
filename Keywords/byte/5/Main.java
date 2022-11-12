1d9c7e036f01d64f6ff52d8a64e402043b6ff418
MoreCodes <arjunaraneta@gmail.com>
2022-11-12 19:31:19 +1100

    Java Programming Exercises

--- Keywords/byte/5/Main.java
+++ Keywords/byte/5/Main.java
@@ -1,5 +1,12 @@
-package Keywords.2;
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a byte value: ");
+        byte userInput = scanner.nextByte();
+
+        System.out.println("User entered byte value: " + userInput);
+    }
 }
