679b6317a35c1b47a8314618fd059258d916f93e
MoreCodes <arjunaraneta@gmail.com>
2022-11-14 19:30:59 +1100

    Java Programming Exercises

--- Keywords/byte/4/Main.java
+++ Keywords/byte/4/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         byte num = 42;
-        printByteValue(num);
+        printByteValue(num); 
     }
 
     static void printByteValue(byte value) {
