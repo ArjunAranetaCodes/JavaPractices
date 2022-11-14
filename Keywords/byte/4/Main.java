b6ec48b43759fce10c60219517fffe581604fe54
MoreCodes <arjunaraneta@gmail.com>
2022-11-14 19:30:57 +1100

    Java Programming Exercises

---
+++ Keywords/byte/4/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        byte num = 42;
+        printByteValue(num);
+    }
+
+    static void printByteValue(byte value) {
+        System.out.println("Byte value passed to method: " + value);
+    }
+}
