abc441b609911b74bb1dd545a4c7bae5a42cd1d4
MoreCodes <arjunaraneta@gmail.com>
2022-11-18 19:29:43 +1100

    Java Programming Exercises

--- Keywords/byte/3/Main.java
+++ Keywords/byte/3/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         byte[] byteArray = {10, 20, 30, 40, 50};
         System.out.println("Elements of byte array:");
-        }
     }
 }
