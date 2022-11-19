f8080961966a8a490499f52133973204c65c7f55
MoreCodes <arjunaraneta@gmail.com>
2022-11-19 19:28:57 +1100

    Java Programming Exercises

--- Keywords/byte/2/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        byte num1 = 50;
-        byte num2 = 30;
-        byte sum = (byte) (num1 + num2);
-        System.out.println("Sum of bytes: " + sum);
-    }
-}
