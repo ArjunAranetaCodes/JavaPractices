a592c8c462c62fa1de86af42c62febc68c05904b
MoreCodes <arjunaraneta@gmail.com>
2022-11-22 19:27:18 +1100

    Java Programming Exercises

--- Keywords/break/5/Main.java
+++ Keywords/break/5/Main.java
@@ -1,5 +1,14 @@
-package Keywords.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        int num = 1;
+
+        do {
+            System.out.println("Number: " + num);
+            if (num == 3) {
+                System.out.println("Breaking the loop at number 3");
+                break;
+            }
+            num++;
+        } while (num <= 5);
+    }
 }
