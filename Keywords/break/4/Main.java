653cfcf05c3b47d0ba8bc86b2b7ad28c5800e484
MoreCodes <arjunaraneta@gmail.com>
2022-11-24 19:26:41 +1100

    Java Programming Exercises

--- Keywords/break/4/Main.java
+++ Keywords/break/4/Main.java
@@ -6,7 +6,6 @@ public class Main {
             for (int j = 1; j <= 3; j++) {
                 System.out.println("Inner loop iteration " + j);
 
-                if (i + j == 4) {
                     System.out.println("Breaking both loops at total " + (i + j));
                 }
             }
