3d43b2caef1768cb4557b0aab7c63821181b25bc
MoreCodes <arjunaraneta@gmail.com>
2022-11-26 19:26:03 +1100

    Java Programming Exercises

--- Keywords/break/3/Main.java
+++ Keywords/break/3/Main.java
@@ -1,5 +1,14 @@
-package Keywords.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 5) {
+            System.out.println("Count: " + count);
+            if (count == 2) {
+                System.out.println("Breaking the loop at count 2");
+                break;
+            }
+            count++;
+        }
+    }
 }
