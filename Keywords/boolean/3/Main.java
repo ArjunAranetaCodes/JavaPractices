658a49f13685687d3224b932efb32e055b64b9fe
MoreCodes <arjunaraneta@gmail.com>
2022-12-06 19:19:51 +1100

    Java Programming Exercises

--- Keywords/boolean/3/Main.java
+++ Keywords/boolean/3/Main.java
@@ -1,5 +1,12 @@
-package Keywords.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        int age = 18;
+        boolean isAdult = (age >= 18);
+
+        if (isAdult) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not yet an adult.");
+        }
+    }
 }
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2022-12-07" -m "Java Programming Exercises"
+git commit --date "2022-12-06" -m "Java Programming Exercises"
 git push origin
