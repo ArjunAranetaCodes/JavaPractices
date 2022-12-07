ef9317cc47dd7679065c42125335191408b0f52d
MoreCodes <arjunaraneta@gmail.com>
2022-12-07 19:19:36 +1100

    Java Programming Exercises

---
+++ Keywords/boolean/2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        boolean isGreater = (x > y);
+        System.out.println("Is x greater than y? " + isGreater);
+    }
+}
