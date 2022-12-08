9c270a100c8441ef63132b3518bfb33df2afb45a
MoreCodes <arjunaraneta@gmail.com>
2022-12-08 19:18:01 +1100

    Java Programming Exercises

--- Keywords/assert/4/Main.java
+++ Keywords/assert/4/Main.java
@@ -1,5 +1,12 @@
-package Keywords.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        int result = calculate(-3, 2);
+        System.out.println("Result is: " + result);
+    }
+
+    private static int calculate(int a, int b) {
+        assert a >= 0 : "Input a must be non-negative";
+        assert b >= 0 : "Input b must be non-negative";
+        return a + b;
+    }
 }
