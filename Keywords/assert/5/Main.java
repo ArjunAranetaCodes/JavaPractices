770d52e692365bf8536765c9e31f74358e75a0e9
MoreCodes <arjunaraneta@gmail.com>
2022-12-08 19:18:17 +1100

    Java Programming Exercises

--- Keywords/assert/5/Main.java
+++ Keywords/assert/5/Main.java
@@ -2,5 +2,3 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         int index = 10;
-        assert index >= 0 && index < numbers.length : "Index out of bounds";
-        Systx
