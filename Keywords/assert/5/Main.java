2582c5e0e030075b0d1a6f167331d0cc5ad29e37
MoreCodes <arjunaraneta@gmail.com>
2022-12-08 19:18:20 +1100

    Java Programming Exercises

--- Keywords/assert/5/Main.java
+++ Keywords/assert/5/Main.java
@@ -2,3 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         int index = 10;
+        assert index >= 0 && index < numbers.length : "Index out of bounds";
+        System.out.println("Value at index " + index + ": " + numbers[index]);
+    }
+}
