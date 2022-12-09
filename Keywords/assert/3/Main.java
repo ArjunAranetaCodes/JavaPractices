32da196b31ff3e6f93778fe6d0472b5c2d6c0ab0
MoreCodes <arjunaraneta@gmail.com>
2022-12-09 19:17:40 +1100

    Java Programming Exercises

--- Keywords/assert/3/Main.java
+++ Keywords/assert/3/Main.java
@@ -1,5 +1,7 @@
-package Keywords.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        int value = -5;
+        assert value >= 0 : "Value must be non-negative";
+        System.out.println("Value is: " + value);
+    }
 }
