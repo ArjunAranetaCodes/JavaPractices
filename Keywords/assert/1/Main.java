e47cce62c7737ccf302ed638f48a366874db34fe
MoreCodes <arjunaraneta@gmail.com>
2022-12-09 19:17:27 +1100

    Java Programming Exercises

---
+++ Keywords/assert/1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        assert age >= 18 : "Age must be 18 or older";
+        System.out.println("Age is: " + age);
+    }
+}
