e9a1e837e06fec4c231149e82025166260f19ea0
MoreCodes <arjunaraneta@gmail.com>
2022-12-10 19:16:44 +1100

    Java Programming Exercises

--- Keywords/abstract/5/Main.java
+++ Keywords/abstract/5/Main.java
@@ -1,5 +1,31 @@
-package Keywords.2;
+interface Drawable {
+    void draw();
+}
 
-public class Main {
-    
+abstract class Shape implements Drawable {
+    int sides;
+
+    abstract void calculateArea();
+}
+
+class Triangle extends Shape {
+    Triangle(int sides) {
+        this.sides = sides;
+    }
+
+    void draw() {
+        System.out.println("Drawing Triangle");
+    }
+
+    void calculateArea() {
+        System.out.println("Triangle Area: " + 0.5 * sides * sides);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(3);
+        triangle.draw();
+        triangle.calculateArea();
+    }
 }
