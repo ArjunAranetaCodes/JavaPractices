5dd914180b19aaacad7e8143ee067679f0943e95
MoreCodes <arjunaraneta@gmail.com>
2022-12-10 19:16:27 +1100

    Java Programming Exercises

--- Keywords/abstract/3/Main.java
+++ Keywords/abstract/3/Main.java
@@ -1,5 +1,21 @@
-package Keywords.2;
+abstract class Vehicle {
+    abstract void start();
 
-public class Main {
-    
+    void stop() {
+        System.out.println("Vehicle stopped");
+    }
+}
+
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car started");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.start();
+        car.stop();
+    }
 }
