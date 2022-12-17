e245329d8eee66553ef05ba129fd59a96a13e2c9
MoreCodes <arjunaraneta@gmail.com>
2022-12-17 17:30:45 +1100

    Java Programming Exercises

--- DataStructures/Basics/Arrays/10/Main.java
+++ DataStructures/Basics/Arrays/10/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
         System.out.println("Result of Matrix Multiplication:");
         for (int[] row : result) {
+            System.out.println(Arrays.toString(row));
         }
     }
 }
