79e2118dec5bb72c88d2fca2bcbee9b0c9d8af23
MoreCodes <arjunaraneta@gmail.com>
2022-12-20 17:30:13 +1100

    Java Programming Exercises

--- DataStructures/Basics/Arrays/9/Main.java
+++ DataStructures/Basics/Arrays/9/Main.java
@@ -5,5 +5,6 @@ public class Main {
         int[] numbers = {2, 4, 6, 8, 2, 6, 10, 12, 14, 8};
         int[] uniqueNumbers = Arrays.stream(numbers).distinct().toArray();
 
+        System.out.println("Original Array: " + Arrays.toString(numbers));
     }
 }
