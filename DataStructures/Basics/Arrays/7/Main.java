136fe30b46e438c5de214a8d2233f2c31b3622ed
MoreCodes <arjunaraneta@gmail.com>
2022-12-24 17:28:54 +1100

    Java Programming Exercises

--- DataStructures/Basics/Arrays/7/Main.java
+++ DataStructures/Basics/Arrays/7/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int[][] matrix = {
             {1, 2, 3},
-            {4, 5, 6},
         };
 
         System.out.println("Matrix elements:");
