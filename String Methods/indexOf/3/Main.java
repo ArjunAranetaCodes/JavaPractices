fea64cf5758313683e93e751bf899cf5d2e579d4
MoreCodes <arjunaraneta@gmail.com>
2022-12-31 23:53:17 +1100

    Java Programming Exercises

--- String Methods/indexOf/3/Main.java
+++ String Methods/indexOf/3/Main.java
@@ -1,15 +1,8 @@
-import java.util.HashSet;
-
 public class Main {
     public static void main(String[] args) {
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.add("Apple");
-        hashSet.add("Banana");
-        hashSet.add("Orange");
-
-        for (String fruit : hashSet) {
-            System.out.println("HashCode of " + fruit + ": " + fruit.hashCode());
-        }
+        String str = "Hello, Java!";
+        char targetChar = 'J';
+        int index = str.indexOf(targetChar);
+        System.out.println("Index of '" + targetChar + "': " + index);
     }
 }
-   
