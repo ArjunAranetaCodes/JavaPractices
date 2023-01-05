55a16e43b87ee4a340ae4e71967147a829cc314e
MoreCodes <arjunaraneta@gmail.com>
2023-01-05 17:19:45 +1100

    Java Programming Exercises

--- DataStructures/Basics/Arrays/1/Main.java
+++ DataStructures/Basics/Arrays/1/Main.java
@@ -4,6 +4,9 @@ public class Main {
         int sum = 0;
 
         for (int num : numbers) {
+            sum += num;
         }
+
+        System.out.println("Sum of array elements: " + sum);
     }
 }
