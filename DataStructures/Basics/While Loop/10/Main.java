7eabc216de281688e0edd5c91f2fb263cc2080d2
MoreCodes <arjunaraneta@gmail.com>
2023-01-10 17:04:46 +1100

    Java Programming Exercises

--- DataStructures/Basics/While Loop/10/Main.java
+++ DataStructures/Basics/While Loop/10/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int base = 2;
         int exponent = 3;
-        int result = 1;
         int i = 1;
         while (i <= exponent) {
         }
