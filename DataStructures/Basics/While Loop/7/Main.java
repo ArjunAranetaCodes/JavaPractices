ec5f5dd5a3c6dbaee5fc1697614f13769fbe6ffc
MoreCodes <arjunaraneta@gmail.com>
2023-01-16 17:02:50 +1100

    Java Programming Exercises

--- DataStructures/Basics/While Loop/7/Main.java
+++ DataStructures/Basics/While Loop/7/Main.java
@@ -1,23 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number (1-3): ");
-        int userInput = scanner.nextInt();
-
-        switch (userInput) {
-            case 1:
-                System.out.println("You entered 1");
-                break;
-            case 2:
-                System.out.println("You entered 2");
-                break;
-            case 3:
-                System.out.println("You entered 3");
-                break;
-            default:
-                System.out.println("Invalid input");
+        int n = 8;
+        int first = 0, second = 1;
+        int i = 1;
+        while (i <= n) {
+            System.out.println(first);
+            int next = first + second;
+            first = second;
+            second = next;
+            i++;
         }
     }
 }
