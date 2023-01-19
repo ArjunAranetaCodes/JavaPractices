24a363938772fafe38b3d691f5d9cda912890e81
MoreCodes <arjunaraneta@gmail.com>
2023-01-19 17:01:18 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/While Loop/4/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        int i = 2;
+        while (i <= n) {
+            System.out.println("Even Number: " + i);
+            i += 2;
+        }
+    }
+}
