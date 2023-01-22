b0f1348071398c25e1d49c445da3df29e7ae202a
MoreCodes <arjunaraneta@gmail.com>
2023-01-22 17:00:17 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/While Loop/1/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int dayOfWeek = 3;
+
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            // Add cases for other days as needed
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
