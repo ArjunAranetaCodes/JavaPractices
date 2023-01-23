aae44f154407652975c7a209c89df7d5fd03d43a
MoreCodes <arjunaraneta@gmail.com>
2023-01-23 16:58:37 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/Switch/8/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int category = 2;
+        int subCategory = 1;
+
+        switch (category) {
+            case 1:
+                System.out.print("Category 1: ");
+                switch (subCategory) {
+                    case 1:
+                        System.out.println("Subcategory 1");
+                        break;
+                    case 2:
+                        System.out.println("Subcategory 2");
+                        break;
+                    default:
+                        System.out.println("Unknown subcategory");
+                }
+                break;
+            case 2:
+                System.out.println("Category 2");
+                break;
+            // Add cases for other categories as needed
+            default:
+                System.out.println("Unknown category");
+        }
+    }
+}
