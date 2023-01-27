3d0355fec221b9d88817f907be9003d732fcbd61
MoreCodes <arjunaraneta@gmail.com>
2023-01-27 16:54:27 +1100

    Java Programming Exercises

--- DataStructures/Basics/Switch/7/Main.java
+++ DataStructures/Basics/Switch/7/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int x = 5;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number (1-3): ");
+        int userInput = scanner.nextInt();
 
-        x += 3;
-        System.out.println("Updated x: " + x);
-
-        x *= 2;
-        System.out.println("Updated x: " + x);
+        switch (userInput) {
+            case 1:
+                System.out.println("You entered 1");
+                break;
+            case 2:
+                System.out.println("You entered 2");
+                break;
+            case 3:
+                System.out.println("You entered 3");
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
     }
 }
