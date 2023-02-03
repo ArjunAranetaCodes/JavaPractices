747f6749bf70eeec4a80c4edb963efb777250e33
MoreCodes <arjunaraneta@gmail.com>
2023-02-03 16:51:05 +1100

    Java Programming Exercises

--- DataStructures/Basics/Switch/3/Main.java
+++ DataStructures/Basics/Switch/3/Main.java
@@ -10,8 +10,6 @@ public class Main {
                 System.out.println("It's a banana");
                 break;
             // Add cases for other fruits as needed
-            default:
-                System.out.println("Unknown fruit");
         }
     }
 }
