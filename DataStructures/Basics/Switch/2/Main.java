73a42eac305d914da9fda2b5b3925dbdeffd82f8
MoreCodes <arjunaraneta@gmail.com>
2023-02-04 16:50:19 +1100

    Java Programming Exercises

--- DataStructures/Basics/Switch/2/Main.java
+++ DataStructures/Basics/Switch/2/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
         switch (fruit) {
             case "Apple":
+                System.out.println("It's an apple");
                 break;
             case "Banana":
+                System.out.println("It's a banana");
                 break;
             // Add cases for other fruits as needed
             default:
+                System.out.println("Unknown fruit");
         }
     }
 }
