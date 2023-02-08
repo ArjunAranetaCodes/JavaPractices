b60ce4528d01bbb9b047bbd3ab5adb3f1214f1b0
MoreCodes <arjunaraneta@gmail.com>
2023-02-08 16:49:05 +1100

    Java Programming Exercises

--- DataStructures/Basics/Switch/1/Main.java
+++ DataStructures/Basics/Switch/1/Main.java
@@ -4,8 +4,6 @@ public class Main {
         int sum = a + b;
         int difference = a - b;
         int product = a * b;
-        int quotient = a / b;
-        int remainder = a % b;
 
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + difference);
