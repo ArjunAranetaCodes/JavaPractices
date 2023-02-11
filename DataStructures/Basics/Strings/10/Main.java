b9f2be455f91f1d7f3e7b56586ad87eaf991d1d8
MoreCodes <arjunaraneta@gmail.com>
2023-02-11 16:47:13 +1100

    Java Programming Exercises

--- DataStructures/Basics/Strings/10/Main.java
+++ DataStructures/Basics/Strings/10/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("Hello");
-        stringBuilder.append(" World");
-        String result = stringBuilder.toString();
+        StringBuilder stringBuilder = new StringBuilder(
     }
 }
