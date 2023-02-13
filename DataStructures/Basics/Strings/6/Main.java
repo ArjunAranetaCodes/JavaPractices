7599217ac48dfd10c2bc45b34a22d968504e42ec
MoreCodes <arjunaraneta@gmail.com>
2023-02-13 16:46:41 +1100

    Java Programming Exercises

--- DataStructures/Basics/Strings/6/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String original = "hello";
-        String uppercase = original.toUpperCase();
-        System.out.println("Uppercase: " + uppercase);
-    }
-}
