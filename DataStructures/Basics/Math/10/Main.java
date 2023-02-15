d0e30f77e068dd5b45d376a42cd37032daf4a847
MoreCodes <arjunaraneta@gmail.com>
2023-02-15 16:44:53 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/10/Main.java
+++ DataStructures/Basics/Math/10/Main.java
@@ -5,7 +5,9 @@ public class Main {
         if (Character.isDigit(ch)) {
             System.out.println("The character is a digit.");
         } else if (Character.isLetter(ch)) {
+            System.out.println("The character is a letter.");
         } else {
+            System.out.println("The character is neither a digit nor a letter.");
         }
     }
 }
