d7e1607916a3dc861696ae5692d6a1c53bc313dc
MoreCodes <arjunaraneta@gmail.com>
2023-02-16 16:44:18 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/Math/7/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 15.99;
+
+        if (price >= 10.0 && price <= 20.0) {
+            System.out.println("The price is reasonable.");
+        } else {
+            System.out.println("The price is not within the acceptable range.");
+        }
+    }
+}
