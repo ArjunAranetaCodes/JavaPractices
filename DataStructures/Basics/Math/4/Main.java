0940d0a33ca651e10dc32d9dbbcbee0a25b16805
MoreCodes <arjunaraneta@gmail.com>
2023-02-17 16:43:33 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/Math/4/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        String result = (x > y) ? "x is greater" : "y is greater";
+        System.out.println(result);
+    }
+}
