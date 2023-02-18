44a0204c8deb270ba70c4cc3633cb0c6b4587311
MoreCodes <arjunaraneta@gmail.com>
2023-02-18 16:42:54 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/10/Main.java
+++ DataStructures/Basics/Math/10/Main.java
@@ -2,7 +2,5 @@ public class Main {
     public static void main(String[] args) {
         double base = 2;
 
-        double result = Math.exp(base);
-        System.out.println("e raised to the power of " + base + ": " + result);
     }
 }
