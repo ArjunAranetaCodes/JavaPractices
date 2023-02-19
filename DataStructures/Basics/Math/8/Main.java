2ea9a788e516873411fa2728a14b7204d362b14f
MoreCodes <arjunaraneta@gmail.com>
2023-02-19 16:42:38 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/8/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        double number = 100;
-
-        // Natural logarithm
-        double logarithm = Math.log(number);
-        System.out.println("Natural logarithm of " + number + ": " + logarithm);
-    }
-}
