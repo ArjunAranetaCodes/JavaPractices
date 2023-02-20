608bed868cc9fc4bc2489ac02222ef960ed8225a
MoreCodes <arjunaraneta@gmail.com>
2023-02-20 16:42:17 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/5/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Generate a random number between 0 and 1
-        double randomValue = Math.random();
-        System.out.println("Random value between 0 and 1: " + randomValue);
-    }
-}
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2023-02-21" -m "Java Programming Exercises"
+git commit --date "2023-02-20" -m "Java Programming Exercises"
 git push origin
