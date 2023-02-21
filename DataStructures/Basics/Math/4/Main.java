e6a7b47005a99c588bdb78b4f18150e6372c5524
MoreCodes <arjunaraneta@gmail.com>
2023-02-21 16:42:09 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/4/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int number = -10;
-
-        // Absolute value
-        int absoluteValue = Math.abs(number);
-        System.out.println("Absolute value of " + number + ": " + absoluteValue);
-    }
-}
