3c744294e21fce082dc11bedafc2339716ebd6a1
MoreCodes <arjunaraneta@gmail.com>
2023-02-21 16:42:02 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/2/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        double number = 25;
-
-        // Square root
-        double squareRoot = Math.sqrt(number);
-        System.out.println("Square root of " + number + ": " + squareRoot);
-    }
-}
