9deb1f9f00db18845b14d32a0b2e6a4d9e8b2186
MoreCodes <arjunaraneta@gmail.com>
2023-02-23 16:40:28 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/Math/9/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 8;
+
+        // Minimum and Maximum
+        int min = Math.min(x, y);
+        int max = Math.max(x, y);
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
