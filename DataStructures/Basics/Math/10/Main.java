d181280a75454d13f82c7e8fe1ef3804c34307b0
MoreCodes <arjunaraneta@gmail.com>
2023-02-25 16:38:44 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/10/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("Hello");
-        stringBuilder.append(" World");
-        String result = stringBuilder.toString();
-        System.out.println("Result: " + result);
-    }
-}
