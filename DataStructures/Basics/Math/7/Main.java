6d9d1169067a213cf74582b7e903db32874fc339
MoreCodes <arjunaraneta@gmail.com>
2023-02-26 16:38:30 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/7/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String sentence = "Java programming is powerful";
-        boolean containsJava = sentence.contains("Java");
-        System.out.println("Contains 'Java': " + containsJava);
-    }
-}
