4f788d83dee702884555ad55e5fef9e4c14c8a6c
MoreCodes <arjunaraneta@gmail.com>
2023-02-27 16:38:18 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/5/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String original = "HELLO";
-        String lowercase = original.toLowerCase();
-        System.out.println("Lowercase: " + lowercase);
-    }
-}
