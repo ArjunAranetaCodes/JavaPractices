362578a4dcb57e8c2a0bf4a9587e626fd32129f9
MoreCodes <arjunaraneta@gmail.com>
2023-02-27 16:38:15 +1100

    Java Programming Exercises

--- DataStructures/Basics/Math/4/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String text = "Programming is fun";
-        String substring = text.substring(0, 11);
-        System.out.println("Substring: " + substring);
-    }
-}
