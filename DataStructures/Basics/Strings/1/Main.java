8c92eccab2de3fb024c734ba645099ab269d66d7
MoreCodes <arjunaraneta@gmail.com>
2023-03-01 16:35:33 +1100

    Java Programming Exercises

--- DataStructures/Basics/Strings/1/Main.java
+++ DataStructures/Basics/Strings/1/Main.java
@@ -3,6 +3,5 @@ public class Main {
         String firstName = "John";
         String lastName = "Doe";
         String fullName = firstName + " " + lastName;
-        System.out.println("Full Name: " + fullName);
     }
 }
