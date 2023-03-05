6848b15bb6ddab030d3816d63f772a81f7670bf5
MoreCodes <arjunaraneta@gmail.com>
2023-03-05 16:33:53 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/Strings/9/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String sentence = "Java is easy to learn.";
+        String newSentence = sentence.replace("easy", "fun");
+        System.out.println("Modified Sentence: " + newSentence);
+    }
+}
