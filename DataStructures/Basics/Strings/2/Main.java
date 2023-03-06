5494d120fa84ede8bf5b7ba5cf5ad4c9aae7339c
MoreCodes <arjunaraneta@gmail.com>
2023-03-06 16:32:49 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/Strings/2/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        int length = message.length();
+        System.out.println("Length of the string: " + length);
+    }
+}
