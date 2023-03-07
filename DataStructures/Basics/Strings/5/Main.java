42640adb4d3d39225c77cdf18a6610f1679bd042
MoreCodes <arjunaraneta@gmail.com>
2023-03-07 16:32:03 +1100

    Java Programming Exercises

--- DataStructures/Basics/Strings/5/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        float floatValue = 3.14f;
-        long longValue = (long) floatValue; // Explicit casting from float to long
-        System.out.println("Long Value: " + longValue);
-    }
-}
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
-git commit --date "2023-03-08" -m "Java Programming Exercises"
+git commit --date "2023-03-07" -m "Java Programming Exercises"
 git push origin
