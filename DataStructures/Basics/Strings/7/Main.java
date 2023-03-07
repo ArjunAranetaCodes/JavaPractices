57cb6ba93c20aec44f2b56f5111487aa40ec322c
MoreCodes <arjunaraneta@gmail.com>
2023-03-07 16:32:14 +1100

    Java Programming Exercises

--- DataStructures/Basics/Strings/7/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        char charValue = 'A';
-        int intValue = (int) charValue; // Explicit casting from char to int
-        System.out.println("Int Value: " + intValue);
-    }
-}
