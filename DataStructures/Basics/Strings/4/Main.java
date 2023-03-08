016c79d348c8c4904a35ac36b4b6088440e1a8cf
MoreCodes <arjunaraneta@gmail.com>
2023-03-08 16:31:50 +1100

    Java Programming Exercises

--- DataStructures/Basics/Strings/4/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int intValue = 5;
-        double doubleValue = 2.5;
-        double result = intValue + doubleValue; // Implicit casting before addition
-        System.out.println("Result: " + result);
-    }
-}
