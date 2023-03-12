cafa8c0fb520235dc3871bbaf062b9b733fc2e7b
MoreCodes <arjunaraneta@gmail.com>
2023-03-12 23:51:51 +1100

    Java Programming Exercises

--- DataStructures/Basics/Operators/9/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
-        int sum = a + b;
-        System.out.println("Sum of two variables: " + sum);
-    }
-}
