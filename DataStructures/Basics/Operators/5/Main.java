9ac64bc62fa10c63d5ad037d7f225881d1b5c9b5
MoreCodes <arjunaraneta@gmail.com>
2023-03-18 23:48:56 +1100

    Java Programming Exercises

---
+++ DataStructures/Basics/Operators/5/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5, y = 3;
+
+        System.out.println("Bitwise AND: " + (x & y));
+        System.out.println("Bitwise OR: " + (x | y));
+        System.out.println("Bitwise XOR: " + (x ^ y));
+        System.out.println("Bitwise NOT for x: " + (~x));
+    }
+}
