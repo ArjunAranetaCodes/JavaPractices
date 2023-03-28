375276efe46d022946676c6fc8273f37018cd9f9
MoreCodes <arjunaraneta@gmail.com>
2023-03-28 23:43:57 +1100

    Java Programming Exercises

--- DataStructures/Basics/Operators/2/Main.java
+++ DataStructures/Basics/Operators/2/Main.java
@@ -3,7 +3,6 @@ public class Main {
         int x = 5, y = 8;
 
         System.out.println("Is x equal to y? " + (x == y));
-        System.out.println("Is x not equal to y? " + (x != y));
         System.out.println("Is x less than y? " + (x < y));
         System.out.println("Is x greater than or equal to y? " + (x >= y));
     }
