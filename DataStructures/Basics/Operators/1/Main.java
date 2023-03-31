9f1e2bd2b15747365fd3c7baf60582edc3de3ca9
MoreCodes <arjunaraneta@gmail.com>
2023-03-31 23:42:53 +1100

    Java Programming Exercises

--- DataStructures/Basics/Operators/1/Main.java
+++ DataStructures/Basics/Operators/1/Main.java
@@ -11,6 +11,5 @@ public class Main {
         System.out.println("Difference: " + difference);
         System.out.println("Product: " + product);
         System.out.println("Quotient: " + quotient);
-        System.out.println("Remainder: " + remainder);
     }
 }
