ca6b5bccf1db74a3baf6e1a3b8a9b26dc79c7216
MoreCodes <arjunaraneta@gmail.com>
2023-04-02 22:39:05 +1000

    Java Programming Exercises

--- DataStructures/Basics/TypeCasting/2/Main.java
+++ DataStructures/Basics/TypeCasting/2/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         double doubleValue = 15.75;
-        int intValue = (int) doubleValue; // Explicit casting from double to int
     }
 }
