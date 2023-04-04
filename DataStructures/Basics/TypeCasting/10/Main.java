dc73d4b193318a48d500ec39c8d0b03b71abfaf8
MoreCodes <arjunaraneta@gmail.com>
2023-04-04 22:37:27 +1000

    Java Programming Exercises

--- DataStructures/Basics/TypeCasting/10/Main.java
+++ DataStructures/Basics/TypeCasting/10/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String strValue = "123";
+        int intValue = Integer.parseInt(strValue); // String to int conversion
+        System.out.println("Int Value: " + intValue);
     }
 }
