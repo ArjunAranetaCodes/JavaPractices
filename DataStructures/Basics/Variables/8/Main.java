0ccadc9f51829eaa319f8d4df879fa01d3516cdc
MoreCodes <arjunaraneta@gmail.com>
2023-04-10 22:31:35 +1000

    Java Programming Exercises

--- DataStructures/Basics/Variables/8/Main.java
+++ DataStructures/Basics/Variables/8/Main.java
@@ -1,5 +1,24 @@
-package DataStructures.Basics.Variables.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        Person person = new Person("John", 25);
+        System.out.println("Object Variable: " + person.getName() + ", " + person.getAge());
+    }
+
+    static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
 }
