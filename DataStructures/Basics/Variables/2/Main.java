40fa36313974771fc894dfcacea78773618880e8
MoreCodes <arjunaraneta@gmail.com>
2023-04-11 22:30:13 +1000

    Java Programming Exercises

--- DataStructures/Basics/Variables/2/Main.java
+++ DataStructures/Basics/Variables/2/Main.java
@@ -1,5 +1,6 @@
-package DataStructures.Basics.Variables.2;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        double value = 3.14;
+        System.out.println("Double Variable: " + value);
+    }
 }
