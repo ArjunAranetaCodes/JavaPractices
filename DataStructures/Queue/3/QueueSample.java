1f82bc794bf16e108031d853eade200c0639c151
MoreCodes <arjunaraneta@gmail.com>
2023-04-24 21:12:48 +1000

    Java Programming Exercises

--- DataStructures/Queue/3/QueueSample.java
+++ DataStructures/Queue/3/QueueSample.java
@@ -1,13 +1,29 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+class Person {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
 public class QueueSample {
     public static void main(String[] args) {
-        Queue<Double> queue = new LinkedList<>();
+        Queue<Person> personQueue = new LinkedList<>();
 
-        queue.offer(3.14);
-        queue.offer(2.71);
+        personQueue.add(new Person("Alice"));
+        personQueue.add(new Person("Bob"));
+        personQueue.add(new Person("Charlie"));
 
-        System.out.println("Front element: " + queue.peek());
+        System.out.println("Person Queue: " + personQueue);
     }
 }
