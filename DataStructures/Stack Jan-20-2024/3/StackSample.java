e50429568e8542f1b18d157d9224eb4494c76a33
MoreCodes <arjunaraneta@gmail.com>
2023-04-29 21:09:46 +1000

    Java Programming Exercises

--- DataStructures/Stack Jan-20-2024/3/StackSample.java
+++ DataStructures/Stack Jan-20-2024/3/StackSample.java
@@ -1,23 +1,46 @@
 import java.util.Stack;
 
-class Student {
-    String name;
-
-    public Student(String name) {
-        this.name = name;
-    }
-}
-
 public class StackSample {
     public static void main(String[] args) {
-        Stack<Student> studentStack = new Stack<>();
-        studentStack.push(new Student("John"));
-        studentStack.push(new Student("Alice"));
+        String infixExpression = "3 + 5 * ( 2 - 8 ) / 2";
+        System.out.println("Postfix Expression: " + infixToPostfix(infixExpression));
+    }
 
-        // Pop and print student names
-        while (!studentStack.isEmpty()) {
-            Student student = studentStack.pop();
-            System.out.println("Popped student: " + student.name);
+    private static String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : infix.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                postfix.append(ch);
+            } else if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop(); // Pop '('
+            } else {
+                while (!stack.isEmpty() && getPrecedence(ch) <= getPrecedence(stack.peek())) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(ch);
+            }
         }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+
+        return postfix.toString();
+    }
+
+    private static int getPrecedence(char operator) {
+        if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+        return 0;
     }
 }
