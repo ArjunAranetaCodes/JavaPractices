96a647162cce4c5826e842a0d1a801d354e73e06
MoreCodes <arjunaraneta@gmail.com>
2023-04-29 21:09:39 +1000

    Java Programming Exercises

--- DataStructures/Stack Jan-20-2024/2/StackSample.java
+++ DataStructures/Stack Jan-20-2024/2/StackSample.java
@@ -2,28 +2,25 @@ import java.util.Stack;
 
 public class StackSample {
     public static void main(String[] args) {
-        String expression = "{[()]}";
-        boolean isBalanced = isBalanced(expression);
-        System.out.println("Is the expression balanced? " + isBalanced);
+        String expression = "{[()]()}";
+        System.out.println("Expression is balanced: " + isBalanced(expression));
     }
 
     private static boolean isBalanced(String expression) {
         Stack<Character> stack = new Stack<>();
-
         for (char ch : expression.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
+            if (ch == '(' || ch == '[' || ch == '{') {
                 stack.push(ch);
             } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
                 stack.pop();
-            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
             } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
                 stack.pop();
+            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
             } else {
-                return false; // Unmatched closing bracket
+                return false; // Unmatched closing parenthesis
             }
         }
-
-        return stack.isEmpty(); // Stack should be empty for balanced expression
+        return stack.isEmpty();
     }
 }
