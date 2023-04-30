2334cf4a3870eb30954e98b15ececa1e4031f362
MoreCodes <arjunaraneta@gmail.com>
2023-04-30 21:07:22 +1000

    Java Programming Exercises

--- DataStructures/LinkedList Jan-20-2024/8/LinkedListSample.java
+++ DataStructures/LinkedList Jan-20-2024/8/LinkedListSample.java
@@ -2,14 +2,13 @@ import java.util.LinkedList;
 
 public class LinkedListSample {
     public static void main(String[] args) {
-        LinkedList<Integer> originalList = new LinkedList<>();
-        originalList.add(10);
-        originalList.add(20);
-        originalList.add(30);
+        LinkedList<String> names = new LinkedList<>();
 
-        LinkedList<Integer> copiedList = new LinkedList<>(originalList);
+        System.out.println("Is the LinkedList empty? " + names.isEmpty());
 
-        System.out.println("Original LinkedList: " + originalList);
-        System.out.println("Copied LinkedList: " + copiedList);
+        names.add("John");
+        names.add("Alice");
+
+        System.out.println("Is the LinkedList empty? " + names.isEmpty());
     }
 }
