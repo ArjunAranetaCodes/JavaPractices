11e7feb4ebbdedf489b7f4d3a7f9f1d28644118c
MoreCodes <arjunaraneta@gmail.com>
2023-05-03 21:02:35 +1000

    Java Programming Exercises

--- Multi-Threading-Jan-20-2024/6/Main.java
+++ Multi-Threading-Jan-20-2024/6/Main.java
@@ -1,73 +1,25 @@
-class Chopstick {
-    private boolean taken = false;
-
-    public synchronized void take() {
-        while (taken) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        taken = true;
-    }
-
-    public synchronized void release() {
-        taken = false;
-        notify();
-    }
-}
-
-class Philosopher extends Thread {
-    private Chopstick leftChopstick;
-    private Chopstick rightChopstick;
-
-    public Philosopher(String name, Chopstick leftChopstick, Chopstick rightChopstick) {
-        super(name);
-        this.leftChopstick = leftChopstick;
-        this.rightChopstick = rightChopstick;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            think();
-            eat();
-        }
-    }
-
-    private void think() {
-        System.out.println(getName() + " is thinking.");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void eat() {
-        leftChopstick.take();
-        rightChopstick.take();
-
-        System.out.println(getName() + " is eating.");
-
-        leftChopstick.release();
-        rightChopstick.release();
-    }
-}
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
+    private static int sharedResource = 0;
+    private static Lock lock = new ReentrantLock();
+
     public static void main(String[] args) {
-        Chopstick[] chopsticks = new Chopstick[5];
-        Philosopher[] philosophers = new Philosopher[5];
+        Thread t1 = new Thread(() -> incrementSharedResource());
+        Thread t2 = new Thread(() -> incrementSharedResource());
 
-        for (int i = 0; i < 5; i++) {
-            chopsticks[i] = new Chopstick();
-        }
+        t1.start();
+        t2.start();
+    }
 
-        for (int i = 0; i < 5; i++) {
-            philosophers[i] = new Philosopher("Philosopher " + (i + 1), chopsticks[i], chopsticks[(i + 1) % 5]);
-            philosophers[i].start();
+    private static void incrementSharedResource() {
+        lock.lock();
+        try {
+            // Critical section
+            System.out.println(Thread.currentThread().getName() + ": " + (++sharedResource));
+        } finally {
+            lock.unlock();
         }
     }
 }
