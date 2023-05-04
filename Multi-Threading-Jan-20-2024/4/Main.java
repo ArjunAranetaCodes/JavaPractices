4c773e5bf926409ffb83b70236347d060f21e9d7
MoreCodes <arjunaraneta@gmail.com>
2023-05-04 21:01:48 +1000

    Java Programming Exercises

--- Multi-Threading-Jan-20-2024/4/Main.java
+++ Multi-Threading-Jan-20-2024/4/Main.java
@@ -1,73 +1,27 @@
-class Chopstick {
-    private boolean taken = false;
+public class Main {
+    private static int number = 1;
 
-    public synchronized void take() {
-        while (taken) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    public static void main(String[] args) {
+        Thread oddThread = new Thread(() -> printOdd());
+        Thread evenThread = new Thread(() -> printEven());
+
+        oddThread.start();
+        evenThread.start();
+    }
+
+    private static synchronized void printOdd() {
+        while (number <= 10) {
+            if (number % 2 != 0) {
+                System.out.println(Thread.currentThread().getName() + ": " + number++);
             }
         }
-        taken = true;
     }
 
-    public synchronized void release() {
-        taken = false;
-        notify();
-    }
-}
-
-class Philosopher extends Thread {
-    private Chopstick leftChopstick;
-    private Chopstick rightChopstick;
-
-    public Philosopher(String name, Chopstick leftChopstick, Chopstick rightChopstick) {
-        super(name);
-        this.leftChopstick = leftChopstick;
-        this.rightChopstick = rightChopstick;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            think();
-            eat();
-        }
-    }
-
-    private void think() {
-        System.out.println(getName() + " is thinking.");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void eat() {
-        leftChopstick.take();
-        rightChopstick.take();
-
-        System.out.println(getName() + " is eating.");
-
-        leftChopstick.release();
-        rightChopstick.release();
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Chopstick[] chopsticks = new Chopstick[5];
-        Philosopher[] philosophers = new Philosopher[5];
-
-        for (int i = 0; i < 5; i++) {
-            chopsticks[i] = new Chopstick();
-        }
-
-        for (int i = 0; i < 5; i++) {
-            philosophers[i] = new Philosopher("Philosopher " + (i + 1), chopsticks[i], chopsticks[(i + 1) % 5]);
-            philosophers[i].start();
+    private static synchronized void printEven() {
+        while (number <= 10) {
+            if (number % 2 == 0) {
+                System.out.println(Thread.currentThread().getName() + ": " + number++);
+            }
         }
     }
 }
