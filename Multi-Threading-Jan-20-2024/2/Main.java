1f9e67af17a3ace06c168ca8532259a4b58c9091
MoreCodes <arjunaraneta@gmail.com>
2023-05-05 21:01:10 +1000

    Java Programming Exercises

--- Multi-Threading-Jan-20-2024/2/Main.java
+++ Multi-Threading-Jan-20-2024/2/Main.java
@@ -1,58 +1,21 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Buffer {
-    private Queue<Integer> items = new LinkedList<>();
-    private int capacity;
-
-    public Buffer(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public synchronized void produce(int item) {
-        while (items.size() == capacity) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        items.add(item);
-        System.out.println("Produced: " + item);
-        notify();
-    }
-
-    public synchronized void consume() {
-        while (items.isEmpty()) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        int item = items.poll();
-        System.out.println("Consumed: " + item);
-        notify();
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Buffer buffer = new Buffer(5);
+        Queue<Integer> buffer = new LinkedList<>();
+        Thread producer = new Thread(() -> produce(buffer));
+        Thread consumer = new Thread(() -> consume(buffer));
 
-        Thread producerThread = new Thread(() -> {
-            for (int i = 1; i <= 10; i++) {
-                buffer.produce(i);
-            }
-        }, "ProducerThread");
+        producer.start();
+        consumer.start();
+    }
 
-        Thread consumerThread = new Thread(() -> {
-            for (int i = 1; i <= 10; i++) {
-                buffer.consume();
-            }
-        }, "ConsumerThread");
+    private static void produce(Queue<Integer> buffer) {
+        // Producer logic to add elements to the buffer
+    }
 
-        producerThread.start();
-        consumerThread.start();
+    private static void consume(Queue<Integer> buffer) {
+        // Consumer logic to remove elements from the buffer
     }
 }
