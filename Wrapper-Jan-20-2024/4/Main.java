072334d29575bfffae8ec73aa23f4482d8815fe0
MoreCodes <arjunaraneta@gmail.com>
2023-05-10 20:55:47 +1000

    Java Programming Exercises

--- Wrapper-Jan-20-2024/4/Main.java
+++ Wrapper-Jan-20-2024/4/Main.java
@@ -4,13 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a binary number: ");
-        String binaryInput = scanner.nextLine();
+        String binaryNumber = scanner.nextLine();
 
-        int decimalValue = binaryToDecimal(binaryInput);
-        System.out.println("Decimal equivalent: " + decimalValue);
-    }
+        // TODO: Convert the binary number to decimal using wrapper classes
 
-    private static int binaryToDecimal(String binary) {
-        return Integer.parseInt(binary, 2);
+        System.out.println("Decimal equivalent: " + decimalEquivalent);
     }
 }
