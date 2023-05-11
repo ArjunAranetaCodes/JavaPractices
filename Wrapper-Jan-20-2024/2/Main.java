2311e3e0931d2d84928a20e6ddf0906c0e33a4f6
MoreCodes <arjunaraneta@gmail.com>
2023-05-11 20:55:00 +1000

    Java Programming Exercises

--- Wrapper-Jan-20-2024/2/Main.java
+++ Wrapper-Jan-20-2024/2/Main.java
@@ -4,15 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        int num = scanner.nextInt();
+        int number = scanner.nextInt();
 
-        int reversedNum = reverseNumber(num);
-        System.out.println("Reversed number: " + reversedNum);
-    }
+        // TODO: Reverse the digits of the number using wrapper classes
 
-    private static int reverseNumber(int number) {
-        String numStr = Integer.toString(number);
-        StringBuilder reversedStr = new StringBuilder(numStr).reverse();
-        return Integer.parseInt(reversedStr.toString());
+        System.out.println("Reversed number: " + reversedNumber);
     }
 }
