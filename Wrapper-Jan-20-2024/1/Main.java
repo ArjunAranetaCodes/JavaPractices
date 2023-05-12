d16a6d62393952f2e91bc34d441a8165df49ca10
MoreCodes <arjunaraneta@gmail.com>
2023-05-12 20:54:40 +1000

    Java Programming Exercises

--- Wrapper-Jan-20-2024/1/Main.java
+++ Wrapper-Jan-20-2024/1/Main.java
@@ -4,22 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        int num = scanner.nextInt();
+        int number = scanner.nextInt();
 
-        int digitSum = calculateDigitSum(num);
-        System.out.println("Sum of digits: " + digitSum);
-    }
-
-    private static int calculateDigitSum(int number) {
         int sum = 0;
-        String numStr = Integer.toString(number);
+        // TODO: Calculate the sum of digits using wrapper classes
 
-        for (int i = 0; i < numStr.length(); i++) {
-            char digitChar = numStr.charAt(i);
-            int digit = Character.getNumericValue(digitChar);
-            sum += digit;
-        }
-
-        return sum;
+        System.out.println("Sum of digits: " + sum);
     }
 }
