39511f56bc28dd6f6dad9662ec2f34fe1f804162
MoreCodes <arjunaraneta@gmail.com>
2023-05-17 20:49:34 +1000

    Java Programming Exercises

---
+++ Encapsulation-Jan-20-2024/10/LibraryBook.java
@@ -0,0 +1,20 @@
+public class LibraryBook {
+    private String title;
+    private String author;
+    private boolean available;
+
+    // Constructor and other methods
+
+    public void checkOut() {
+        // Implement book checkout logic
+    }
+
+    public void returnBook() {
+        // Implement book return logic
+    }
+
+    public boolean isAvailable() {
+        // Implement availability check logic
+        return available;
+    }
+}
