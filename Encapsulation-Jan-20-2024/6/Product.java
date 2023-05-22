0aed51a3e141ca574004607e69ca8fec8f791c36
MoreCodes <arjunaraneta@gmail.com>
2023-05-22 20:46:31 +1000

    Java Programming Exercises

---
+++ Encapsulation-Jan-20-2024/6/Product.java
@@ -0,0 +1,16 @@
+public class Product {
+    private int productID;
+    private String name;
+    private double price;
+
+    // Constructor and other methods
+
+    public void setPrice(double price) {
+        // Implement price update logic
+    }
+
+    public double getPrice() {
+        // Implement price retrieval logic
+        return price;
+    }
+}
