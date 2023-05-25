9a819f6e03272c92a7188e0191935279765efa66
MoreCodes <arjunaraneta@gmail.com>
2023-05-25 20:44:59 +1000

    Java Programming Exercises

---
+++ Encapsulation-Jan-20-2024/4/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    private int studentID;
+    private String name;
+    private char grade;
+
+    // Constructor and other methods
+
+    public void updateGrade(char newGrade) {
+        // Implement grade update logic
+    }
+
+        return grade;
+    }
+}
