fad98ec2c00f835f499afafe8ce77fcdcb599892
MoreCodes <arjunaraneta@gmail.com>
2023-05-26 20:44:22 +1000

    Java Programming Exercises

---
+++ Encapsulation-Jan-20-2024/3/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    // Constructor and other methods
+
+    public void setPrice(double price) {
+        // Implement price update logic
+    }
+
+    public double getPrice() {
+        // Implement price retrieval logic
+        return price;
+    }
+}
