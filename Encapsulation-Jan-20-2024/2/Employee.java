1c30f278e77af5b0bd51e3fd69409b963923a5d4
MoreCodes <arjunaraneta@gmail.com>
2023-05-27 20:42:52 +1000

    Java Programming Exercises

---
+++ Encapsulation-Jan-20-2024/2/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    private int employeeID;
+    private String name;
+    private double salary;
+
+    // Constructor and other methods
+
+    public void setSalary(double salary) {
+        // Implement salary update logic
+    }
+
+    public double getSalary() {
+        // Implement salary retrieval logic
+        return salary;
+    }
+}
