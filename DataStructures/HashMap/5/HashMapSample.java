89b9a558b746c92826339e3181560243c598a19e
MoreCodes <arjunaraneta@gmail.com>
2023-07-28 20:17:35 +1000

    Java Programming Exercises

--- DataStructures/HashMap/5/HashMapSample.java
+++ DataStructures/HashMap/5/HashMapSample.java
@@ -1,14 +1,20 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
-public class HashSetSample {
+public class HashMapSample {
     public static void main(String[] args) {
-        HashSet<String> set = new HashSet<>();
-        set.add("Java");
-        set.add("Python");
-        set.add("C++");
+        HashMap<String, Integer> map1 = new HashMap<>();
+        map1.put("A", 1);
+        map1.put("B", 2);
 
-        System.out.println("HashSet elements: " + set);
+        HashMap<String, Integer> map2 = new HashMap<>();
+        map2.put("B", 3);
+        map2.put("C", 4);
 
-        // Other HashSet operations can be performed here
+        // Merging two HashMaps
+        map1.putAll(map2);
+
+        // Displaying the merged HashMap
+        System.out.println("Merged HashMap: " + map1);
     }
 }
