7d40f2a7d540f5c18c4f24d1e31174e9d252d12b
MoreCodes <arjunaraneta@gmail.com>
2023-07-30 18:27:03 +0800

    Java Programming Exercises

---
+++ DataStructures/HashSet/10/HashSetSample.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class HashSetSample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("John");
+        set.add("Jane");
+        set.add("Doe");
+
+        System.out.println("Size of HashSet: " + set.size());
+
+        // Other HashSet operations can be performed here
+    }
+}
