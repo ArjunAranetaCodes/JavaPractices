89f3e28ee9bfea7477a76a72b4a1b94d2be98789
MoreCodes <arjunaraneta@gmail.com>
2023-08-09 18:22:06 +0800

    Java Programming Exercises

--- DataStructures/Queue/10/QueueSample.java
+++ DataStructures/Queue/10/QueueSample.java
@@ -1,5 +1,14 @@
-package DataStructures.Queue.2;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class QueueSample {
-    
+    public static void main(String[] args) {
+        Queue<String> arrayDequeQueue = new ArrayDeque<>();
+
+        arrayDequeQueue.offer("One");
+        arrayDequeQueue.offer("Two");
+        arrayDequeQueue.offer("Three");
+
+        System.out.println("ArrayDeque Queue: " + arrayDequeQueue);
+    }
 }
