4f2ed9cf33c99a5a95ce982b9fd9e2aad9deb3dc
MoreCodes <arjunaraneta@gmail.com>
2023-08-09 18:21:43 +0800

    Java Programming Exercises

--- DataStructures/Queue/8/QueueSample.java
+++ DataStructures/Queue/8/QueueSample.java
@@ -1,5 +1,28 @@
-package DataStructures.Queue.2;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+               "name='" + name + '\'' +
+               '}';
+    }
+}
 
 public class QueueSample {
-    
+    public static void main(String[] args) {
+        Queue<Person> personQueue = new LinkedList<>();
+
+        personQueue.offer(new Person("John"));
+        personQueue.offer(new Person("Alice"));
+
+        System.out.println("Person Queue: " + personQueue);
+    }
 }
