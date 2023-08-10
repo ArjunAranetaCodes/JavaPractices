449b5a61e984ac35844f57d37dd41d9a07ca0f58
MoreCodes <arjunaraneta@gmail.com>
2023-08-10 18:21:14 +0800

    Java Programming Exercises

--- DataStructures/Queue/6/QueueSample.java
+++ DataStructures/Queue/6/QueueSample.java
@@ -1,5 +1,14 @@
-package DataStructures.Queue.2;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class QueueSample {
-    
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Queue<Integer> queue = linkedList;
+
+        queue.offer(10);
+        queue.offer(20);
+
+        System.out.println("Queue: " + queue);
+    }
 }
