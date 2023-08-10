9cf3b0f8d06f6571414da1b6e691400171ae5efc
MoreCodes <arjunaraneta@gmail.com>
2023-08-10 18:21:07 +0800

    Java Programming Exercises

--- DataStructures/Queue/5/QueueSample.java
+++ DataStructures/Queue/5/QueueSample.java
@@ -1,5 +1,13 @@
-package DataStructures.Queue.2;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class QueueSample {
-    
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+
+        System.out.println("Is the queue empty? " + queue.isEmpty());
+
+        queue.offer("Java");
+        System.out.println("Is the queue empty? " + queue.isEmpty());
+    }
 }
