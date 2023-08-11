f2c364ef4d3ed8847a114492ee7102e762859790
MoreCodes <arjunaraneta@gmail.com>
2023-08-11 18:20:46 +0800

    Java Programming Exercises

--- DataStructures/Queue/3/QueueSample.java
+++ DataStructures/Queue/3/QueueSample.java
@@ -1,5 +1,13 @@
-package DataStructures.Queue.2;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class QueueSample {
-    
+    public static void main(String[] args) {
+        Queue<Double> queue = new LinkedList<>();
+
+        queue.offer(3.14);
+        queue.offer(2.71);
+
+        System.out.println("Front element: " + queue.peek());
+    }
 }
