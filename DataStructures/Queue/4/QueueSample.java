effbf76bca9f331d0a00ee6a561bddfdc2485018
MoreCodes <arjunaraneta@gmail.com>
2023-08-11 18:20:54 +0800

    Java Programming Exercises

--- DataStructures/Queue/4/QueueSample.java
+++ DataStructures/Queue/4/QueueSample.java
@@ -1,5 +1,18 @@
-package DataStructures.Queue.2;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class QueueSample {
-    
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer("A");
+        queue.offer("B");
+        queue.offer("C");
+
+        System.out.println("Queue before clearing: " + queue);
+
+        queue.clear();
+
+        System.out.println("Queue after clearing: " + queue);
+    }
 }
