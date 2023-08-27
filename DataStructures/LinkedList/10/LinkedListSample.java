dadb62429c32cce30af5f2b4ecb1da4e149220a9
MoreCodes <arjunaraneta@gmail.com>
2023-08-27 18:08:09 +0800

    Java Programming Exercises

--- DataStructures/LinkedList/10/LinkedListSample.java
+++ DataStructures/LinkedList/10/LinkedListSample.java
@@ -1,12 +1,17 @@
 import java.util.LinkedList;
+import java.util.Collections;
 
 public class LinkedListSample {
     public static void main(String[] args) {
-        LinkedList<String> linkedList = new LinkedList<>();
-        linkedList.add("Java");
-        linkedList.add("is");
-        linkedList.add("awesome");
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
 
-        System.out.println("LinkedList: " + linkedList);
+        System.out.println("Original LinkedList: " + linkedList);
+
+        Collections.reverse(linkedList);
+
+        System.out.println("Reversed LinkedList: " + linkedList);
     }
 }
