b233e4a8da8c35c40b4a8aafdbf0f5ddf8df90c4
MoreCodes <arjunaraneta@gmail.com>
2023-08-28 18:07:34 +0800

    Java Programming Exercises

--- DataStructures/LinkedList/7/LinkedListSample.java
+++ DataStructures/LinkedList/7/LinkedListSample.java
@@ -3,10 +3,9 @@ import java.util.LinkedList;
 public class LinkedListSample {
     public static void main(String[] args) {
         LinkedList<String> linkedList = new LinkedList<>();
-        linkedList.add("Java");
-        linkedList.add("is");
-        linkedList.add("awesome");
 
-        System.out.println("LinkedList: " + linkedList);
+        boolean isEmpty = linkedList.isEmpty();
+
+        System.out.println("Is LinkedList empty? " + isEmpty);
     }
 }
