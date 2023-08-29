2b19d670ba5e5744c8fed5ac38c064e2c76f979f
MoreCodes <arjunaraneta@gmail.com>
2023-08-29 18:06:50 +0800

    Java Programming Exercises

--- DataStructures/LinkedList/3/LinkedListSample.java
+++ DataStructures/LinkedList/3/LinkedListSample.java
@@ -1,12 +1,18 @@
 import java.util.LinkedList;
+import java.util.Iterator;
 
 public class LinkedListSample {
     public static void main(String[] args) {
-        LinkedList<String> linkedList = new LinkedList<>();
-        linkedList.add("Java");
-        linkedList.add("is");
-        linkedList.add("awesome");
+        LinkedList<Double> linkedList = new LinkedList<>();
+        linkedList.add(2.5);
+        linkedList.add(4.7);
+        linkedList.add(6.3);
 
-        System.out.println("LinkedList: " + linkedList);
+        System.out.println("LinkedList Elements:");
+
+        Iterator<Double> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
