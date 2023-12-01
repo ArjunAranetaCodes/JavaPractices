1438c9fe0aab7d0e65122909a0739d728b8800b1
MoreCodes <arjunaraneta@gmail.com>
2023-12-01 22:12:40 +0800

    Lamda Practice

---
+++ Lamdas/12-01-2023/FirstThreeElements.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FirstThreeElements {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> firstThree = numbers.stream().limit(3).collect(Collectors.toList());
+        System.out.println(firstThree);
+    }
+}
