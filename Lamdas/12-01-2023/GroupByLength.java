84679896689689f267fd98be5d6f07aa5cb42bb0
MoreCodes <arjunaraneta@gmail.com>
2023-12-01 22:12:55 +0800

    Lamda Practice

---
+++ Lamdas/12-01-2023/GroupByLength.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupByLength {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("apple", "banana", "kiwi", "grape");
+        Map<Integer, List<String>> groupedByLength = words.stream().collect(Collectors.groupingBy(String::length));
+        System.out.println(groupedByLength);
+    }
+}
