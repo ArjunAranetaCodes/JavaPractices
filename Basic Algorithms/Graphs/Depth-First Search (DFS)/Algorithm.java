32223c5919422bbd4de58687c97265937595d8d1
MoreCodes <arjunaraneta@gmail.com>
2024-01-28 18:16:52 +1100

    Java Programming Exercises

--- Basic Algorithms/Graphs/Depth-First Search (DFS)/Algorithm.java
+++ Basic Algorithms/Graphs/Depth-First Search (DFS)/Algorithm.java
@@ -6,11 +6,6 @@ class Graph {
     private LinkedList<Integer>[] adjacencyList;
 
     public Graph(int vertices) {
-        this.vertices = vertices;
-        adjacencyList = new LinkedList[vertices];
-        for (int i = 0; i < vertices; i++) {
-            adjacencyList[i] = new LinkedList<>();
-        }
     }
 
     public void addEdge(int source, int destination) {
