bccde5d46decf9f0a846bc6f784eccdc91bd6995
MoreCodes <arjunaraneta@gmail.com>
2024-01-29 18:17:05 +1100

    Java Programming Exercises

--- Basic Algorithms/Graphs/Depth-First Search (DFS)/Algorithm.java
+++ Basic Algorithms/Graphs/Depth-First Search (DFS)/Algorithm.java
@@ -14,6 +14,7 @@ class Graph {
     }
 
     public void addEdge(int source, int destination) {
+        adjacencyList[source].add(destination);
     }
 
     public void breadthFirstSearch(int startVertex) {
