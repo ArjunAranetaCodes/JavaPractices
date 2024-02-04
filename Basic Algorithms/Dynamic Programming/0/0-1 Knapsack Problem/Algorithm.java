01679e3e42986667f9411c64e22b15cf09e36712
MoreCodes <arjunaraneta@gmail.com>
2024-02-04 17:55:43 +1100

    Java Programming Exercises

--- Basic Algorithms/Dynamic Programming/0/0-1 Knapsack Problem/Algorithm.java
+++ Basic Algorithms/Dynamic Programming/0/0-1 Knapsack Problem/Algorithm.java
@@ -4,7 +4,7 @@ public class Algorithm {
         int n = weights.length;
         int[][] dp = new int[n + 1][capacity + 1];
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n; i++) { 
             for (int w = 0; w <= capacity; w++) {
                 if (i == 0 || w == 0) {
                     dp[i][w] = 0;
