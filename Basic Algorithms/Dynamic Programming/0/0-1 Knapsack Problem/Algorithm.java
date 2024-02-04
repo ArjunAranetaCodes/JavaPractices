f6e246977faf41be7938faae946d842a10b4c472
MoreCodes <arjunaraneta@gmail.com>
2024-02-04 17:55:41 +1100

    Java Programming Exercises

--- Basic Algorithms/Dynamic Programming/0/0-1 Knapsack Problem/Algorithm.java
+++ Basic Algorithms/Dynamic Programming/0/0-1 Knapsack Problem/Algorithm.java
@@ -6,6 +6,13 @@ public class Algorithm {
 
         for (int i = 0; i <= n; i++) {
             for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
             }
         }
 
