237ffd120b0e5a09d38650656b535c458996bcf0
Arjun Araneta <arjunaraneta@gmail.com>
2024-10-24 00:48:55 +0800

    Create Algorithm.java

---
+++ Codings/Basic Algorithms/Graphs/Breadth-First Search (BFS)/Algorithm.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Algorithm {
+    private int vertices;
+    private LinkedList<Integer> adjacencyList[];
+
+    public Algorithm(int vertices) {
+        this.vertices = vertices;
+        adjacencyList = new LinkedList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int vertex, int neighbor) {
+        adjacencyList[vertex].add(neighbor);
+    }
+
+    public void DFS(int startVertex) {
+        boolean visited[] = new boolean[vertices];
+        DFSUtil(startVertex, visited);
+    }
+
+    private void DFSUtil(int vertex, boolean visited[]) {
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+
+        for (int neighbor : adjacencyList[vertex]) {
+            if (!visited[neighbor]) {
+                DFSUtil(neighbor, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Algorithm graph = new Algorithm(7);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+
+        System.out.println("Depth-First Search starting from vertex 0:");
+        graph.DFS(0);
+    }
+}
