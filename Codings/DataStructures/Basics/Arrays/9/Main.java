45194c49d44fd47a543d9dcee75047a644a70b1b
Arjun Araneta <arjunaraneta@gmail.com>
2024-11-28 13:39:17 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Arrays/9/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 8, 2, 6, 10, 12, 14, 8};
+        int[] uniqueNumbers = Arrays.stream(numbers).distinct().toArray();
+
+        System.out.println("Original Array: " + Arrays.toString(numbers));
+        System.out.println("Array with Duplicates Removed: " + Arrays.toString(uniqueNumbers));
+    }
+}
