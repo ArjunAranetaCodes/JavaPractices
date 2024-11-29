bb07bc0c2f6e7fd9163e9bbcda18b78972cfda51
Arjun Araneta <arjunaraneta@gmail.com>
2024-11-29 13:24:12 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Conditions/6/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double number = 3.75;
+
+        // Round to the nearest integer
+        long roundedNumber = Math.round(number);
+        System.out.println("Rounded value of " + number + ": " + roundedNumber);
+    }
+}
