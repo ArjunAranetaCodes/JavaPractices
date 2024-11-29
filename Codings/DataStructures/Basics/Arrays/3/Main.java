eab00e0f938618b6595af191932e77200e7f6af7
Arjun Araneta <arjunaraneta@gmail.com>
2024-11-29 13:23:56 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Arrays/3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange", "Mango"};
+
+        System.out.println("Fruits in the array:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
