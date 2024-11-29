8a2d9fa3102e5e3636f89e94bbf2e07932ee96bd
Arjun Araneta <arjunaraneta@gmail.com>
2024-11-29 13:23:58 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Conditions/10/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double base = 2;
+
+        double result = Math.exp(base);
+        System.out.println("e raised to the power of " + base + ": " + result);
+    }
+}
