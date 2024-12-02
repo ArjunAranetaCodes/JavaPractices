ed3423a60e978b4ff5a424865b2e9f92ed8e4e2e
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-03 06:26:46 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Math/6/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int marks = 75;
+
+        if (marks >= 90 || marks <= 100) {
+            System.out.println("Excellent performance!");
+        } else {
+            System.out.println("Good performance.");
+        }
+    }
+}
