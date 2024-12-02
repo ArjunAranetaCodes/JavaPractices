3d8b38968b991170fdffa643e67ca3d2564a5981
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-03 06:26:49 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Math/5/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 25;
+
+        if (number > 0 && number % 2 == 0) {
+            System.out.println("The number is a positive even integer.");
+        } else {
+            System.out.println("The number is either not positive or not even.");
+        }
+    }
+}
