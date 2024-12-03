6faf1e7f97189f63d65e78e3cb63a78eb4ab95b5
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-04 07:35:00 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Operators/6/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 8;
+
+        int max = (a > b) ? a : b;
+
+        System.out.println("Maximum value: " + max);
+    }
+}
