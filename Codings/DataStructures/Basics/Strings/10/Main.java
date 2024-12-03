38be9525dbb7d298865090068e01828aa024d818
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-04 07:35:17 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Strings/10/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Hello");
+        stringBuilder.append(" World");
+        String result = stringBuilder.toString();
+        System.out.println("Result: " + result);
+    }
+}
