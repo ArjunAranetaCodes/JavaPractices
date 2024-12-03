b38ee4963910fa77612054933e329131971440cf
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-04 07:34:57 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Operators/2/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5, y = 8;
+
+        System.out.println("Is x equal to y? " + (x == y));
+        System.out.println("Is x not equal to y? " + (x != y));
+        System.out.println("Is x greater than y? " + (x > y));
+        System.out.println("Is x less than y? " + (x < y));
+        System.out.println("Is x greater than or equal to y? " + (x >= y));
+        System.out.println("Is x less than or equal to y? " + (x <= y));
+    }
+}
