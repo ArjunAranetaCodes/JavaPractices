00e9dfc704c33a40870d94e1ee85306f0fd49e59
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-06 01:48:37 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Switch/9/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        long number = 123456789012345L;
+
+        switch ((int) (number % 10)) {
+            case 1:
+                System.out.println("Ends with 1");
+                break;
+            case 2:
+                System.out.println("Ends with 2");
+                break;
+            // Add cases for other digits as needed
+            default:
+                System.out.println("Other digit");
+        }
+    }
+}
