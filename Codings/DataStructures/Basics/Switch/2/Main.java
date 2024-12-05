27e591cc960cf11c2d7174b01974d8e5998d4dce
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-06 01:48:21 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Switch/2/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String fruit = "Apple";
+
+        switch (fruit) {
+            case "Apple":
+                System.out.println("It's an apple");
+                break;
+            case "Banana":
+                System.out.println("It's a banana");
+                break;
+            // Add cases for other fruits as needed
+            default:
+                System.out.println("Unknown fruit");
+        }
+    }
+}
