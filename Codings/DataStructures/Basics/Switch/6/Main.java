67f9fec357287742a035875ce545377fe628b6cc
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-06 01:48:26 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Switch/6/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    enum Day { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY }
+
+    public static void main(String[] args) {
+        Day today = Day.WEDNESDAY;
+
+        switch (today) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+            case FRIDAY:
+                System.out.println("It's a weekday");
+                break;
+            case SATURDAY:
+            case SUNDAY:
+                System.out.println("It's a weekend");
+                break;  
+                
+        }
+    }
+}
