d5014df00386a251244a661bec576196b36bcd44
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-08 01:07:03 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/TypeCasting/7/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        char charValue = 'A';
+        int intValue = (int) charValue; // Explicit casting from char to int
+        System.out.println("Int Value: " + intValue);
+    }
+}
