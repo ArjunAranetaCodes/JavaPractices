b260188dc6e5b2fd07f754f9c8d44fba54537b4c
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-07 15:45:33 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/TypeCasting/2/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double doubleValue = 15.75;
+        int intValue = (int) doubleValue; // Explicit casting from double to int
+        System.out.println("Int Value: " + intValue);
+    }
+}
