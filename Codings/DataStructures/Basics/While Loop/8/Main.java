58d38c375e3097c4ecc48cf10f1059a0340bd32c
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-10 11:55:29 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/While Loop/8/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 17;
+        boolean isPrime = true;
+        int i = 2;
+        while (i <= num / 2) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+            i++;
+        }
+        
+        System.out.println(num + " is prime: " + isPrime);
+    }
+}
