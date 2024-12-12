36f8ad1accca566d605be52de61dfc25b5a950e2
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-12 11:15:43 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/While Loop/9/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 98765;
+        int count = 0;
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+        System.out.println("Number of digits: " + count);
+    }
+}
