f4cb6a73e4901345dd846e870a1f5501c220566a
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-12 11:16:01 +0800

    Create HashSetSample.java

---
+++ Codings/DataStructures/HashSet/2/HashSetSample.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class HashSetSample {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+
+        System.out.println("HashSet elements: " + set);
+
+        // Other HashSet operations can be performed here
+    }
+}
