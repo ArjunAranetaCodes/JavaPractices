71494ab126f324ef1412e4f74a90930e34875c90
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-12 11:15:55 +0800

    Create HashSetSample.java

---
+++ Codings/DataStructures/HashSet/1/HashSetSample.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class HashSetSample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("Java");
+        set.add("Python");
+        set.add("C++");
+
+        System.out.println("HashSet elements: " + set);
+
+        // Other HashSet operations can be performed here
+    }
+}
