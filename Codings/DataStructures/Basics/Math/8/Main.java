3c857d794efe4237fbc3a712e221a182a10b9d46
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-12 11:15:44 +0800

    Create Main.java

---
+++ Codings/DataStructures/Basics/Math/8/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = null;
+
+        if (name == null) {
+            System.out.println("Name is not provided.");
+        } else {
+            System.out.println("Name: " + name);
+        }
+    }
+}
