91c50d2d3951645ecdc5fb1b125c09ec5122cb96
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-14 15:40:00 +0800

    Create LinkedListSample.java

---
+++ Codings/DataStructures/LinkedList/1/LinkedListSample.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinkedListSample {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("Java");
+        linkedList.add("is");
+        linkedList.add("awesome");
+
+        System.out.println("LinkedList: " + linkedList);
+    }
+}
