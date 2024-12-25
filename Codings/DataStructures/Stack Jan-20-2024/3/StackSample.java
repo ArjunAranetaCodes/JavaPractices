a06193774435cdadc9ec455df6ff4d9cbdfa0885
Arjun Araneta <arjunaraneta@gmail.com>
2024-12-25 13:31:38 +1100

    Create StackSample.java

---
+++ Codings/DataStructures/Stack Jan-20-2024/3/StackSample.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class StackSample {
+    public static void main(String[] args) {
+        String infixExpression = "3 + 5 * ( 2 - 8 ) / 2";
+        System.out.println("Postfix Expression: " + infixToPostfix(infixExpression));
+    }
+
+    private static String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : infix.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                postfix.append(ch);
+            } else if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop(); // Pop '('
+            } else {
+                while (!stack.isEmpty() && getPrecedence(ch) <= getPrecedence(stack.peek())) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(ch);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+
+        return postfix.toString();
+    }
+
+    private static int getPrecedence(char operator) {
+        if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+        return 0;
+    }
+}
