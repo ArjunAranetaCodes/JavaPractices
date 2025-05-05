6931f86227841305961993567921c259a745cab8
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-06 08:25:20 +1000

    Create LinkedListSample.java

---
+++ Codings/DataStructures/LinkedList Jan-20-2024/1/LinkedListSample.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinkedListSample {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Orange");
+
+        System.out.println("LinkedList: " + linkedList);
+    }
+}
