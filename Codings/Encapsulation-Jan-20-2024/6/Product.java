fe8bb3eb53151792e11d8cd72218b688cb1cd5aa
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-15 07:58:49 +1000

    Create Product.java

---
+++ Codings/Encapsulation-Jan-20-2024/6/Product.java
@@ -0,0 +1,15 @@
+public class Product {
+    private int productID;
+    private String name;
+    private double price;
+
+    // Constructor and other methods
+
+    public void setPrice(double price) {
+        // Implement price update logic
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
