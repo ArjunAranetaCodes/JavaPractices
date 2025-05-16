62cef04d1363f829071222a54ce1987021ce0e5c
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-16 17:51:57 +1000

    Create Student.java

---
+++ Codings/Encapsulation-Jan-20-2024/4/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    private int studentID;
+    private String name;
+    private char grade;
+
+    // Constructor and other methods
+
+    public void updateGrade(char newGrade) {
+        // Implement grade update logic
+    }
+
+    public char getGrade() {
+        // Implement grade retrieval logic
+        return grade;
+    }
+}
