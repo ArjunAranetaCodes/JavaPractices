551dfe16bff07cc5613d40e9e5f6be244ff399cf
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-16 17:51:52 +1000

    Create TemperatureConverter.java

---
+++ Codings/Encapsulation-Jan-20-2024/5/TemperatureConverter.java
@@ -0,0 +1,24 @@
+public class TemperatureConverter {
+    private double celsius;
+    private double fahrenheit;
+
+    // Constructor and other methods
+
+    public void setCelsius(double celsius) {
+        // Implement Celsius update logic
+    }
+
+    public double getCelsius() {
+        // Implement Celsius retrieval logic
+        return celsius;
+    }
+
+    public void setFahrenheit(double fahrenheit) {
+        // Implement Fahrenheit update logic
+    }
+
+    public double getFahrenheit() {
+        // Implement Fahrenheit retrieval logic
+        return fahrenheit;
+    }
+}
