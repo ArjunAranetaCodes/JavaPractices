21f3805630eebbc1892a64bac0f67b0da69ed185
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-17 10:37:59 +1000

    Create Main.java

---
+++ Codings/Generics-Jan-25-2024/3/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Pair<String, Integer> pair = new Pair<>("John", 25);
+        System.out.println(pair.getFirst());   // Output: John
+        System.out.println(pair.getSecond());  // Output: 25
+    }
+}
+
+class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }   
+}
