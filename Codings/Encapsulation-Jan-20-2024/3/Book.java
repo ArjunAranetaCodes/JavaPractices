ff4ff9353fd993af29959dafda8f9cf3a0ca64f3
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-22 10:30:46 +1000

    Create Book.java

---
+++ Codings/Encapsulation-Jan-20-2024/3/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    // Constructor and other methods
+
+    public void setPrice(double price) {
+        // Implement price update logic
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
