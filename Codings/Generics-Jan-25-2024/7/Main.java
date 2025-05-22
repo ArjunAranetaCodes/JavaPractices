6efee5e17255c6ac1d76c08902168228c11d1a78
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-22 10:31:05 +1000

    Create Main.java

---
+++ Codings/Generics-Jan-25-2024/7/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
+        printArray(intArray);    // Output: 1 2 3 4
+        printArray(doubleArray); // Output: 1.1 2.2 3.3 4.4
+    }
+
+    static <T extends Number> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
+    
