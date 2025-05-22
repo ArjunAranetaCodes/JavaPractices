c4d6865e15e29eff25e26b49007540e166bc4ce5
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-22 10:30:55 +1000

    Create LibraryBook.java

---
+++ Codings/Encapsulation-Jan-20-2024/10/LibraryBook.java
@@ -0,0 +1,15 @@
+public class LibraryBook {
+    private String title;
+    private String author;
+    private boolean available;
+
+    public void checkOut() {
+    }
+
+    public void returnBook() {
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+}
