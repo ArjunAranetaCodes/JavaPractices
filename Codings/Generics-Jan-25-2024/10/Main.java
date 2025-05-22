f17eb0a6640a044c8d98347e8061f7832d3d3b75
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-22 10:31:09 +1000

    Create Main.java

---
+++ Codings/Generics-Jan-25-2024/10/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Circle Area: " + circle.calculateArea());       // Output: Circle Area: 78.53981633974483
+        System.out.println("Rectangle Area: " + rectangle.calculateArea()); // Output: Rectangle Area: 24.0
+    }
+}
+
+abstract class Shape<T extends Number> {
+    abstract T calculateArea();
+}
+
+class Circle extends Shape<Double> {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override 
+    Double calculateArea() {
+        return Math.PI * radius * radius;
+    } 
+}
+
+class
