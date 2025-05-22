35c2fac52736469e9a13821b3aae830bc9eb28e3
Arjun Araneta <arjunaraneta@gmail.com>
2025-05-22 10:30:50 +1000

    Create Employee.java

---
+++ Codings/Encapsulation-Jan-20-2024/2/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    private int employeeID;
+    private String name;
+    private double salary;
+
+    // Constructor and other methods
+
+    public void setSalary(double salary) {
+        // Implement salary update logic
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
