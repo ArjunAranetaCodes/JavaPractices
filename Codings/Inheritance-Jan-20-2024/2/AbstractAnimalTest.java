bbfcad4eb97dfd51fb469a6f59be19871220798d
Arjun Araneta <arjunaraneta@gmail.com>
2025-06-05 06:35:46 +1000

    Create AbstractAnimalTest.java

---
+++ Codings/Inheritance-Jan-20-2024/2/AbstractAnimalTest.java
@@ -0,0 +1,30 @@
+abstract class Animal {
+    String name;
+
+    abstract void makeSound();
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+    }
+}
+
+class Dog extends Animal {
+    String breed;
+
+    Dog(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+class AbstractAnimalTest {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy", "Labrador");
+        dog.displayInfo();
+        dog.makeSound();
+    }
+}
