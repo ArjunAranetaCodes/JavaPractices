5ec9d0267558fc4354de1af3d33cc9c6e261ca9d
Arjun Araneta <arjunaraneta@gmail.com>
2025-06-14 08:32:22 +1000

    Create Main.java

---
+++ Codings/Keywords/break/3/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 5) {
+            System.out.println("Count: " + count);
+            if (count == 2) {
+                System.out.println("Breaking the loop at count 2");
+                break;
+            }
+            count++; 
+        }
+    }
+}
