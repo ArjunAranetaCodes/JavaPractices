a08176c7432c7f7928e700faac0a308a634230d6
Arjun Araneta <arjunaraneta@gmail.com>
2025-06-13 17:35:31 +1000

    Create Main.java

---
+++ Codings/Keywords/case/1/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int dayOfWeek = 3;
+
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
