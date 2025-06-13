e3237dee1330c6fe34566fa83d7681f3a66132b1
Arjun Araneta <arjunaraneta@gmail.com>
2025-06-14 08:32:41 +1000

    Create Main.java

---
+++ Codings/Keywords/boolean/3/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        boolean isAdult = (age >= 18);
+
+        if (isAdult) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not yet an adult.");
+        }
+    }
+}
