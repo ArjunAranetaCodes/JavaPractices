a8bc3bdf9db23729e578704be77d30c7ab40132f
Arjun Araneta <arjunaraneta@gmail.com>
2025-06-14 08:32:38 +1000

    Create Main.java

---
+++ Codings/Keywords/boolean/5/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean sunny = true;
+        boolean warm = false;
+
+        boolean goToBeach = sunny && warm;
+        boolean stayHome = !goToBeach;
+
+        System.out.println("Go to the beach? " + goToBeach);
+        System.out.println("Stay home? " + stayHome);
+    }
+}
