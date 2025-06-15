503c02d92969ed6e73fd90d70556cbf87ebfcad9
Arjun Araneta <arjunaraneta@gmail.com>
2025-06-15 13:54:13 +1000

    Create Main.java

---
+++ Codings/Keywords/case/3/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String color = "red";
+
+        switch (color) {
+            case "red":
+                System.out.println("The color is red");
+                break;
+            case "blue":
+                System.out.println("The color is blue");
+                break;
+            default:
+                System.out.println("Unknown color");
+        }
+    }
+}
