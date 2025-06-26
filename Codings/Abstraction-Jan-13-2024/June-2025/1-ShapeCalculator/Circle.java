6ed7e8b408b51503fb255387ea635239ca99108c
Arjun Araneta <arjunaraneta@gmail.com>
2025-06-26 10:26:28 +1000

    Create Circle.java

---
+++ Codings/Abstraction-Jan-13-2024/June-2025/1-ShapeCalculator/Circle.java
@@ -0,0 +1,28 @@
+package ShapeCalculator;
+
+public class Circle extends Shape {
+    private double radius;
+    
+    public Circle(String name, String color, double radius) {
+        super(name, color);
+        this.radius = radius;
+    }
+    
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+    
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+} 
