c3d75d9fbcd9362bb860fe2da5a880a3ae5b083e
Arjun Araneta <arjunaraneta@gmail.com>
2025-07-23 07:00:24 +1000

    Create PasswordValidator.java

---
+++ Codings/Encapsulation/Encapsulation-Jan-20-2024/7/PasswordValidator.java
@@ -0,0 +1,14 @@
+public class PasswordValidator {
+    private String password;
+
+    // Constructor and other methods
+
+    public void setPassword(String password) {
+        // Implement password validation logic
+    }
+
+    public boolean isValidPassword() {
+        // Implement password validation check
+        return false;
+    }
+}
