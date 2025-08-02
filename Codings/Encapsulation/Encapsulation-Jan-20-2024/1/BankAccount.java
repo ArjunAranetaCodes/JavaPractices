d776eade82dcff43ecdfd4294d63890955d59d9e
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-02 15:56:48 +1000

    Create BankAccount.java

---
+++ Codings/Encapsulation/Encapsulation-Jan-20-2024/1/BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+    private String accountHolderName;
+
+    // Constructor and other methods
+
+    public void deposit(double amount) {
+        // Implement deposit logic
+    }
+
+    public void withdraw(double amount) {
+        // Implement withdrawal logic
+    }
+
+    public double getBalance() {
+        // Implement balance retrieval logic
+        return balance;
+    }
+}
