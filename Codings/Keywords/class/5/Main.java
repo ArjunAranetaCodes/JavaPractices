cf8ca330560ebfa51f25ec31e6c8529ca5414743
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-24 17:29:24 +1000

    Create Main.java

---
+++ Codings/Keywords/class/5/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Simple if-else statement
+        int number = 8;
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
