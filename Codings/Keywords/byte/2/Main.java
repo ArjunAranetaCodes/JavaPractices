e409c768470e9fd0d810b12a38efeaeaec5693ab
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-24 17:29:03 +1000

    Create Main.java

---
+++ Codings/Keywords/byte/2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        byte num1 = 50;
+        byte num2 = 30;
+        byte sum = (byte) (num1 + num2);
+        System.out.println("Sum of bytes: " + sum);
+    }
+}
