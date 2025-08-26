3377f45e83e2f1e506d8e2c798cf2535f9600c4e
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-26 12:19:21 +1000

    Create Main.java

---
+++ Codings/Keywords/const/2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Result: " + calculateConstant());
+    }
+ 
+    private static final int calculateConstant() {
+        return 5 * 8;
+    }
+}
