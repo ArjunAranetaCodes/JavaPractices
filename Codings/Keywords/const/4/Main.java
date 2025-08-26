af5e337374f6bffb07a8b342e99da9e0595126dd
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-26 12:19:17 +1000

    Create Main.java

---
+++ Codings/Keywords/const/4/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        final int[] MY_ARRAY = {1, 2, 3, 4, 5};
+        System.out.println("Array elements: " + Arrays.toString(MY_ARRAY));
+    }
+}
