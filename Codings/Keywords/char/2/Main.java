2f4119a0a867df43fde5454e6503f9972456aec9
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-27 08:43:22 +1000

    Create Main.java

---
+++ Codings/Keywords/char/2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char firstChar = 'A';
+        char secondChar = 'B';
+        char result = (char) (firstChar + secondChar);
+        System.out.println("Result of char arithmetic: " + result);
+    }
+}
