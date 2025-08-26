81b934b7f10e523ddc34bcc4460b44d2fd5ff1f9
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-26 12:19:10 +1000

    Create Main.java

---
+++ Codings/Keywords/continue/1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                System.out.println("Skipping iteration " + i);
+                continue;
+            }
+            System.out.println("Executing iteration " + i);
+        }
+    }
+}
