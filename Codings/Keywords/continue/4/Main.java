34be8877408a4dcaaeb21c04bf13ea43d4bcbf12
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-26 12:19:24 +1000

    Create Main.java

---
+++ Codings/Keywords/continue/4/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        outerLoop:
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (j == 2) {
+                    System.out.println("Skipping inner loop iteration " + j + " in outer loop " + i);
+                    continue;
+                }
+                System.out.println("Executing inner loop iteration " + j + " in outer loop " + i);
+            }
+        }
+    }
+}
