b1ac4bb04952d45ffbd10515ab87cc447410ed94
Arjun Araneta <arjunaraneta@gmail.com>
2025-08-28 07:44:05 +1000

    Create Main.java

---
+++ Codings/Keywords/else/2/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        boolean hasID = true;
+
+        if (age >= 18) {
+            if (hasID) {
+                System.out.println("You can enter the venue.");
+            } else {
+                System.out.println("You need to show ID for entry.");
+            }
+        } else {
+            System.out.println("You are underage, cannot enter.");
+        }
+    }
+}
