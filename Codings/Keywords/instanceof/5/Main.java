d79debe6a90419debbb6fad1e4d7518681951174
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-06 07:57:23 +1000

    Create Main.java

---
+++ Codings/Keywords/instanceof/5/Main.java
@@ -0,0 +1,23 @@
+class Vehicle {
+    void start() {
+        System.out.println("Vehicle starting...");
+    }
+}
+
+class Car extends Vehicle {
+    void drive() {
+        System.out.println("Car driving...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car();
+
+        if (myCar instanceof Car) {
+            ((Car) myCar).drive();
+        } else {
+            System.out.println("Not a valid operation on this vehicle type");
+        }
+    }
+}
