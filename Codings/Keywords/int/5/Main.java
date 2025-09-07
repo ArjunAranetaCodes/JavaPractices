de44cf42f494d9cefe54f378bb225fcf8ba7b766
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-07 12:19:23 +1000

    Create Main.java

---
+++ Codings/Keywords/int/5/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Printing integers from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
