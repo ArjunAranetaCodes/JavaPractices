cd733fac5e5970344ae5f536e1b5191708fffdda
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-07 12:19:16 +1000

    Create Main.java

---
+++ Codings/Keywords/interface/3/Main.java
@@ -0,0 +1,22 @@
+// Program 3
+interface MyInterface {
+    void myMethod();
+
+    default void defaultMethod() {
+        System.out.println("Default implementation of defaultMethod");
+    }
+}
+
+class MyClass implements MyInterface {
+    public void myMethod() {
+        System.out.println("Implementation of myMethod");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.myMethod();
+        obj.defaultMethod();
+    }
+}
