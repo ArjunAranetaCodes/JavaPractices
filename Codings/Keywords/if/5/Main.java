2b48a16d68b4041c9eb8bf894cc17b8d60aa40a4
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-07 12:19:01 +1000

    Create Main.java

---
+++ Codings/Keywords/if/5/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 8;
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println("The number is " + result + "."); 
+    }
+}
