e6bba63107b4fd91a6feaca397b8fdd50416803d
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-10 07:27:50 +1000

    Create Main.java

---
+++ Codings/Keywords/protected/3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    protected static String protectedString = "Hello, protected world!";
+
+    public static void main(String[] args) {
+        System.out.println("Protected String: " + protectedString);
+    }
+}
+    
