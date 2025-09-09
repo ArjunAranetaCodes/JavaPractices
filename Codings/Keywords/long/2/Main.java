146f1f4240912c9fffdc1557fe4ddbef4fddf727
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-09 21:02:24 +1000

    Create Main.java

---
+++ Codings/Keywords/long/2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long num1 = 1000000L;
+        long num2 = 500000L;
+        long result = num1 + num2;
+        System.out.println("Result of addition: " + result);
+    }
+}
