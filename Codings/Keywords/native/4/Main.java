2d7a5a5e910f84c15fceb89d7ed5a563b082d5d9
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-09 21:02:46 +1000

    Create Main.java

---
+++ Codings/Keywords/native/4/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public native void myNativeMethod(); 
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.myNativeMethod(); 
+    }
+
+    static {
+        System.loadLibrary("MyLibrary");
+    }
+}
