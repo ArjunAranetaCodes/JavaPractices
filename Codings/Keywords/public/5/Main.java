64bdda9a3d3e7258f5ae22176793e89b7d6421ff
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-15 07:25:37 +1000

    Create Main.java

---
+++ Codings/Keywords/public/5/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.print("Numbers: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
+    
