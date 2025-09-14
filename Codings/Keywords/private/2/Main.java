8948856cc348733933bf57cc6d27df46a6120da3
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-15 07:25:10 +1000

    Create Main.java

---
+++ Codings/Keywords/private/2/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    private void displayMessage() {
+        System.out.println("This is a private method.");
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.displayMessage();
+    }
+}
