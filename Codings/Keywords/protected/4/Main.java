d188c13e786ab39e947e09f3f02c566feaeadce0
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-14 13:51:27 +1000

    Create Main.java

---
+++ Codings/Keywords/protected/4/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    protected class ProtectedInnerClass {
+        public void display() {
+            System.out.println("Inside the protected inner class.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Main mainObj = new Main();
+        ProtectedInnerClass innerObj = mainObj.new ProtectedInnerClass();
+        innerObj.display();
+    } 
+}
+ 
