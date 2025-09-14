9b9a92a7b09ca769acb5eecea6f2ee6432d7abd3
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-14 13:51:25 +1000

    Create Main.java

---
+++ Codings/Keywords/protected/5/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    protected Main() {
+        System.out.println("Protected constructor called.");
+    }
+
+    public static void main(String[] args) {
+        Main mainObj = new Main();
+    }
+}
+
+   
