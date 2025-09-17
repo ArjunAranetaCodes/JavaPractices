3cb8930815d50f3df8de1d0a149e6285214249cb
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-18 09:20:49 +1000

    Create Main.java

---
+++ Codings/Keywords/strictfp/2/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    strictfp static double calculateSquare(double num) {
+        return num * num;
+    }
+
+    strictfp static float calculateCube(float num) {
+        return num * num * num;
+    }
+
+    public static void main(String[] args) {
+        double squareResult = calculateSquare(3.5);
+        float cubeResult = calculateCube(2.0f);
+
+        System.out.println("Square Result: " + squareResult);
+        System.out.println("Cube Result: " + cubeResult); 
+    }
+}
+    
