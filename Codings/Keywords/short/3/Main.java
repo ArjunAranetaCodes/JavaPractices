580c02c2063613b1825c40d177a5e32006566dda
Arjun Araneta <arjunaraneta@gmail.com>
2025-09-19 08:19:42 +1000

    Create Main.java

---
+++ Codings/Keywords/short/3/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int intValue = 200;
+        short shortValue = (short) intValue;
+        System.out.println("Short value: " + shortValue); 
+    }
+}
