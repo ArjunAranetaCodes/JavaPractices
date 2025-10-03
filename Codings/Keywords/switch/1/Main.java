5e3ff1ffdc7b856fb528701a4276c5f99f319707
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-03 23:03:46 +1000

    Create Main.java

---
+++ Codings/Keywords/switch/1/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int choice = 2;
+
+        switch (choice) {
+            case 1:
+                System.out.println("Choice is 1");
+                break;
+            case 2:
+                System.out.println("Choice is 2");
+                break;
+            case 3:
+                System.out.println("Choice is 3");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+}
