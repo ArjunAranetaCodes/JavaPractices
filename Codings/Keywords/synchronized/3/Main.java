3d51263b3e05986e6602152eb43db3833bbfc13b
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-04 11:43:15 +1000

    Create Main.java

---
+++ Codings/Keywords/synchronized/3/Main.java
@@ -0,0 +1,13 @@
+class SharedResource {
+    synchronized static void synchronizedStaticMethod() {
+        // Code that needs synchronization
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        SharedResource.synchronizedStaticMethod();
+    }
+}
+   
