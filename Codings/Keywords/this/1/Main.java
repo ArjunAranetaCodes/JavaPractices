b57596cb38786e45a08b722632324b5b794e7b7a
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-04 11:43:21 +1000

    Create Main.java

---
+++ Codings/Keywords/this/1/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    private int number;
+
+    public Main(int number) {
+        this.number = number;
+    }
+
+    public void displayNumber() {
+        System.out.println("Number: " + this.number);
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main(42);  
+        obj.displayNumber();
+    }  
+
+}
