3922703880104caae36730888a2aee68e201fb7d
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-04 11:43:25 +1000

    Create Main.java

---
+++ Codings/Keywords/synchronized/5/Main.java
@@ -0,0 +1,35 @@
+class SharedResource {
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
+
+    void method1() {
+        // Code without synchronization
+
+        synchronized (lock1) {
+            // Code that needs synchronization with lock1
+        }
+
+        // Code without synchronization
+    }
+
+    void method2() {
+        // Code without synchronization
+
+        synchronized (lock2) {
+            // Code that needs synchronization with lock2
+        }
+
+        // Code without synchronization
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        SharedResource sharedResource = new SharedResource();
+
+        synchronized (sharedResource) {
+            sharedResource.method1();
+            sharedResource.method2();
+        }
+    }
+}
