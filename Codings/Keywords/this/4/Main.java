21b300fde566e4022d6aeca9fe64acbbd53ac938
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-04 11:43:30 +1000

    Create Main.java

---
+++ Codings/Keywords/this/4/Main.java
@@ -0,0 +1,22 @@
+class Main {
+    private int value;
+
+    public Main(int value) {
+        this.value = value;
+    }
+
+    public void printValue() {
+        System.out.println("Value: " + this.value);
+    }
+
+    public void processValue(int newValue) {
+        this.value = newValue;
+        this.printValue();
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main(10);
+        obj.processValue(20);
+    }
+}
+ 
