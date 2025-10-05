b7f36e98564ff1123042a498124ca423149a2395
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-05 20:32:06 +1100

    Create Main.java

---
+++ Codings/Keywords/this/3/Main.java
@@ -0,0 +1,18 @@
+class Main {
+    private String message;
+
+    public Main setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public void displayMessage() {
+        System.out.println("Message: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.setMessage("Hello, ").displayMessage();
+    }
+}
+   
