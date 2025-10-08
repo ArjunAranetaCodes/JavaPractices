1fbf917d61e781fec2add7f2e54d486480deb020
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-08 19:32:44 +1100

    Create Main.java

---
+++ Codings/Keywords/try/2/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; // ArithmeticException
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
+ 
