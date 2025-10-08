547149e1f4ece46efbb90531efad0e1ccdd7caed
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-09 09:20:11 +1100

    Create Main.java

---
+++ Codings/Keywords/var/5/Main.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        var names = List.of("Alice", "Bob", "Charlie");
+        for (var name : names) {
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+}
+   
