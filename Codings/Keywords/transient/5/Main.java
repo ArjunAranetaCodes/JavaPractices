da543efecde34867745d87a91e1ee6374576f66a
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-08 19:32:47 +1100

    Create Main.java

---
+++ Codings/Keywords/transient/5/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class MyClass implements Serializable {
+    transient int[] transientArray = {1, 2, 3};
+    int[] nonTransientArray = {4, 5, 6};
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        System.out.println("Transient Array: " + Arrays.toString(obj.transientArray));
+        System.out.println("Non-Transient Array: " + Arrays.toString(obj.nonTransientArray));
+    }
+}
+ 
