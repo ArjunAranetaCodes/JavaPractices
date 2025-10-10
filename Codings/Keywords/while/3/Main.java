ddb504a20b671075731d899db22d067e297c9645
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-10 16:16:19 +1100

    Create Main.java

---
+++ Codings/Keywords/while/3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 3) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
+}
+ 
