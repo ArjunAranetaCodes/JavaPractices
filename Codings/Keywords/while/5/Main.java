096239e1e2af44d02a16ad8cf7074dc209d93423
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-10 16:16:15 +1100

    Create Main.java

---
+++ Codings/Keywords/while/5/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int index = 0;
+
+        while (index < numbers.length) {
+            System.out.println("Number at index " + index + ": " + numbers[index]);
+            index++;
+        }
+    }
+}
+  
