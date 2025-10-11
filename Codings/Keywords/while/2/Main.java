4139a900d06ed40cbd7d4484b9dd5250c8f9b695
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-11 13:54:40 +1100

    Create Main.java

---
+++ Codings/Keywords/while/2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (true) {
+            System.out.println("Iteration: " + i);
+            i++;
+            if (i > 5) {
+                break;
+            }
+        }
+    }
+}
+   
