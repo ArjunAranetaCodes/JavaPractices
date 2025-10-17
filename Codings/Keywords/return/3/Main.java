2ae8604ee5478845261b87ee41f8bad9b625f85d
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-17 13:20:12 +1100

    Create Main.java

---
+++ Codings/Keywords/return/3/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int factorial = calculateFactorial(5);
+        System.out.println("Factorial: " + factorial);
+    }
+
+    static int calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1); 
+        }
+    }
+}
