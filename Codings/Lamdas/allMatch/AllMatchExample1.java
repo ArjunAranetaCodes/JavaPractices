b56f1bd8b35f15c3f2f18e4071c64704d6adfe11
Arjun Araneta <arjunaraneta@gmail.com>
2025-10-19 13:19:12 +1100

    Create AllMatchExample1.java

---
+++ Codings/Lamdas/allMatch/AllMatchExample1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class AllMatchExample1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(2, 4, 6, 8, 10);
+
+        boolean allEven = numbers.stream().allMatch(num -> num % 2 == 0);
+
+        System.out.println("Are all numbers even? " + allEven);
+    }
+}
