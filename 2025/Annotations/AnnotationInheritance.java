42103e52e0b5a3dafb9872be2e8350801c41391b
Arjun Araneta <arjunaraneta@gmail.com>
2025-11-11 08:37:43 +1100

    Create AnnotationInheritance.java

---
+++ 2025/Annotations/AnnotationInheritance.java
@@ -0,0 +1,35 @@
+/**
+ * Tutorial: Annotation Inheritance
+ * Demonstrates inherited annotations
+ */
+import java.lang.annotation.*;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@interface InheritedAnnotation {
+    String value();
+}
+
+@InheritedAnnotation("Parent")
+class Parent {
+}
+
+class Child extends Parent {
+}
+
+public class AnnotationInheritance {
+    public static void main(String[] args) {
+        // Child inherits annotation from Parent
+        if (Child.class.isAnnotationPresent(InheritedAnnotation.class)) {
+            InheritedAnnotation annotation = Child.class.getAnnotation(InheritedAnnotation.class);
+            System.out.println("Child has inherited annotation: " + annotation.value());
+        }
+        
+        // Parent has its own annotation
+        if (Parent.class.isAnnotationPresent(InheritedAnnotation.class)) {
+            InheritedAnnotation annotation = Parent.class.getAnnotation(InheritedAnnotation.class);
+            System.out.println("Parent has annotation: " + annotation.value());
+        }
+    }
+}
+
