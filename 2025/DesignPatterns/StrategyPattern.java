097ff438cc390724b3c9c98112e1f41830874143
Arjun Araneta <arjunaraneta@gmail.com>
2025-11-24 07:09:54 +1100

    Create StrategyPattern.java

---
+++ 2025/DesignPatterns/StrategyPattern.java
@@ -0,0 +1,61 @@
+/**
+ * Tutorial: Strategy Design Pattern
+ * Demonstrates strategy pattern
+ */
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using Credit Card");
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using PayPal");
+    }
+}
+
+class PaymentContext {
+    private PaymentStrategy strategy;
+    
+    public void setStrategy(PaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+    
+    public void executePayment(double amount) {
+        strategy.pay(amount);
+    }
+}
+
+public class StrategyPattern {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+        
+        context.setStrategy(new CreditCardPayment());
+        context.executePayment(100.0);
+        
+        context.setStrategy(new PayPalPayment());
+        context.executePayment(200.0);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
