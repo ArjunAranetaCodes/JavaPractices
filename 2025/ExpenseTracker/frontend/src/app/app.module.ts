ea478bdaef2c5df0023589c8f1b7e2ce6145a81f
Arjun Araneta <arjunaraneta@gmail.com>
2026-01-06 06:54:04 +1100

    Create app.module.ts

---
+++ 2025/ExpenseTracker/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ExpenseService } from './services/expense.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [ExpenseService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
