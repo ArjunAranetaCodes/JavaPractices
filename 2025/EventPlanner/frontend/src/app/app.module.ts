5f0f381be2dfbeac17ad58efd860550be0af1a0e
Arjun Araneta <arjunaraneta@gmail.com>
2026-01-10 08:19:49 +1100

    Create app.module.ts

---
+++ 2025/EventPlanner/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { EventService } from './services/event.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [EventService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
