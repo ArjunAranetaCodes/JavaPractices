dff7e65e52112da2082be9afe81fcf788d04a174
Arjun Araneta <arjunaraneta@gmail.com>
2026-01-11 08:35:23 +1100

    Create blog-post.service.ts

---
+++ 2025/BlogManagement/frontend/src/app/services/blog-post.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+  private apiUrl = 'http://localhost:8101/api/posts';
+
+  constructor(private http: HttpClient) {}
+
+  getAllPosts(): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(this.apiUrl);
+  }
+
+  getPostById(id: number): Observable<BlogPost> {
+    return this.http.get<BlogPost>(`${this.apiUrl}/${id}`);
+  }
+
+  searchPosts(query: string): Observable<BlogPost[]> {
+    const params = new HttpParams().set('query', query);
+    return this.http.get<BlogPost[]>(`${this.apiUrl}/search`, { params });
+  }
+
+  getPostsByStatus(status: string): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${this.apiUrl}/status/${status}`);
+  }
+
+  getPostsByCategory(category: string): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${this.apiUrl}/category/${category}`);
+  }
+
+  getPostsByAuthor(author: string): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${this.apiUrl}/author/${author}`);
+  }
+
+  getFeaturedPosts(): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${this.apiUrl}/featured`);
+  }
+
+  createPost(post: BlogPost): Observable<BlogPost> {
+    return this.http.post<BlogPost>(this.apiUrl, post);
+  }
+
+  updatePost(id: number, post: BlogPost): Observable<BlogPost> {
+    return this.http.put<BlogPost>(`${this.apiUrl}/${id}`, post);
+  }
+
+  deletePost(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
+
