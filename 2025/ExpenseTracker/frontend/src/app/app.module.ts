047e6e16f73d5cbd8887c013104b72e41f72d0fe
Arjun Araneta <arjunaraneta@gmail.com>
2026-01-25 18:32:04 +1100

    Update app.module.ts

--- 2025/ExpenseTracker/frontend/src/app/app.module.ts
+++ 2025/ExpenseTracker/frontend/src/app/app.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ExpenseService } from './services/expense.service';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseListComponent,
+    ExpenseFormComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
-  providers: [ExpenseService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
