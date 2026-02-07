f072967ec11bf0068a8b5607ac90c61311dea082
Arjun Araneta <arjunaraneta@gmail.com>
2026-02-08 06:25:14 +1100

    Create app.module.ts

---
+++ 2025/MovieWatchlist/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MovieService } from './services/movie.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [MovieService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
