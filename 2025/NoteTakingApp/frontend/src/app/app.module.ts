4f9ac4f0bf1d06da1bc0b84510e886fb41f382c5
Arjun Araneta <arjunaraneta@gmail.com>
2026-02-14 08:52:18 +1100

    Create app.module.ts

---
+++ 2025/NoteTakingApp/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NoteService } from './services/note.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [NoteService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
