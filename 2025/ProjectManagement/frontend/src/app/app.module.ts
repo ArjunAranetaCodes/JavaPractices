407fe23165ffba6761e5d6291491a9e41228a9f6
Arjun Araneta <arjunaraneta@gmail.com>
2026-03-04 07:15:49 +1100

    Create app.module.ts

---
+++ 2025/ProjectManagement/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProjectService } from './services/project.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [ProjectService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
