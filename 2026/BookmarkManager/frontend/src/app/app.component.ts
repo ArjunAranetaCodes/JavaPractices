88ab115c429703206b6940675d50ec1092a0580d
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-07 07:04:27 +1000

    Create app.component.ts

---
+++ 2026/BookmarkManager/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-bookmark-list></app-bookmark-list>',
+  styles: []
+})
+export class AppComponent {
+  title = 'Bookmark Manager';
+}
