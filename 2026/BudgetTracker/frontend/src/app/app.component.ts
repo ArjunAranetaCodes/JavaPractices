d7d6b1488603c1c3124301c12a9326606e1964e7
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-09 07:41:19 +1000

    Create app.component.ts

---
+++ 2026/BudgetTracker/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-transaction-list></app-transaction-list>',
+  styles: []
+})
+export class AppComponent {
+  title = 'Budget Tracker';
+}
