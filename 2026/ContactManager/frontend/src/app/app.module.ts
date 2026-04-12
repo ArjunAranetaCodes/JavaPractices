e2d72478849437534ed0db53c05c16b5af9bf305
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-13 07:12:52 +1000

    Create app.module.ts

---
+++ 2026/ContactManager/frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactListComponent,
+    ContactFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
