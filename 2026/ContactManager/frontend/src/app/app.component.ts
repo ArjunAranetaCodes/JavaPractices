621f41bba76b2adbac3fb601c89226c17c627756
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-13 07:12:43 +1000

    Create app.component.ts

---
+++ 2026/ContactManager/frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-contact-list></app-contact-list>',
+  styles: []
+})
+export class AppComponent {
+  title = 'Contact Manager';
+}
+
