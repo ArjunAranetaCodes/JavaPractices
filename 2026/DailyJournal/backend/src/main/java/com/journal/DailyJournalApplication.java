574d0480eadd384cd9afd4ac597b9524a78692da
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-18 09:09:13 +1000

    Create DailyJournalApplication.java

---
+++ 2026/DailyJournal/backend/src/main/java/com/journal/DailyJournalApplication.java
@@ -0,0 +1,12 @@
+package com.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyJournalApplication.class, args);
+    }
+}
