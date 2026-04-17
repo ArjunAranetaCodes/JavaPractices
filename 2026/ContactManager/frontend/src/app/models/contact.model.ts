daaac2b258609674398fc343f60201342011b345
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-18 09:09:03 +1000

    Create contact.model.ts

---
+++ 2026/ContactManager/frontend/src/app/models/contact.model.ts
@@ -0,0 +1,12 @@
+export interface Contact {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone?: string;
+  company?: string;
+  jobTitle?: string;
+  notes?: string;
+  favorite?: boolean;
+}
+
