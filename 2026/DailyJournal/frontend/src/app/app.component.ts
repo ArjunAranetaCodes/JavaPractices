5d46805d287010811b537a5eef48ff657b2dd0f4
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-19 13:16:26 +1000

    Create app.component.ts

---
+++ 2026/DailyJournal/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-journal-list></app-journal-list>',
+  styles: []
+})
+export class AppComponent {
+  title = 'Daily Journal';
+}
