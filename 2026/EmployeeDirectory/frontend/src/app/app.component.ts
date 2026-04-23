231e4e860a3bd8bb9386be8ad72b493a5c432d2a
Arjun Araneta <arjunaraneta@gmail.com>
2026-04-24 08:58:16 +1000

    Create app.component.ts

---
+++ 2026/EmployeeDirectory/frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-employee-list></app-employee-list>',
+  styles: []
+})
+export class AppComponent {
+  title = 'Employee Directory';
+}
+
