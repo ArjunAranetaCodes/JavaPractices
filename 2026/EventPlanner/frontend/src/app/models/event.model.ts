e0f24e41cce465a3ff3ae7d5ee9dc06c27fb7037
Arjun Araneta <arjunaraneta@gmail.com>
2026-05-27 12:09:32 +1000

    Create event.model.ts

---
+++ 2026/EventPlanner/frontend/src/app/models/event.model.ts
@@ -0,0 +1,10 @@
+export interface EventItem {
+  id?: number;
+  title: string;
+  description?: string;
+  location?: string;
+  startDateTime: string;
+  endDateTime: string;
+  category?: string;
+  status?: string;
+}
